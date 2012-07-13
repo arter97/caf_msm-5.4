ae78b144a42822c0de6b0f1ef56cc85f87d3edc5
Murali Nalajala <mnalajal@codeaurora.org>
2012-07-13 16:54:40 +0530

    msm: pm2: Add CPR specific function callbacks to PM driver
    CPR(Core Power Reduction) is a new h/w block added in 8x25
    2.0 revision. This block reduce the power consumption of
    SOC using sensors inside the SOC. Add function callbacks to
    PM driver to perform disable, enable, save and restore of
    CPR context data when the core do an IdlePC.
    
    Signed-off-by: Murali Nalajala <mnalajal@codeaurora.org>
    (cherry picked from commit ff723ece018452e722155daa67ff0fdd65cc5c96)
    
    Change-Id: I4f9ba9a06919c58c6fe2cbc8ce60267be5ce7f22
    Signed-off-by: Utsab Bose <ubose@codeaurora.org>

--- arch/arm/mach-msm/pm.h
+++ arch/arm/mach-msm/pm.h
@@ -76,6 +76,11 @@ struct msm_pm_sleep_status_data {
 	uint32_t mask;
 };
 
+struct msm_pm_cpr_ops {
+	void (*cpr_suspend)(void);
+	void (*cpr_resume)(void);
+};
+
 void msm_pm_set_platform_data(struct msm_pm_platform_data *data, int count);
 void msm_pm_set_irq_extns(struct msm_pm_irq_calls *irq_calls);
 int msm_pm_idle_prepare(struct cpuidle_device *dev);
@@ -125,5 +130,7 @@ static inline void msm_pm_add_stats(enum msm_pm_time_stats_id *enable_stats,
 		int size) {}
 static inline void msm_pm_add_stat(enum msm_pm_time_stats_id id, int64_t t) {}
 #endif
+
+void msm_pm_set_cpr_ops(struct msm_pm_cpr_ops *ops);
 
 #endif  /* __ARCH_ARM_MACH_MSM_PM_H */
