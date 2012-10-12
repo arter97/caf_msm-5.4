1edba140244141de925728f6eb4744c9a6e86dd5
Manu Gautam <mgautam@codeaurora.org>
2012-10-12 14:02:50 +0530

    DWC3: Enable XHCI host in OTG mode
    DWC3 has three kind of registers and initilization sequences:
    Global, Host and Device. When switching DWC3 roles in OTG mode
    from A to B, or B to A, software needs to reinitialization some
    Global registers (Event Buffers) and as well as reinitialize
    ep0in/out endpoints when switching to peripheral mode.
    Host mode requires OTG driver to set PortPowerControl IN OSTS
    register only after XHCI drivers sets the same in PORTSC.
    
    Change-Id: I9bc29e00e31d68c0834b6c93896d38267f317c0c
    Signed-off-by: Manu Gautam <mgautam@codeaurora.org>

--- drivers/usb/host/xhci-plat.c
+++ drivers/usb/host/xhci-plat.c
@@ -179,7 +179,6 @@ static int xhci_plat_probe(struct platform_device *pdev)
 	phy = devm_usb_get_phy(&pdev->dev, USB_PHY_TYPE_USB2);
 	if (phy && phy->otg) {
 		dev_dbg(&pdev->dev, "%s otg support available\n", __func__);
-		hcd->driver->stop(hcd);
 		ret = otg_set_host(phy->otg, &hcd->self);
 		if (ret) {
 			dev_err(&pdev->dev, "%s otg_set_host failed\n",
