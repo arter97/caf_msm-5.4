0932d40628bec8107783024caeb2130712f2796d
Anshuman Dani <adani@codeaurora.org>
2012-11-06 22:19:50 +0530

    msm: kgsl: Add CP_WAIT_FOR_IDLE packet before updating timestamp
    HW workaround to resolve MMU pagefault caused by memory
    freeing early before GPU completes it.
    
    This is race condition where timestamp is getting updated
    and the event handler runs before the GPU actually got done
    with the timestamp, hence freeing the buffer early while GPU
    still not completed with it.
    
    Change-Id: I6d338d7dca680f57f2c39ed5a14381b261bf8bce
    Signed-off-by: Anshuman Dani <adani@codeaurora.org>

--- drivers/gpu/msm/adreno_ringbuffer.c
+++ drivers/gpu/msm/adreno_ringbuffer.c
@@ -613,6 +613,16 @@ adreno_ringbuffer_addcmds(struct adreno_ringbuffer *rb,
 		GSL_RB_WRITE(ringcmds, rcmd_gpu, 0x00);
 	}
 
+	/* HW Workaround for MMU Page fault
+	* due to memory getting free early before
+	* GPU completes it.
+	*/
+	if (adreno_is_a2xx(adreno_dev)) {
+		GSL_RB_WRITE(ringcmds, rcmd_gpu,
+			cp_type3_packet(CP_WAIT_FOR_IDLE, 1));
+		GSL_RB_WRITE(ringcmds, rcmd_gpu, 0x00);
+	}
+
 	/* scratchpad ts for recovery */
 	GSL_RB_WRITE(ringcmds, rcmd_gpu, cp_type0_packet(REG_CP_TIMESTAMP, 1));
 	GSL_RB_WRITE(ringcmds, rcmd_gpu, rb->timestamp[KGSL_MEMSTORE_GLOBAL]);
