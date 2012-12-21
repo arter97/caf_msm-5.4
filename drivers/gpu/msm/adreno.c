bad6983a19e6d08d869cdbf60a572c9399fdd3ef
Anshuman Dani <adani@codeaurora.org>
2012-12-21 12:35:31 +0530

    msm: kgsl: Ensure correct GPU patch ID is set
    Correct the patch ID that is returned by newer versions of
    the GPU hardware.  This ensures the correct firmware is used
    for the GPU.
    
    Signed-off-by: Anshuman Dani <adani@codeaurora.org>
    (cherry picked from commit 85f921f80e44bb8e6d30ab17b789178a608ea47d)
    
    Change-Id: I5b1ebc923f03e3927381e6c018d383692f7b8f52
    CRs-fixed: 435396
    Signed-off-by: Srivalli Oguri <oguri@codeaurora.org>

--- drivers/gpu/msm/adreno.c
+++ drivers/gpu/msm/adreno.c
@@ -620,7 +620,7 @@ a2xx_getchipid(struct kgsl_device *device)
 	if (cpu_is_qsd8x50())
 		patchid = 1;
 	else if (cpu_is_msm8960() &&
-			SOCINFO_VERSION_MAJOR(soc_platform_version) == 3)
+			SOCINFO_VERSION_MAJOR(soc_platform_version) >= 3)
 		patchid = 6;
 	else if (cpu_is_msm8625() && minorid == 0)
 		minorid = 1;
