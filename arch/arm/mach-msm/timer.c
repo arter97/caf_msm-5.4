59b1a5a78098f8ad7daf46742eeddca996d214b2
Jay Chokshi <jchokshi@codeaurora.org>
2013-03-12 11:37:18 -0700

    msm: timer: Use 32768Hz GPT and SCLK on 8064 family
    On 8064 SOC family, a dedicated XO is derived to support
    sleep clock at 32768Hz unlike 8960 SOC family where TCXO
    divider was used with effective freq. of 32765Hz.
    
    CRs-fixed: 462079
    CRs-Fixed: 453095
    Change-Id: I79142703c4ffac209f4dedeefebc235eeb8b140d
    Signed-off-by: Jay Chokshi <jchokshi@codeaurora.org>
    Signed-off-by: Taniya Das <tdas@codeaurora.org>

--- arch/arm/mach-msm/timer.c
+++ arch/arm/mach-msm/timer.c
@@ -1,7 +1,7 @@
 /* linux/arch/arm/mach-msm/timer.c
  *
  * Copyright (C) 2007 Google, Inc.
- * Copyright (c) 2009-2012, The Linux Foundation. All rights reserved.
+ * Copyright (c) 2009-2013, The Linux Foundation. All rights reserved.
  *
  * This software is licensed under the terms of the GNU General Public
  * License version 2, as published by the Free Software Foundation, and
@@ -1030,9 +1030,11 @@ static void __init msm_timer_init(void)
 		__raw_writel(DGT_CLK_CTL_DIV_4, MSM_TMR_BASE + DGT_CLK_CTL);
 		gpt->status_mask = BIT(10);
 		dgt->status_mask = BIT(2);
-		gpt->freq = 32765;
-		gpt_hz = 32765;
-		sclk_hz = 32765;
+		if (!cpu_is_apq8064()) {
+			gpt->freq = 32765;
+			gpt_hz = 32765;
+			sclk_hz = 32765;
+		}
 		if (!cpu_is_msm8930() && !cpu_is_msm8930aa() &&
 		    !cpu_is_msm8627()) {
 			gpt->flags |= MSM_CLOCK_FLAGS_UNSTABLE_COUNT;
