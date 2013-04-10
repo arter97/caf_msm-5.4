b2314c65ec1073b76a0d4aa17d00cb25255c4d53
Amy Maloche <amaloche@codeaurora.org>
2013-04-10 13:04:52 +0100

    input: atmel_mxt_ts: Disabling secure touch for power events.
    Any power event in the driver happening while secure touch is
    active will cause it to close.
    Additional fix to make sure secure touch events are forwarded
    only if all previous events have already been processed.
    
    Change-Id: I57f7cb45e2e53a923b9ab3b7c9baa34f1d3f9b5f
    Acked-by: Christian Bolis <cbolis@qti.qualcomm.com>
    Signed-off-by: Amy Maloche <amaloche@codeaurora.org>

--- drivers/input/touchscreen/atmel_mxt_ts.c
+++ drivers/input/touchscreen/atmel_mxt_ts.c
@@ -392,6 +392,7 @@ struct mxt_data {
 	atomic_t st_enabled;
 	atomic_t st_pending_irqs;
 	struct completion st_completion;
+	struct completion st_powerdown;
 #endif
 };
 
@@ -949,8 +950,8 @@ static void mxt_handle_touch_suppression(struct mxt_data *data, u8 status)
 static irqreturn_t mxt_filter_interrupt(struct mxt_data *data)
 {
 	if (atomic_read(&data->st_enabled)) {
-		atomic_cmpxchg(&data->st_pending_irqs, 0, 1);
-		complete(&data->st_completion);
+		if (atomic_cmpxchg(&data->st_pending_irqs, 0, 1) == 0)
+			complete(&data->st_completion);
 		return IRQ_HANDLED;
 	}
 	return IRQ_NONE;
@@ -1995,6 +1996,7 @@ static ssize_t mxt_secure_touch_enable_store(struct device *dev,
 		atomic_set(&data->st_enabled, 0);
 		complete(&data->st_completion);
 		mxt_interrupt(data->client->irq, data);
+		complete(&data->st_powerdown);
 		break;
 	case 1:
 		if (atomic_read(&data->st_enabled)) {
@@ -2007,6 +2009,8 @@ static ssize_t mxt_secure_touch_enable_store(struct device *dev,
 			err = -EIO;
 			break;
 		}
+		INIT_COMPLETION(data->st_completion);
+		INIT_COMPLETION(data->st_powerdown);
 		atomic_set(&data->st_pending_irqs, 0);
 		atomic_set(&data->st_enabled, 1);
 		break;
@@ -2034,7 +2038,7 @@ static ssize_t mxt_secure_touch_show(struct device *dev,
 		return err;
 
 	if (atomic_cmpxchg(&data->st_pending_irqs, 1, 0) != 1)
-		return -EBADF;
+		return -EINVAL;
 
 	return scnprintf(buf, PAGE_SIZE, "%u", 1);
 }
@@ -2067,9 +2071,25 @@ static const struct attribute_group mxt_attr_group = {
 	.attrs = mxt_attrs,
 };
 
+
+#if defined(CONFIG_SECURE_TOUCH)
+static void mxt_secure_touch_stop(struct mxt_data *data)
+{
+	if (atomic_read(&data->st_enabled)) {
+		complete(&data->st_completion);
+		wait_for_completion_interruptible(&data->st_powerdown);
+	}
+}
+#else
+static void mxt_secure_touch_stop(struct mxt_data *data)
+{
+}
+#endif
+
 static int mxt_start(struct mxt_data *data)
 {
 	int error;
+	mxt_secure_touch_stop(data);
 
 	/* restore the old power state values and reenable touch */
 	error = __mxt_write_reg(data->client, data->t7_start_addr,
@@ -2087,6 +2107,7 @@ static int mxt_stop(struct mxt_data *data)
 {
 	int error;
 	u8 t7_data[T7_DATA_SIZE] = {0};
+	mxt_secure_touch_stop(data);
 
 	error = __mxt_write_reg(data->client, data->t7_start_addr,
 				T7_DATA_SIZE, t7_data);
@@ -2623,6 +2644,7 @@ static int fb_notifier_callback(struct notifier_block *self,
 static void secure_touch_init(struct mxt_data *data)
 {
 	init_completion(&data->st_completion);
+	init_completion(&data->st_powerdown);
 }
 #else
 static void secure_touch_init(struct mxt_data *data)
@@ -2851,7 +2873,7 @@ static int mxt_probe(struct i2c_client *client,
 
 	mxt_debugfs_init(data);
 
-	secure_touch_init(data);
+	mxt_secure_touch_init(data);
 
 	return 0;
 
@@ -3066,6 +3088,7 @@ static int mxt_resume(struct device *dev)
 
 	/* calibrate */
 	if (data->pdata->need_calibration) {
+		mxt_secure_touch_stop(data);
 		error = mxt_write_object(data, MXT_GEN_COMMAND_T6,
 					MXT_COMMAND_CALIBRATE, 1);
 		if (error < 0)
