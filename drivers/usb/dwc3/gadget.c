001c337d11216edf8e4a821e995065d313e7c388
Vijayavardhan Vennapusa <vvreddy@codeaurora.org>
2013-05-17 13:24:49 +0530

    USB: dwc3: gadget: Run time resume dwc3 device in dwc3_gadget_start()
    There might be scenario where dwc3_msm platform device is runtime
    suspended and is in low power mode by the time composite_probe
    calls dwc3_gadget_start(). If device is in low power mode and SW does
    register access, it could cause crash. Hence runtime resume the
    platform device before accessing the register in dwc3_gadget_start().
    
    CRs-Fixed: 487270
    Change-Id: I53676ba42478a8e9fcad0ea7fb24d6c87a57ed1a
    Signed-off-by: Vijayavardhan Vennapusa <vvreddy@codeaurora.org>

--- drivers/usb/dwc3/gadget.c
+++ drivers/usb/dwc3/gadget.c
@@ -1577,6 +1577,7 @@ static int dwc3_gadget_start(struct usb_gadget *g,
 		goto err0;
 	}
 
+	pm_runtime_get_sync(dwc->dev);
 	spin_lock_irqsave(&dwc->lock, flags);
 
 	if (dwc->gadget_driver) {
@@ -1652,6 +1653,7 @@ static int dwc3_gadget_start(struct usb_gadget *g,
 	dwc3_gadget_enable_irq(dwc);
 
 	spin_unlock_irqrestore(&dwc->lock, flags);
+	pm_runtime_put(dwc->dev);
 
 	return 0;
 
@@ -1663,6 +1665,7 @@ err2:
 
 err1:
 	spin_unlock_irqrestore(&dwc->lock, flags);
+	pm_runtime_put(dwc->dev);
 
 	free_irq(irq, dwc);
 
