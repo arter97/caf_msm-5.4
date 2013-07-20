8ca194323d8a8ecc8976b54b4615d80fc190502b
David Keitel <dkeitel@codeaurora.org>
2013-07-19 20:19:06 -0700

    power: qpnp-charger: do not reset iusbmax
    In the current implementation if the charger_monitor
    bit is set and external_power_supply_changed callback
    is called the USB input current limit may be reconfigured
    to the USB_WALL_THRESHOLD_MA value.
    
    Fix this by only allowing the input current limit for usb
    to be changed through the external_power_supply_change
    callback if it has changed from the previous requested
    value.
    
    CRs-Fixed: 457381
    Change-Id: Ic85941740bc3e9131d68feb8e4ec41621c95abb8
    Signed-off-by: David Keitel <dkeitel@codeaurora.org>

--- drivers/power/qpnp-charger.c
+++ drivers/power/qpnp-charger.c
@@ -296,6 +296,7 @@ struct qpnp_chg_chip {
 	unsigned int			safe_voltage_mv;
 	unsigned int			max_voltage_mv;
 	unsigned int			min_voltage_mv;
+	int				prev_usb_max_ma;
 	int				set_vddmax_mv;
 	int				delta_vddmax_mv;
 	unsigned int			warm_bat_mv;
@@ -1007,6 +1008,7 @@ qpnp_chg_usb_usbin_valid_irq_handler(int irq, void *_chip)
 			qpnp_chg_usb_suspend_enable(chip, 1);
 			if (!qpnp_chg_is_dc_chg_plugged_in(chip))
 				chip->chg_done = false;
+			chip->prev_usb_max_ma = -EINVAL;
 		} else {
 			schedule_delayed_work(&chip->eoc_work,
 				msecs_to_jiffies(EOC_CHECK_PERIOD_MS));
@@ -1625,6 +1627,10 @@ qpnp_batt_external_power_changed(struct power_supply *psy)
 	if (qpnp_chg_is_usb_chg_plugged_in(chip)) {
 		chip->usb_psy->get_property(chip->usb_psy,
 			  POWER_SUPPLY_PROP_CURRENT_MAX, &ret);
+
+		if (chip->prev_usb_max_ma == ret.intval)
+			goto skip_set_iusb_max;
+
 		if (ret.intval <= 2 && !chip->use_default_batt_values &&
 						get_prop_batt_present(chip)) {
 			qpnp_chg_usb_suspend_enable(chip, 1);
@@ -1640,8 +1646,10 @@ qpnp_batt_external_power_changed(struct power_supply *psy)
 				qpnp_chg_iusbmax_set(chip, ret.intval / 1000);
 			}
 		}
+		chip->prev_usb_max_ma = ret.intval;
 	}
 
+skip_set_iusb_max:
 	pr_debug("end of power supply changed\n");
 	power_supply_changed(&chip->batt_psy);
 }
@@ -3102,6 +3110,7 @@ qpnp_charger_probe(struct spmi_device *spmi)
 		return -ENOMEM;
 	}
 
+	chip->prev_usb_max_ma = -EINVAL;
 	chip->dev = &(spmi->dev);
 	chip->spmi = spmi;
 
