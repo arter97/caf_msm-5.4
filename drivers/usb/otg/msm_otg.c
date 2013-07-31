f2ae8a5ab5660214ef16b64ccc3024d1bcb20912
Danny Segal <dsegal@codeaurora.org>
2013-07-31 13:53:43 +0300

    usb: msm_otg: Workaound for USB HW issue with remote-wakeup during LPM
    When a remote-wakeup operation is issued by the SW during USB
    low-power mode, the USB HW issues a J to K state transition on the USB bus.
    Shortly after this transition the D- lines drops from 3.3V to 1.5V.
    The SW workaround implemented for this issue is to write the FPR bit of
    the PORTSC register (which triggers USB remote-wakeup) in the same write
    of PHCD bit clearing (which exits the USB HW from low-power mode).
    
    Change-Id: I4a7efd0a57ece8771f451a0f5b1526f7b00261fd
    CRs-Fixed: 502579
    Signed-off-by: Daniel Segal <dsegal@codeaurora.org>

--- drivers/usb/otg/msm_otg.c
+++ drivers/usb/otg/msm_otg.c
@@ -974,6 +974,9 @@ static int msm_otg_resume(struct msm_otg *motg)
 	unsigned temp;
 	u32 phy_ctrl_val = 0;
 	unsigned ret;
+	bool in_device_mode;
+	bool bus_is_suspended;
+	bool is_remote_wakeup;
 
 	if (!atomic_read(&motg->in_lpm))
 		return 0;
@@ -1025,17 +1028,37 @@ static int msm_otg_resume(struct msm_otg *motg)
 	 * PHY comes out of low power mode (LPM) in case of wakeup
 	 * from asynchronous interrupt.
 	 */
-	if (!(readl(USB_PORTSC) & PORTSC_PHCD))
+	if (!(readl_relaxed(USB_PORTSC) & PORTSC_PHCD))
 		goto skip_phy_resume;
 
-	writel(readl(USB_PORTSC) & ~PORTSC_PHCD, USB_PORTSC);
+	in_device_mode = otg->gadget && test_bit(ID, &motg->inputs);
+
+	bus_is_suspended =
+		readl_relaxed(USB_PORTSC) & PORTSC_SUSP_MASK;
+
+	is_remote_wakeup = in_device_mode && bus_is_suspended;
+
+	if (is_remote_wakeup && pdata->rw_during_lpm_workaround) {
+		/* In some targets there is a HW issue with remote wakeup
+		 * during low-power mode. As a workaround, the FPR bit
+		 * is written simultaneously with the clearing of the
+		 * PHCD bit.
+		 */
+		writel_relaxed(
+			(readl_relaxed(USB_PORTSC) & ~PORTSC_PHCD) |
+			PORTSC_FPR_MASK,
+			USB_PORTSC);
+	} else {
+		writel_relaxed(readl_relaxed(USB_PORTSC) & ~PORTSC_PHCD,
+			USB_PORTSC);
+	}
+
 	while (cnt < PHY_RESUME_TIMEOUT_USEC) {
-		if (!(readl(USB_PORTSC) & PORTSC_PHCD))
+		if (!(readl_relaxed(USB_PORTSC) & PORTSC_PHCD))
 			break;
 		udelay(1);
 		cnt++;
 	}
-
 	if (cnt >= PHY_RESUME_TIMEOUT_USEC) {
 		/*
 		 * This is a fatal error. Reset the link and
@@ -3335,6 +3358,8 @@ struct msm_otg_platform_data *msm_otg_dt_to_pdata(struct platform_device *pdev)
 				&pdata->phy_type);
 	of_property_read_u32(node, "qcom,hsusb-otg-pmic-id-irq",
 				&pdata->pmic_id_irq);
+	of_property_read_u32(node, "qcom,hsusb-otg-rw-during-lpm-workaround",
+				(u32*)&pdata->rw_during_lpm_workaround);
 	return pdata;
 }
 
