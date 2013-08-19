2c73af30b92fd328281eb45ead8b00140bcb334b
Dhivya Subramanian <dthiru@codeaurora.org>
2013-08-19 13:22:12 -0700

    Revert "msm: kgsl: fix a race condition when calling kref_put"
    This reverts commit 5675478ac37cec80a12e2787939adb268acb1006
    
    Change-Id: Ib5b6f0ba4be824f8ddf81e60e09cd591ce7e524a
    Signed-off-by: Dhivya Subramanian <dthiru@codeaurora.org>

--- drivers/gpu/msm/kgsl.h
+++ drivers/gpu/msm/kgsl.h
@@ -320,10 +320,10 @@ static inline int timestamp_cmp(unsigned int a, unsigned int b)
 	return ((a > b) && (a - b <= KGSL_TIMESTAMP_WINDOW)) ? 1 : -1;
 }
 
-static inline int
+static inline void
 kgsl_mem_entry_get(struct kgsl_mem_entry *entry)
 {
-	return kref_get_unless_zero(&entry->refcount);
+	kref_get(&entry->refcount);
 }
 
 static inline void
