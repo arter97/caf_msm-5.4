CAF-staging/msm8916_kernel

msm: camera: use 24MHz for camera sensor ext clk
8974 V1 hardware was not supporting 24MHz mclk.
adding support to use 24 Mhz clk from V2.

Change-Id: I3b75fe81d127b755ac9a93e35fedd9fc670566d5
Signed-off-by: Xu Han <hanxu@codeaurora.org>

arch/arm/mach-msm/board-8974-gpiomux.c
@@ -1365,6 +1365,11 @@ void __init msm_8974_init_gpiomux(void)
 		return;
 	}
 
+	pr_debug("%s:%d socinfo_get_version %x\n", __func__, __LINE__,
+		socinfo_get_version());
+	if (socinfo_get_version() >= 0x20000)
+		msm_tlmm_misc_reg_write(TLMM_SPARE_REG, 0xf);
+
 #if defined(CONFIG_KS8851) || defined(CONFIG_KS8851_MODULE)
 	if (!(of_board_is_dragonboard() && machine_is_apq8074()))
 		msm_gpiomux_install(msm_eth_configs, \

arter97/caf_msm-5.4

arch/arm/mach-msm/board-8974-gpiomux.c
@@ -1194,6 +1194,11 @@ void __init msm_8974_init_gpiomux(void)
 		return;
 	}
 
+	pr_err("%s:%d socinfo_get_version %x\n", __func__, __LINE__,
+		socinfo_get_version());
+	if (socinfo_get_version() >= 0x20000)
+		msm_tlmm_misc_reg_write(TLMM_SPARE_REG, 0xf);
+
 #if defined(CONFIG_KS8851) || defined(CONFIG_KS8851_MODULE)
 	msm_gpiomux_install(msm_eth_configs, ARRAY_SIZE(msm_eth_configs));
 #endif