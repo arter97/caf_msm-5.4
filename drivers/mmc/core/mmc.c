f87dd6c646b185f95b18e207695c7452745a6261
Sujit Reddy Thumma <sthumma@codeaurora.org>
2013-12-05 19:35:43 +0530

    mmc: core: fix buffer overflow during memcpy of ext_csd
    Fix buffer overflow while caching the mmc ext_csd content.
    Also, to avoid duplicate allocation keep the allocated ext_csd
    till the card is removed.
    
    CRs-Fixed: 583929
    Change-Id: I5d69e37f6fd1f5249479d454c353be050df40b6d
    Signed-off-by: Sujit Reddy Thumma <sthumma@codeaurora.org>

--- drivers/mmc/core/mmc.c
+++ drivers/mmc/core/mmc.c
@@ -1398,7 +1398,7 @@ static int mmc_init_card(struct mmc_host *host, u32 ocr,
 		err = mmc_get_ext_csd(card, &ext_csd);
 		if (err)
 			goto free_card;
-		memcpy(&card->cached_ext_csd, ext_csd, sizeof(card->ext_csd));
+		card->cached_ext_csd = ext_csd;
 		err = mmc_read_ext_csd(card, ext_csd);
 		if (err)
 			goto free_card;
@@ -1598,7 +1598,6 @@ static int mmc_init_card(struct mmc_host *host, u32 ocr,
 	}
 
 
-	mmc_free_ext_csd(ext_csd);
 	return 0;
 
 free_card:
@@ -1606,8 +1605,6 @@ free_card:
 	if (!oldcard)
 		mmc_remove_card(card);
 err:
-	mmc_free_ext_csd(ext_csd);
-
 	return err;
 }
 
