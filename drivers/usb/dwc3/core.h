effd3248e97b8be2d28135087af266653b345d84
Maya Erez <merez@codeaurora.org>
2013-12-10 13:34:02 +0200

    usb: dwc3: Do not set half-full elastic buffer
    On some platforms setting of half-full elastic buffer will cause data
    corruption and hence we need to avoid this setting.
    Disable the usage of the default setting of half-full elastic buffer
    towards SolvNet Case 8000626044.
    
    Change-Id: I113c6e0602e057496d5e84fc2f6a3f59d12efc98
    Signed-off-by: Maya Erez <merez@codeaurora.org>

--- drivers/usb/dwc3/core.h
+++ drivers/usb/dwc3/core.h
@@ -805,6 +805,8 @@ struct dwc3 {
 	void (*notify_event) (struct dwc3 *, unsigned);
 	int			tx_fifo_size;
 	bool			tx_fifo_reduced;
+
+	bool			nominal_elastic_buffer;
 };
 
 /* -------------------------------------------------------------------------- */
