66c6b59d85402f0ded2337bfbeb2e9a23aec82c9
Katish Paran <kparan@codeaurora.org>
2013-12-24 17:46:29 +0530

    diag: Safeguard for bound checks and integer underflow.
    At certain point in diag driver there can be integer underflow
    and thus can lead to memory leak. Bound checks are placed to
    ensure correct behavior of condition statements.
    
    CRs-fixed: 549470
    Conflicts:
    	drivers/char/diag/diag_debugfs.c
    
    Change-Id: I47e02f764c2c7412db6f90fd42192fee32a761d3
    Signed-off-by: Katish Paran <kparan@codeaurora.org>
    Signed-off-by: Abhishek Ranjan <aranja@codeaurora.org>

--- drivers/char/diag/diag_debugfs.c
+++ drivers/char/diag/diag_debugfs.c
@@ -195,7 +195,8 @@ static ssize_t diag_dbgfs_read_dcistats(struct file *file,
 				char __user *ubuf, size_t count, loff_t *ppos)
 {
 	char *buf = NULL;
-	int bytes_remaining, bytes_written = 0, bytes_in_buf = 0, i = 0;
+	unsigned int bytes_remaining, bytes_written = 0;
+	unsigned int bytes_in_buf = 0, i = 0;
 	struct diag_dci_data_info *temp_data = dci_data_smd;
 	unsigned int buf_size;
 	buf_size = (DEBUG_BUF_SIZE < count) ? DEBUG_BUF_SIZE : count;
@@ -366,8 +367,7 @@ static ssize_t diag_dbgfs_read_table(struct file *file, char __user *ubuf,
 	unsigned int bytes_remaining;
 	unsigned int bytes_in_buffer = 0;
 	unsigned int bytes_written;
-	unsigned int buf_size;
-	buf_size = (DEBUG_BUF_SIZE < count) ? DEBUG_BUF_SIZE : count;
+	int buf_size = (DEBUG_BUF_SIZE < count) ? DEBUG_BUF_SIZE : count;
 
 	if (diag_dbgfs_table_index >= diag_max_reg) {
 		/* Done. Reset to prepare for future requests */
@@ -536,7 +536,7 @@ static ssize_t diag_dbgfs_read_bridge(struct file *file, char __user *ubuf,
 	unsigned int bytes_remaining;
 	unsigned int bytes_in_buffer = 0;
 	unsigned int bytes_written;
-	unsigned int buf_size;
+	int buf_size = (DEBUG_BUF_SIZE < count) ? DEBUG_BUF_SIZE : count;
 	int bytes_hsic_inited = 45;
 	int bytes_hsic_not_inited = 410;
 
