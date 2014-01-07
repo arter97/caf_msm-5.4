4edcedb1aa806bf5aa2141a106b0757304b93436
Nirmal Abraham <nabrah@codeaurora.org>
2014-01-07 14:17:55 +0530

    Restore MM_HEAP size for IOMMU disabled configuration - Change MM_HEAP size back to 120 MB to account for   camera ZSL mode usecase.
    Change-Id: I9cd7f8d2d9cf127f7d98d4585981e588c4966b47
    Signed-off-by: Nirmal Abraham <nabrah@codeaurora.org>

--- arch/arm/mach-msm/board-8960.c
+++ arch/arm/mach-msm/board-8960.c
@@ -144,7 +144,7 @@ struct sx150x_platform_data msm8960_sx150x_data[] = {
 
 #endif
 
-#define MSM_PMEM_ADSP_SIZE         0x6000000 /* 96 Mbytes */
+#define MSM_PMEM_ADSP_SIZE         0x7800000 /* 120 Mbytes */
 #define MSM_PMEM_AUDIO_SIZE        0x4CF000
 #define MSM_PMEM_SIZE 0x4000000 /* 64 Mbytes */
 #define MSM_LIQUID_PMEM_SIZE 0x4000000 /* 64 Mbytes */
