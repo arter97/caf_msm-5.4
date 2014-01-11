b6682cb46191250019308ab216ddd4cb17da8b33
Jack Pham <jackp@codeaurora.org>
2014-01-10 20:14:01 -0800

    usb: dwc3 / xhci_plat: Call xhci_suspend/resume when entering/exiting LPM
    Before placing the controller in low power mode using runtime
    suspend, if host mode is active, xhci_suspend() should be
    called to properly save state, stop root hub timers and clear
    the Run/Stop bit. Likewise, if the controller receives an event
    not handled by the HCD IRQ while in host suspend, it should resume
    the host driver accordingly.
    
    Add calls to xhci_suspend/resume by introducing runtime PM callbacks
    to the xhci_plat driver (which were previously removed in commit
    9db42d39 "usb: dwc3_otg: Remove phy dependency from xhci-plat").
    
    CRs-Fixed: 590596
    Change-Id: Ifc8016d22d2ad3f5dcd51881d22846952d37667e
    Signed-off-by: Jack Pham <jackp@codeaurora.org>

--- drivers/usb/host/xhci-plat.c
+++ drivers/usb/host/xhci-plat.c
@@ -158,7 +158,9 @@ static int xhci_plat_probe(struct platform_device *pdev)
 		goto release_mem_region;
 	}
 
-	pm_runtime_no_callbacks(&pdev->dev);
+	if (pdev->dev.parent)
+		pm_runtime_resume(pdev->dev.parent);
+
 	pm_runtime_set_active(&pdev->dev);
 	pm_runtime_enable(&pdev->dev);
 	pm_runtime_get_sync(&pdev->dev);
@@ -245,8 +247,32 @@ static int xhci_plat_resume(struct device *dev)
 	return xhci_resume(xhci, 0);
 }
 
+#ifdef CONFIG_PM_RUNTIME
+static int xhci_plat_runtime_suspend(struct device *dev)
+{
+	struct usb_hcd *hcd = dev_get_drvdata(dev);
+	struct xhci_hcd *xhci = hcd_to_xhci(hcd);
+
+	dev_dbg(dev, "xhci-plat runtime suspend\n");
+
+	return xhci_suspend(xhci);
+}
+
+static int xhci_plat_runtime_resume(struct device *dev)
+{
+	struct usb_hcd *hcd = dev_get_drvdata(dev);
+	struct xhci_hcd *xhci = hcd_to_xhci(hcd);
+
+	dev_dbg(dev, "xhci-plat runtime resume\n");
+
+	return xhci_resume(xhci, false);
+}
+#endif
+
 static const struct dev_pm_ops xhci_plat_pm_ops = {
 	SET_SYSTEM_SLEEP_PM_OPS(xhci_plat_suspend, xhci_plat_resume)
+	SET_RUNTIME_PM_OPS(xhci_plat_runtime_suspend, xhci_plat_runtime_resume,
+			   NULL)
 };
 #define DEV_PM_OPS	(&xhci_plat_pm_ops)
 #else
