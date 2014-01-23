08b49826fc6dd5429f0a7e7a25541428267a2d3f
Deepak Kumar <dkumar@codeaurora.org>
2014-01-23 22:53:06 +0530

    Revert "msm: kgsl: Enable the shader corruption workaround for all A3XX targets"
    This reverts commit 3a48a977618cf9ca3c1c8879d1372a132d48e283.
    
    Change-Id: I6c2454e853cbc33a94b44ef41f013d2d6e566737
    Signed-off-by: Deepak Kumar <dkumar@codeaurora.org>

--- drivers/gpu/msm/adreno.c
+++ drivers/gpu/msm/adreno.c
@@ -1962,8 +1962,8 @@ static int adreno_init(struct kgsl_device *device)
 	/* Power down the device */
 	kgsl_pwrctrl_disable(device);
 
-	/* Enable the power on shader corruption fix for all A3XX targets */
-	if (adreno_is_a3xx(adreno_dev))
+	/* Certain targets need the fixup.  You know who you are */
+	if (adreno_is_a330v2(adreno_dev))
 		adreno_a3xx_pwron_fixup_init(adreno_dev);
 
 	set_bit(ADRENO_DEVICE_INITIALIZED, &adreno_dev->priv);
