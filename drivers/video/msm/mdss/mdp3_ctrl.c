arter97/caf_msm-5.4

commit f4e6d5d20829b8a72bdab47bd5c27fe90e3b7e7e

msm: mdss: fix the display reset error
Increase the dma stop time out value as stopping operation
may take longer than the previous defined value. Gracefully
handle the time out condition to allow it to retry when
the next frame comes in.

Change-Id: I5120f309db2dbaeebef3b2878caa13cec17c9e1f
Signed-off-by: rayz <rayz@codeaurora.org>

drivers/video/msm/mdss/mdp3_ctrl.c
@@ -692,7 +692,7 @@ static int mdp3_ctrl_reset_cmd(struct msm_fb_data_type *mfd)
 
 	rc = mdp3_dma->stop(mdp3_dma, mdp3_session->intf);
 	if (rc) {
-		pr_err("fail to stop the MDP3 dma\n");
+		rc = mdp3_dma->stop(mdp3_dma, mdp3_session->intf);
 		goto reset_error;
 	}
 
@@ -749,7 +749,7 @@ static int mdp3_ctrl_reset(struct msm_fb_data_type *mfd)
 
 	rc = mdp3_dma->stop(mdp3_dma, mdp3_session->intf);
 	if (rc) {
-		pr_err("fail to stop the MDP3 dma\n");
+		rc = mdp3_dma->stop(mdp3_dma, mdp3_session->intf);
 		goto reset_error;
 	}
 
@@ -965,7 +965,11 @@ static int mdp3_ctrl_display_commit_kickoff(struct msm_fb_data_type *mfd,
 	panel = mdp3_session->panel;
 	if (!mdp3_iommu_is_attached(MDP3_CLIENT_DMA_P)) {
 		pr_debug("continuous splash screen, IOMMU not attached\n");
-		mdp3_ctrl_reset(mfd);
+		rc = mdp3_ctrl_reset(mfd);
+		if (rc) {
+			pr_err("fail to reset display\n");
+			return -EINVAL;
+		}
 		reset_done = true;
 	}
 	mdp3_release_splash_memory();
@@ -1017,6 +1021,7 @@ static void mdp3_ctrl_pan_display(struct msm_fb_data_type *mfd)
 	u32 offset;
 	int bpp;
 	struct mdss_panel_info *panel_info = mfd->panel_info;
+	int rc = 0;
 
 	pr_debug("mdp3_ctrl_pan_display\n");
 	if (!mfd || !mfd->mdp.private1)
@@ -1028,7 +1033,11 @@ static void mdp3_ctrl_pan_display(struct msm_fb_data_type *mfd)
 
 	if (!mdp3_iommu_is_attached(MDP3_CLIENT_DMA_P)) {
 		pr_debug("continuous splash screen, IOMMU not attached\n");
-		mdp3_ctrl_reset(mfd);
+		rc = mdp3_ctrl_reset(mfd);
+		if (rc) {
+			pr_err("fail to reset display\n");
+			return;
+		}
 	}
 	mdp3_release_splash_memory();
 

drivers/video/msm/mdss/mdp3_dma.c
@@ -18,7 +18,7 @@
 #include "mdp3_hwio.h"
 
 #define DMA_STOP_POLL_SLEEP_US 1000
-#define DMA_STOP_POLL_TIMEOUT_US 32000
+#define DMA_STOP_POLL_TIMEOUT_US 200000
 #define DMA_HISTO_RESET_TIMEOUT_MS 40
 #define DMA_LUT_CONFIG_MASK 0xfffffbe8
 #define DMA_CCS_CONFIG_MASK 0xfffffc17

commit 7c3f7a9bc76cec9f73708feae1d7088a5c9bdade

msm: mdss: fix the error handler in DMA stop
The error handle is wrong in DMA stop, which is introduced
by f4e6d5d20829b8a72bdab47bd5c27fe90e3b7e7e.

Change-Id: I1bcfd7a5b7a1f50c44fa80e8306af2f4a7dafd65
Signed-off-by: rayz <rayz@codeaurora.org>

drivers/video/msm/mdss/mdp3_ctrl.c
@@ -692,7 +692,7 @@ static int mdp3_ctrl_reset_cmd(struct msm_fb_data_type *mfd)
 
 	rc = mdp3_dma->stop(mdp3_dma, mdp3_session->intf);
 	if (rc) {
-		rc = mdp3_dma->stop(mdp3_dma, mdp3_session->intf);
+		pr_err("fail to stop the MDP3 dma %d\n", rc);
 		goto reset_error;
 	}
 
@@ -749,7 +749,7 @@ static int mdp3_ctrl_reset(struct msm_fb_data_type *mfd)
 
 	rc = mdp3_dma->stop(mdp3_dma, mdp3_session->intf);
 	if (rc) {
-		rc = mdp3_dma->stop(mdp3_dma, mdp3_session->intf);
+		pr_err("fail to stop the MDP3 dma %d\n", rc);
 		goto reset_error;
 	}
 