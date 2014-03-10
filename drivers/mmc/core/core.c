7758faec3469f8e496cc75b0e2f730ec490408c2
Adrian Hunter <adrian.hunter@intel.com>
2014-03-10 15:02:41 +0200

    mmc: slot-gpio: Add GPIO descriptor based CD GPIO API
    Add functions to request a CD GPIO using the GPIO descriptor API.
    Note that the new request function is paired with mmc_gpiod_free_cd()
    not mmc_gpio_free_cd().  Note also that it must be called prior to
    mmc_add_host() otherwise the caller must also call
    mmc_gpiod_request_cd_irq().
    
    Change-Id: Ia12816897a3d20c4cc4a2be35f90841aee3144d7
    Signed-off-by: Adrian Hunter <adrian.hunter@intel.com>
    Reviewed-by: Linus Walleij <linus.walleij@linaro.org>
    Signed-off-by: Chris Ball <chris@printf.net>
    Git-commit: 740a221ef0e579dc7c675cf6b90f5313509788f7
    Git-repo: git://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git
    Signed-off-by: Venkat Gopalakrishnan <venkatg@codeaurora.org>

--- drivers/mmc/core/core.c
+++ drivers/mmc/core/core.c
@@ -37,6 +37,7 @@
 #include <linux/mmc/host.h>
 #include <linux/mmc/mmc.h>
 #include <linux/mmc/sd.h>
+#include <linux/mmc/slot-gpio.h>
 
 #include "core.h"
 #include "bus.h"
@@ -2502,6 +2503,7 @@ void mmc_start_host(struct mmc_host *host)
 		mmc_power_off(host);
 	else
 		mmc_power_up(host, host->ocr_avail);
+	mmc_gpiod_request_cd_irq(host);
 	_mmc_detect_change(host, 0, false);
 }
 
@@ -2513,6 +2515,8 @@ void mmc_stop_host(struct mmc_host *host)
 	host->removed = 1;
 	spin_unlock_irqrestore(&host->lock, flags);
 #endif
+	if (host->slot.cd_irq >= 0)
+		disable_irq(host->slot.cd_irq);
 
 	host->rescan_disable = 1;
 	cancel_delayed_work_sync(&host->detect);
