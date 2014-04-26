dda8a92a65c33d931b0bd5023c08e3968aa910a3
Huaibin Yang <huaibiny@codeaurora.org>
2014-04-25 21:26:37 -0700

    msm_fb: display: add mipi dsi video i2c driver
    mipi dsi video panels or bridge chips can be configured through i2c
    bus, so this generic dsi video i2c driver is added to implement basic
    i2c read/write and call back functions for display peripheral
    controls.  Later the specific configuration of dsi and i2c peripheral
    sequence for a specific panel can be added.
    
    Change-Id: Ie5eb1219b9b0855ff7a6c8a36a9197c459341b97
    Signed-off-by: Huaibin Yang <huaibiny@codeaurora.org>

---
+++ drivers/video/msm/mipi_dsi_i2c.c
@@ -0,0 +1,293 @@
+/* Copyright (c) 2014, The Linux Foundation. All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 and
+ * only version 2 as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ */
+#include <linux/types.h>
+#include <linux/kernel.h>
+#include <linux/module.h>
+#include <linux/init.h>
+#include <linux/device.h>
+#include <linux/platform_device.h>
+#include <linux/fs.h>
+#include <linux/i2c.h>
+#include <mach/gpio.h>
+
+#include "msm_fb.h"
+#include "mipi_dsi.h"
+#include "mipi_dsi_i2c.h"
+
+struct mipi_dsi_i2c_resource dsi_i2c_rsc;
+struct mipi_dsi_i2c_configure dsi_i2c_cfg;
+
+/*
+ * If i2c read or write fails, wait for 100ms to try again, and try
+ * max 3 times.
+ */
+#define MAX_WAIT_TIME (100)
+#define MAX_RW_TRIES (3)
+
+static int mipi_dsi_i2c_read(u8 addr, u8 reg, u8 *buf, u8 len)
+{
+	int ret = 0, i = 0;
+	struct i2c_msg msg[2];
+	struct i2c_client *client = dsi_i2c_rsc.client;
+
+	if (!client) {
+		pr_err("no mipi dsi i2c client\n");
+		ret = -ENODEV;
+		goto r_err;
+	}
+
+	client->addr = addr;
+
+	msg[0].addr = addr;
+	msg[0].flags = 0;
+	msg[0].len = 1;
+	msg[0].buf = &reg;
+
+	msg[1].addr = addr;
+	msg[1].flags = I2C_M_RD;
+	msg[1].len = len;
+	msg[1].buf = buf;
+
+	do {
+		if (i2c_transfer(client->adapter, msg, 2) == 2) {
+			ret = 0;
+			goto r_err;
+		}
+		msleep(MAX_WAIT_TIME);
+	} while (++i < MAX_RW_TRIES);
+
+	ret = -EIO;
+	pr_err("i2c read failed after %d tries\n", MAX_RW_TRIES);
+
+r_err:
+	return ret;
+}
+
+int mipi_dsi_i2c_read_byte(u8 addr, u8 reg, u8 *buf)
+{
+	return mipi_dsi_i2c_read(addr, reg, buf, 1);
+}
+
+static int mipi_dsi_i2c_write_byte(u8 addr, u8 reg, u8 val)
+{
+	int ret = 0, i = 0;
+	u8 buf[2] = {reg, val};
+	struct i2c_msg msg[1];
+	struct i2c_client *client = dsi_i2c_rsc.client;
+
+	if (!client) {
+		pr_err("no mipi dsi i2c client\n");
+		ret = -ENODEV;
+		goto w_err;
+	}
+
+	client->addr = addr;
+
+	msg[0].addr = addr;
+	msg[0].flags = 0;
+	msg[0].len = 2;
+	msg[0].buf = buf;
+
+	do {
+		if (i2c_transfer(client->adapter, msg, 1) >= 1) {
+			ret = 0;
+			goto w_err;
+		}
+		msleep(MAX_WAIT_TIME);
+	} while (++i < MAX_RW_TRIES);
+
+	ret = -EIO;
+	pr_err("i2c write failed after %d tries\n", MAX_RW_TRIES);
+
+w_err:
+	return ret;
+}
+
+int mipi_dsi_i2c_write_regs(struct mipi_dsi_i2c_reg_cfg *cfg, int size)
+{
+	int ret = 0;
+	int i;
+
+	for (i = 0; i < size; i++) {
+		ret = mipi_dsi_i2c_write_byte(cfg[i].i2c_addr, cfg[i].reg,
+			cfg[i].val);
+		if (ret) {
+			pr_err("mipi_dsi reg writes failed\n");
+			goto w_regs_fail;
+		}
+		msleep(50);
+	}
+
+w_regs_fail:
+	return ret;
+}
+
+static struct i2c_device_id mipi_dsi_i2c_id[] = {
+	{ "mipi_dsi_i2c", 0 },
+	{ }
+};
+
+static int __devinit mipi_dsi_i2c_probe(struct i2c_client *client,
+	const struct i2c_device_id *id)
+{
+	const struct mipi_dsi_i2c_platform_data *pdata;
+	int ret = 0;
+
+	dsi_i2c_rsc.client = client;
+
+	pdata = client->dev.platform_data;
+	if (!pdata) {
+		ret = -ENODEV;
+		goto p_err;
+	}
+
+	ret = gpio_request(pdata->pd_gpio, "mipi_dsi_i2c_pd");
+	if (ret) {
+		pr_err("%s: gpio request %d failed\n", __func__,
+			pdata->pd_gpio);
+		dsi_i2c_rsc.gpio = -1;
+		ret = -ENODEV;
+		goto p_err;
+	}
+
+	dsi_i2c_rsc.gpio = pdata->pd_gpio;
+
+p_err:
+	return ret;
+}
+
+static int __devexit mipi_dsi_i2c_remove(struct i2c_client *client)
+{
+	return 0;
+}
+
+static struct i2c_driver mipi_dsi_i2c_driver = {
+	.driver = {
+		.name = "mipi_dsi_i2c",
+		.owner = THIS_MODULE,
+	},
+	.probe = mipi_dsi_i2c_probe,
+	.remove = mipi_dsi_i2c_remove,
+	.id_table = mipi_dsi_i2c_id,
+};
+
+static int mipi_dsi_i2c_lcd_on(struct platform_device *pdev)
+{
+	struct mipi_dsi_i2c_configure *cfg = &dsi_i2c_cfg;
+
+	if (cfg->config_dsi)
+		if (cfg->config_dsi())
+			pr_err("mipi dsi i2c config dsi fails!\n");
+
+	if (cfg->config_i2c)
+		if (cfg->config_i2c())
+			pr_err("mipi dsi i2c config i2c fails!\n");
+
+	return 0;
+}
+
+static int mipi_dsi_i2c_lcd_off(struct platform_device *pdev)
+{
+	return 0;
+}
+
+static int mipi_dsi_i2c_lcd_late_init(struct platform_device *pdev)
+{
+	return 0;
+}
+
+static int mipi_dsi_i2c_lcd_early_off(struct platform_device *pdev)
+{
+	return 0;
+}
+
+static void mipi_dsi_i2c_set_backlight(struct msm_fb_data_type *mfd)
+{
+	return;
+}
+
+static struct msm_fb_panel_data mipi_dsi_i2c_panel_data = {
+	.on             = mipi_dsi_i2c_lcd_on,
+	.off            = mipi_dsi_i2c_lcd_off,
+	.late_init      = mipi_dsi_i2c_lcd_late_init,
+	.early_off      = mipi_dsi_i2c_lcd_early_off,
+	.set_backlight  = mipi_dsi_i2c_set_backlight,
+};
+
+static int __devinit mipi_dsi_i2c_lcd_probe(struct platform_device *pdev)
+{
+	msm_fb_add_device(pdev);
+
+	return 0;
+}
+
+static struct platform_driver this_driver = {
+	.probe  = mipi_dsi_i2c_lcd_probe,
+	.driver = {
+		.name   = "mipi_dsi_i2c_lcd",
+	},
+};
+
+int mipi_dsi_i2c_device_register(struct msm_panel_info *pinfo)
+{
+	struct platform_device *pdev = NULL;
+	int ret = 0;
+
+	ret = platform_driver_register(&this_driver);
+	if (ret)
+		goto reg_err;
+
+	pdev = platform_device_alloc("mipi_dsi_i2c_lcd",
+		((pinfo->pdest + 1) << 8));
+	if (IS_ERR_OR_NULL(pdev)) {
+		ret = -ENOMEM;
+		goto reg_err;
+	}
+
+	mipi_dsi_i2c_panel_data.panel_info = *pinfo;
+
+	ret = platform_device_add_data(pdev, &mipi_dsi_i2c_panel_data,
+		sizeof(mipi_dsi_i2c_panel_data));
+	if (ret) {
+		pr_err("%s: platform_device_add_data failed!\n", __func__);
+		goto reg_err;
+	}
+
+	ret = platform_device_add(pdev);
+	if (ret) {
+		pr_err("%s: platform_device_register failed!\n", __func__);
+		goto reg_err;
+	}
+
+	return ret;
+
+reg_err:
+	platform_device_put(pdev);
+
+	return ret;
+}
+
+static int __init mipi_dsi_i2c_msm_init(void)
+{
+	return i2c_add_driver(&mipi_dsi_i2c_driver);
+}
+
+static void __exit mipi_dsi_i2c_msm_exit(void)
+{
+	i2c_del_driver(&mipi_dsi_i2c_driver);
+}
+
+module_init(mipi_dsi_i2c_msm_init);
+module_exit(mipi_dsi_i2c_msm_exit);
+MODULE_LICENSE("GPL v2");
+MODULE_DESCRIPTION("MIPI DSI VIDEO I2C driver");
---
+++ drivers/video/msm/mipi_dsi_i2c.h
@@ -0,0 +1,46 @@
+/* Copyright (c) 2014, The Linux Foundation. All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 and
+ * only version 2 as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ */
+
+#ifndef __MIPI_DSI_I2C_H__
+#define __MIPI_DSI_I2C_H__
+
+struct mipi_dsi_i2c_reg_cfg {
+	u8 i2c_addr;
+	u8 reg;
+	u8 val;
+};
+
+/*
+ * client may use this info to implement its own i2c read and write
+ */
+struct mipi_dsi_i2c_resource {
+	struct i2c_client *client;
+	int gpio;
+};
+
+/*
+ * two hook functions for the specific setup
+ */
+struct mipi_dsi_i2c_configure {
+	int (*config_dsi)(void);
+	int (*config_i2c)(void);
+};
+
+extern struct mipi_dsi_i2c_resource dsi_i2c_rsc;
+extern struct mipi_dsi_i2c_configure dsi_i2c_cfg;
+
+int mipi_dsi_i2c_read_byte(u8 addr, u8 reg, u8 *buf);
+int mipi_dsi_i2c_write_regs(struct mipi_dsi_i2c_reg_cfg *cfg, int size);
+int mipi_dsi_i2c_device_register(struct msm_panel_info *pinfo);
+
+#endif /* __MIPI_DSI_I2C_H__ */
