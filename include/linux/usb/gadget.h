52b24e1973917a5a3db49738319b5422d55562c1
Bar Weiner <bweiner@codeaurora.org>
2014-05-13 15:45:00 +0300

    usb: dwc3: gadget: allow running wakeup in delayed context
    Adding the option of running the gadget wakeup function
    from the context in which it is called, or delaying it - in case
    it is called from an atomic context. This is required in order
    to support a case where the gadget is in lpm and the wakeup
    would result in a queue to the low-powered hardware.
    
    Change-Id: I09fc60e75c79202c97251146aab1539de3307424
    Signed-off-by: Bar Weiner <bweiner@codeaurora.org>

--- include/linux/usb/gadget.h
+++ include/linux/usb/gadget.h
@@ -566,6 +566,7 @@ struct usb_gadget {
 	unsigned			a_hnp_support:1;
 	unsigned			a_alt_hnp_support:1;
 	unsigned			quirk_ep_out_aligned_size:1;
+	void				*private;
 };
 #define work_to_gadget(w)	(container_of((w), struct usb_gadget, work))
 
