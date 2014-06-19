3f4ebe17d8ca797d51f8584e5d40daeae6e2d277
Vamsi Krishna <vskrishn@codeaurora.org>
2014-06-19 10:14:03 -0700

    dwc3: gadget: Add support for no interrupt flag for sg reqs
    Currently controller driver does not honor the no interrupt
    flag from function driver and always sets LST (xfer complete) bit
    on last request. High throughput functions like rndis/rmnet can
    queue multiple requests and in some scenario endup with large
    number requests pending with h/w. They will only be recycled after
    hw completes the last request. Add support for no interrupt flag
    flag for sg (scatter/gather) requests so that application driver
    can decide when to generate interrupt. Controller will generate
    xfer-in-progress event when ioc is set.
    
    Change-Id: If02eb8602cafdbabf4bbe7f829711e9aaa304035
    Signed-off-by: Vamsi Krishna <vskrishn@codeaurora.org>

--- drivers/usb/dwc3/gadget.c
+++ drivers/usb/dwc3/gadget.c
@@ -510,7 +510,7 @@ static int dwc3_gadget_set_ep_config(struct dwc3 *dwc, struct dwc3_ep *dep,
 		dep->stream_capable = true;
 	}
 
-	if (usb_endpoint_xfer_isoc(desc))
+	if (!usb_endpoint_xfer_control(desc))
 		params.param1 |= DWC3_DEPCFG_XFER_IN_PROGRESS_EN;
 
 	/*
@@ -802,7 +802,8 @@ static void dwc3_gadget_ep_free_request(struct usb_ep *ep,
  */
 static void dwc3_prepare_one_trb(struct dwc3_ep *dep,
 		struct dwc3_request *req, dma_addr_t dma,
-		unsigned length, unsigned last, unsigned chain, unsigned node)
+		unsigned length, unsigned last, unsigned chain, unsigned node,
+		unsigned ioc)
 {
 	struct dwc3		*dwc = dep->dwc;
 	struct dwc3_trb		*trb;
@@ -853,6 +854,8 @@ update_trb:
 	case USB_ENDPOINT_XFER_BULK:
 	case USB_ENDPOINT_XFER_INT:
 		trb->ctrl = DWC3_TRBCTL_NORMAL;
+		if (ioc)
+			trb->ctrl |= DWC3_TRB_CTRL_IOC;
 		break;
 	default:
 		/*
@@ -983,6 +986,7 @@ static void dwc3_prepare_trbs(struct dwc3_ep *dep, bool starting)
 
 			for_each_sg(sg, s, request->num_mapped_sgs, i) {
 				unsigned chain = true;
+				unsigned	ioc = 0;
 
 				length = sg_dma_len(s);
 				dma = sg_dma_address(s);
@@ -1025,6 +1029,7 @@ static void dwc3_prepare_trbs(struct dwc3_ep *dep, bool starting)
 
 					if (trbs_left <= temp)
 						last_one = true;
+
 				}
 
 start_trb_queuing:
@@ -1035,8 +1040,12 @@ start_trb_queuing:
 				if (last_one)
 					chain = false;
 
+				if (!last_one && !chain &&
+					!request->no_interrupt)
+					ioc = 1;
+
 				dwc3_prepare_one_trb(dep, req, dma, length,
-						last_one, chain, i);
+						last_one, chain, i, ioc);
 
 				if (last_one)
 					break;
@@ -1070,7 +1079,7 @@ start_trb_queuing:
 				last_one = 1;
 
 			dwc3_prepare_one_trb(dep, req, dma, length,
-					last_one, false, 0);
+					last_one, false, 0, 0);
 
 			dbg_queue(dep->number, &req->request, 0);
 			if (last_one)
@@ -2480,12 +2489,6 @@ static void dwc3_endpoint_interrupt(struct dwc3 *dwc,
 		break;
 	case DWC3_DEPEVT_XFERINPROGRESS:
 		dep->dbg_ep_events.xferinprogress++;
-		if (!usb_endpoint_xfer_isoc(dep->endpoint.desc)) {
-			dev_dbg(dwc->dev, "%s is not an Isochronous endpoint\n",
-					dep->name);
-			return;
-		}
-
 		dwc3_endpoint_transfer_complete(dwc, dep, event, 0);
 		break;
 	case DWC3_DEPEVT_XFERNOTREADY:
