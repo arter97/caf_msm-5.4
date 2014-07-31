5874a98b18df62d609ee8f65d843592f922ff9d8
Mayank Rana <mrana@codeaurora.org>
2014-07-30 17:26:13 -0700

    usb: dwc3-msm: Add support to mark vbus always present
    This change adds devicetree flag to indicate as VBUS is always
    present. It also uses the setting to force BSV to 1.
    
    Change-Id: I01c201bf7615654355ad75bef0fe09c75d942dec
    Signed-off-by: Mayank Rana <mrana@codeaurora.org>

--- drivers/usb/dwc3/dwc3-msm.c
+++ drivers/usb/dwc3/dwc3-msm.c
@@ -1,4 +1,4 @@
-/* Copyright (c) 2012-2014, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2012-2015, The Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -3178,6 +3178,8 @@ static int dwc3_msm_probe(struct platform_device *pdev)
 		dev_err(&pdev->dev, "Failed to get dwc3 device\n");
 		goto put_dwc3;
 	}
+
+	dwc->vbus_active = of_property_read_bool(node, "qcom,vbus-present");
 
 	if (dwc->dotg)
 		mdwc->otg_xceiv = dwc->dotg->otg.phy;
