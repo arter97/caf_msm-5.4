e3c51aa9dd24bb939a41eeacf86676afa780d50c
Yaniv Gardi <ygardi@codeaurora.org>
2014-08-03 15:58:02 +0300

    scsi: ufs: remove a redundant call of ufshcd_release()
    In this patch a redundant call of ufshcd_release() is removed
    after being called twice.
    
    Change-Id: I28fc5b1525c3a598ec7402fdb8c698344bcb69a0
    Signed-off-by: Yaniv Gardi <ygardi@codeaurora.org>

--- drivers/scsi/ufs/ufshcd.c
+++ drivers/scsi/ufs/ufshcd.c
@@ -4440,11 +4440,7 @@ static int ufshcd_abort(struct scsi_cmnd *cmd)
 
 	clear_bit_unlock(tag, &hba->lrb_in_use);
 	wake_up(&hba->dev_cmd.tag_wq);
-	/*
-	 * This ufshcd_release() corresponds to the original scsi cmd that got
-	 * aborted here (as we won't get any IRQ for it).
-	 */
-	ufshcd_release(hba);
+
 out:
 	if (!err) {
 		err = SUCCESS;
@@ -4452,6 +4448,11 @@ out:
 		dev_err(hba->dev, "%s: failed with err %d\n", __func__, err);
 		err = FAILED;
 	}
+
+	/*
+	 * This ufshcd_release() corresponds to the original scsi cmd that got
+	 * aborted here (as we won't get any IRQ for it).
+	 */
 	ufshcd_release(hba);
 	return err;
 }
