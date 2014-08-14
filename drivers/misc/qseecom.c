fe4af4d06cebfa36cdd160365eca973b290e8ef8
William Clark <wclark@codeaurora.org>
2014-08-13 17:17:21 -0700

    qseecom: fix secure app loading failure by kernel client -- part 2
    Make a change to remove the kzfree in __qseecom_enable_clk_scale_up();
    otherwise, if __qseecom_enable_clk_scale_up failed, kzfree(img_data)
    will be called again and cause NULL pointer dereference.
    
    CRs-fixed: 709819
    
    Change-Id: Ia7d6d623349abbe2d0c0b38df874544f9b7d6d8c
    Signed-off-by: William Clark <wclark@codeaurora.org>

--- drivers/misc/qseecom.c
+++ drivers/misc/qseecom.c
@@ -1787,7 +1787,6 @@ static int __qseecom_load_fw(struct qseecom_dev_handle *data, char *appname)
 
 	ret = __qseecom_enable_clk_scale_up(data);
 	if (ret) {
-		kzfree(img_data);
 		ret = -EIO;
 		goto loadfw_err;
 	}
