e7be2f499d8964d001320295029c38597d0c5faf
Beeresh Gopal <gbeeresh@codeaurora.org>
2014-08-21 15:29:54 +0530

    msm: mdss: Silence warning message on enabling DRM driver
    On enabling DRM driver compilation fails due to warning
    message. This patch fixes the cause for warning.
    
    Change-Id: Iffc881b9bbcca15a5e4934750a00648d08d6d588
    Signed-off-by: Beeresh Gopal <gbeeresh@codeaurora.org>

--- drivers/video/msm/mdss/mdss_mdp_util.c
+++ drivers/video/msm/mdss/mdss_mdp_util.c
@@ -549,7 +549,7 @@ int mdss_mdp_get_img(struct msmfb_data *img, struct mdss_mdp_img_data *data)
 	if (img->flags & MDP_BLIT_SRC_GEM) {
 		data->srcp_file = NULL;
 		ret = kgsl_gem_obj_addr(img->memory_id, (int) img->priv,
-					start, len);
+					(unsigned long *)start, len);
 	} else if (img->flags & MDP_MEMORY_ID_TYPE_FB) {
 		file = fget_light(img->memory_id, &data->p_need);
 		if (file == NULL) {
