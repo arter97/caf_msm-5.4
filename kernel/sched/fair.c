39af0aee44282e97fa6c144f003a2695ba4d6531
Srivatsa Vaddagiri <vatsa@codeaurora.org>
2014-08-25 10:36:57 +0530

    sched: Initialize env->loop variable to 0
    load_balance() function does not explicitly initialize env->loop
    variable to 0. As a result, there is a vague possibility of
    move_tasks() hitting a very long (unnecessary) loop when its unable to
    move tasks from src_cpu. This can lead to unpleasant results like a
    watchdog bark. Fix this by explicitly initializing env->loop variable
    to 0 (in both load_balance() and active_load_balance_cpu_stop()).
    
    Change-Id: I36b84c91a9753870fa16ef9c9339db7b706527be
    Signed-off-by: Srivatsa Vaddagiri <vatsa@codeaurora.org>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -7417,6 +7417,7 @@ static int load_balance(int this_cpu, struct rq *this_rq,
 		.fbq_type	= all,
 		.imbalance	= 0,
 		.flags		= 0,
+		.loop		= 0,
 	};
 
 	/*
@@ -7786,6 +7787,7 @@ static int active_load_balance_cpu_stop(void *data)
 		.src_rq		= busiest_rq,
 		.idle		= CPU_IDLE,
 		.flags		= 0,
+		.loop		= 0,
 	};
 
 	raw_spin_lock_irq(&busiest_rq->lock);
