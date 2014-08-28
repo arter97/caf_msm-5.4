93f844e0614f908582fd4c6e3d5b114533ff50a7
Josh Cartwright <joshc@codeaurora.org>
2014-08-28 10:32:50 -0500

    ARM: msm: add support for the apq8084
    APQ8084 board files as of commit
    f351234d05725e0cbc9a863177fad661dc296d22 (Merge "ARM: dts: msm: Add
    clock node for root enable")
    
    [ updated clk to ]
    acdce027751d5a7488b283f0ce3111f873a5816d (Merge "defconfig: arm64: Enable
    ONESHOT_SYNC for msm8994")
    
    Also, rename serial clocks to allow for the use of either the upstream
    or downstream serial driver.
    
    Signed-off-by: Josh Cartwright <joshc@codeaurora.org>

---
+++ arch/arm/mach-msm/board-8084.c
@@ -0,0 +1,79 @@
+/* Copyright (c) 2013-2014, The Linux Foundation. All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 and
+ * only version 2 as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+#include <linux/err.h>
+#include <linux/kernel.h>
+#include <linux/of.h>
+#include <linux/of_address.h>
+#include <linux/of_platform.h>
+#include <linux/regulator/rpm-smd-regulator.h>
+#include <asm/mach/map.h>
+#include <asm/mach/arch.h>
+#include <soc/qcom/socinfo.h>
+#include <soc/qcom/rpm-smd.h>
+#include <soc/qcom/smd.h>
+#include <soc/qcom/smem.h>
+#include "board-dt.h"
+#include "clock.h"
+
+static struct of_dev_auxdata apq8084_auxdata_lookup[] __initdata = {
+	OF_DEV_AUXDATA("qca,qca1530", 0x00000000, "qca1530.1", NULL),
+	OF_DEV_AUXDATA("qcom,xhci-msm-hsic", 0xf9c00000, "msm_hsic_host", NULL),
+	OF_DEV_AUXDATA("qcom,msm_pcie", 0xFC520000, "msm_pcie.1", NULL),
+	OF_DEV_AUXDATA("qcom,msm_pcie", 0xFC528000, "msm_pcie.2", NULL),
+	{}
+};
+
+/*
+ * Used to satisfy dependencies for devices that need to be
+ * run early or in a particular order. Most likely your device doesn't fall
+ * into this category, and thus the driver should not be added here. The
+ * EPROBE_DEFER can satisfy most dependency problems.
+ */
+void __init apq8084_add_drivers(void)
+{
+	msm_smd_init();
+	msm_rpm_driver_init();
+	rpm_smd_regulator_driver_init();
+	msm_gcc_8084_init();
+}
+
+void __init apq8084_init(void)
+{
+	struct of_dev_auxdata *adata = apq8084_auxdata_lookup;
+
+	/*
+	 * populate devices from DT first so smem probe will get called as part
+	 * of msm_smem_init.  socinfo_init needs smem support so call
+	 * msm_smem_init before it.  apq8084_init_gpiomux needs socinfo so
+	 * call socinfo_init before it.
+	 */
+	board_dt_populate(adata);
+
+	msm_smem_init();
+
+	if (socinfo_init() < 0)
+		pr_err("%s: socinfo_init() failed\n", __func__);
+
+	apq8084_add_drivers();
+}
+
+static const char *apq8084_dt_match[] __initconst = {
+	"qcom,apq8084",
+	NULL
+};
+
+DT_MACHINE_START(APQ8084_DT,
+		"Qualcomm Technologies, Inc. APQ 8084 (Flattened Device Tree)")
+	.init_machine		= apq8084_init,
+	.dt_compat		= apq8084_dt_match,
+MACHINE_END
