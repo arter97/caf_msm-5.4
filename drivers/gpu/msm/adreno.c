RonGokhale/codeaurora_kernel

msm: kgsl: Clean-up gpuaddr allocated from kgsl gen_pool.
All GPU Mapping which are done using kgsl_mmu_map_global
API, allocates gpu address from kgsl gen_pool. Only calling
kgsl_mmu_unmap for such mappings doesn't free up the gpuaddr
assigned from KGSL gen_pool. This results in Kernel bug when
KGSL gen_pool is destroyed. This changes makes sure that
all KGSL gen_pool GPU address are freed before destroying
kgsl gen_pool.

CRs-Fixed: 523042
Signed-off-by: Deepak Kumar <dkumar@codeaurora.org>
Change-Id: Iba46f18e7fe4fba661f5fe142f3470f61a31baa0

drivers/gpu/msm/adreno.c
@@ -556,12 +556,16 @@ static void adreno_cleanup_pt(struct kgsl_device *device,
 	struct adreno_ringbuffer *rb = &adreno_dev->ringbuffer;
 
 	kgsl_mmu_unmap(pagetable, &rb->buffer_desc);
+	kgsl_mmu_put_gpuaddr(pagetable, &rb->buffer_desc);
 
 	kgsl_mmu_unmap(pagetable, &rb->memptrs_desc);
+	kgsl_mmu_put_gpuaddr(pagetable, &rb->memptrs_desc);
 
 	kgsl_mmu_unmap(pagetable, &device->memstore);
+	kgsl_mmu_put_gpuaddr(pagetable, &device->memstore);
 
 	kgsl_mmu_unmap(pagetable, &device->mmu.setstate_memory);
+	kgsl_mmu_put_gpuaddr(pagetable, &device->mmu.setstate_memory);
 }
 
 static int adreno_setup_pt(struct kgsl_device *device,
@@ -598,12 +602,15 @@ static int adreno_setup_pt(struct kgsl_device *device,
 
 unmap_memstore_desc:
 	kgsl_mmu_unmap(pagetable, &device->memstore);
+	kgsl_mmu_put_gpuaddr(pagetable, &device->memstore);
 
 unmap_memptrs_desc:
 	kgsl_mmu_unmap(pagetable, &rb->memptrs_desc);
+	kgsl_mmu_put_gpuaddr(pagetable, &rb->memptrs_desc);
 
 unmap_buffer_desc:
 	kgsl_mmu_unmap(pagetable, &rb->buffer_desc);
+	kgsl_mmu_put_gpuaddr(pagetable, &rb->buffer_desc);
 
 error:
 	return result;

arter97/caf_msm-5.4

Conflicts:
        drivers/gpu/msm/adreno.c
        drivers/gpu/msm/z180.c

drivers/gpu/msm/adreno.c
@@ -782,14 +782,19 @@ static void adreno_cleanup_pt(struct kgsl_device *device,
 	struct adreno_ringbuffer *rb = &adreno_dev->ringbuffer;
 
 	kgsl_mmu_unmap(pagetable, &rb->buffer_desc);
+	kgsl_mmu_put_gpuaddr(pagetable, &rb->buffer_desc);
 
 	kgsl_mmu_unmap(pagetable, &device->memstore);
+	kgsl_mmu_put_gpuaddr(pagetable, &device->memstore);
 
 	kgsl_mmu_unmap(pagetable, &adreno_dev->pwron_fixup);
+	kgsl_mmu_put_gpuaddr(pagetable, &adreno_dev->pwron_fixup);
 
 	kgsl_mmu_unmap(pagetable, &device->mmu.setstate_memory);
+	kgsl_mmu_put_gpuaddr(pagetable, &device->mmu.setstate_memory);
 
 	kgsl_mmu_unmap(pagetable, &adreno_dev->profile.shared_buffer);
+	kgsl_mmu_put_gpuaddr(pagetable, &adreno_dev->profile.shared_buffer);
 }
 
 static int adreno_setup_pt(struct kgsl_device *device,