cb4ff36ddee63f7bb55d443792b826363cab7748
Mayank Rana <mrana@codeaurora.org>
2014-09-13 19:46:52 -0700

    Revert "u_bam_data: Add check to perform teth_bridge connect and disconnect"
    This reverts commit 3065a0a5b95fe9b5e7afc3c22a3e60b77fd45938.
    
    Change-Id: Ie5ef8c1f4d9aca00d8db158a704f99c393018e7f
    Signed-off-by: Mayank Rana <mrana@codeaurora.org>

--- drivers/usb/gadget/u_bam_data.c
+++ drivers/usb/gadget/u_bam_data.c
@@ -111,7 +111,6 @@ struct bam_data_ch_info {
 	unsigned int		rx_flow_control_disable;
 	unsigned int		rx_flow_control_enable;
 	unsigned int		rx_flow_control_triggered;
-	atomic_t		is_teth_bridge_connected;
 };
 
 static struct work_struct *rndis_conn_w;
@@ -1104,7 +1103,6 @@ static void bam2bam_data_connect_work(struct work_struct *w)
 			}
 			is_ipa_rndis_net_on = true;
 		}
-		atomic_set(&d->is_teth_bridge_connected, 1);
 	} else { /* transport type is USB_GADGET_XPORT_BAM2BAM */
 		/* Upadate BAM specific attributes in usb_request */
 		usb_bam_reset_complete();
@@ -1409,31 +1407,21 @@ void bam_data_disconnect(struct data_port *gr, u8 port_num)
 		}
 	}
 
-	/*
-	 * Make sure to perform teth_bridge_disconnect() only if
-	 * teth_bridge_init() is performed for different USB functions.
-	 */
 	if (d->trans == USB_GADGET_XPORT_BAM2BAM_IPA) {
-		if (atomic_read(&d->is_teth_bridge_connected)) {
-			void *priv;
-
-			if (d->func_type == USB_FUNC_ECM) {
-				priv = ecm_qc_get_ipa_priv();
-				ecm_ipa_disconnect(priv);
-			} else if (d->func_type == USB_FUNC_RNDIS) {
-				priv = rndis_qc_get_ipa_priv();
-				rndis_ipa_pipe_disconnect_notify(priv);
-				is_ipa_rndis_net_on = false;
-			} else if (d->func_type == USB_FUNC_MBIM) {
-				teth_bridge_disconnect(
-						d->ipa_params.src_client);
-			}
-			pr_debug("%s(): teth_bridge() disconnected\n",
-								__func__);
-			atomic_set(&d->is_teth_bridge_connected, 0);
-			port->last_event = U_BAM_DATA_DISCONNECT_E;
-			queue_work(bam_data_wq, &port->disconnect_w);
+		void *priv;
+		if (d->func_type == USB_FUNC_ECM) {
+			priv = ecm_qc_get_ipa_priv();
+			ecm_ipa_disconnect(priv);
+		} else if (d->func_type == USB_FUNC_RNDIS) {
+			priv = rndis_qc_get_ipa_priv();
+			rndis_ipa_pipe_disconnect_notify(priv);
+			is_ipa_rndis_net_on = false;
+		} else if (d->func_type == USB_FUNC_MBIM) {
+			teth_bridge_disconnect(d->ipa_params.src_client);
 		}
+
+		port->last_event = U_BAM_DATA_DISCONNECT_E;
+		queue_work(bam_data_wq, &port->disconnect_w);
 	} else {
 		if (usb_bam_client_ready(false))
 			pr_err("%s: usb_bam_client_ready failed\n",
@@ -1546,7 +1534,6 @@ int bam_data_connect(struct data_port *gr, u8 port_num,
 			goto exit;
 	}
 
-	atomic_set(&d->is_teth_bridge_connected, 0);
 	port->last_event = U_BAM_DATA_CONNECT_E;
 	queue_work(bam_data_wq, &port->connect_w);
 	ret = 0;
