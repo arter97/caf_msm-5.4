a80bb66a8aa2b508f6c4d6aa332a8f73ddac61c7
Tony Truong <truong@codeaurora.org>
2014-09-30 15:09:33 -0700

    Revert "arm64: PCI(e) arch support"
    This reverts commit 32f20775e6400953915146d8888f096281f346f2.
    This change adds the arch support for PCI(e) for arm64 on 3.10
    kernel and is not compatible with the 3.14 kernel. The files
    added or modfied in this patch conflict with the ones for PCI(e)
    arm64 3.14 patch. Reverting this change resolves the conflicts
    when applying the arch support for PCI(e) for arm64 patch for
    3.14 kernel.
    
    Change-Id: Ibe7f304c7969d25cf84ab233d135a568bedd77d7
    Signed-off-by: Tony Truong <truong@codeaurora.org>

--- arch/arm64/include/asm/io.h
+++ arch/arm64/include/asm/io.h
@@ -209,29 +209,10 @@ static inline u64 __raw_readq_no_log(const volatile void __iomem *addr)
 #define writeq_no_log(v, c)		({ __iowmb(); writeq_relaxed_no_log((v), (c)); })
 
 /*
- * A typesafe __io() helper
- */
-static inline void __iomem *__typesafe_io(unsigned long addr)
-{
-	return (void __iomem *)addr;
-}
-
-/*
  *  I/O port access primitives.
  */
-#define PCI_IOBASE		((void __iomem *)(MODULES_VADDR - SZ_32M))
-
-#if defined(CONFIG_PCI)
-#define IO_SPACE_LIMIT  ((resource_size_t)0xffffffff)
-#define __io(a)         __typesafe_io((unsigned long)PCI_IOBASE + \
-				      ((a) & IO_SPACE_LIMIT))
-#else
 #define IO_SPACE_LIMIT		0xffff
-#define __io(a)         __typesafe_io((a) & IO_SPACE_LIMIT)
-#endif
-extern void __iomem *ioport_map(unsigned long port, unsigned int nr);
-extern void ioport_unmap(void __iomem *addr);
-extern int pci_ioremap_io(unsigned int offset, phys_addr_t phys_addr);
+#define PCI_IOBASE		((void __iomem *)(MODULES_VADDR - SZ_2M))
 
 static inline u8 inb(unsigned long addr)
 {
