29ea971a0521e09870c0e19085d444b0967dcb5e
Ravit Katzav <rkatzav@codeaurora.org>
2014-10-21 15:35:16 +0300

    msm: ipa: resume endpoint on connect fix
    On ipa connect endpoint should be unsuspended according  to the resource
    state. use the resume_on_connect flag for unsuspend EP on connect while
    its resource is granted.
    
    Change-Id: I531a8e987930e2b78d3a32eede5cfd2f52fc102b
    Signed-off-by: Ravit Katzav <rkatzav@codeaurora.org>

--- drivers/platform/msm/ipa/ipa_i.h
+++ drivers/platform/msm/ipa/ipa_i.h
@@ -406,7 +406,6 @@ struct ipa_ep_context {
 	u32 dflt_flt6_rule_hdl;
 	bool skip_ep_cfg;
 	bool keep_ipa_awake;
-	bool resume_on_connect;
 	struct ipa_wlan_stats wstats;
 
 	/* sys MUST be the last element of this struct */
@@ -657,6 +656,7 @@ struct ipa_sps_pm {
  * @ep: list of all end points
  * @skip_ep_cfg_shadow: state to update filter table correctly across
   power-save
+ * @resume_on_connect:resume ep on ipa_connect while resource is granted
  * @flt_tbl: list of all IPA filter tables
  * @mode: IPA operating mode
  * @mmio: iomem
@@ -718,6 +718,7 @@ struct ipa_context {
 	unsigned long bam_handle;
 	struct ipa_ep_context ep[IPA_NUM_PIPES];
 	bool skip_ep_cfg_shadow[IPA_NUM_PIPES];
+	bool resume_on_connect[IPA_CLIENT_MAX];
 	struct ipa_flt_tbl flt_tbl[IPA_NUM_PIPES][IPA_IP_MAX];
 	void __iomem *mmio;
 	u32 ipa_wrapper_base;
