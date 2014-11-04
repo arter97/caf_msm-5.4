485033885dd4c61f24ff63476992704ed4e6e0ac
Srivatsa Vaddagiri <vatsa@codeaurora.org>
2014-11-04 15:25:50 +0530

    sched: per-cpu mostly_idle threshold
    sched_mostly_idle_load and sched_mostly_idle_nr_run knobs help pack
    tasks on cpus to some extent. In some cases, it may be desirable to
    have different packing limits for different cpus. For example, pack to
    a higher limit on high-performance cpus compared to power-efficient
    cpus.
    
    This patch removes the global mostly_idle tunables and makes them
    per-cpu, thus letting task packing behavior to be controlled in a
    fine-grained manner.
    
    Change-Id: I9f521cd0ee1c29b729f056c140619c9682ba555c
    Signed-off-by: Srivatsa Vaddagiri <vatsa@codeaurora.org>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -1651,6 +1651,7 @@ void reset_all_window_stats(u64 window_start, unsigned int window_size,
 	int cpu;
 	u64 wallclock;
 	struct task_struct *g, *p;
+	unsigned int old_window_size = sched_ravg_window;
 
 	for_each_online_cpu(cpu) {
 		struct rq *rq = cpu_rq(cpu);
@@ -1682,8 +1683,13 @@ void reset_all_window_stats(u64 window_start, unsigned int window_size,
 	for_each_online_cpu(cpu) {
 		struct rq *rq = cpu_rq(cpu);
 
-		if (window_start)
+		if (window_start) {
+			u32 mostly_idle_load = rq->mostly_idle_load;
+
 			rq->window_start = window_start;
+			rq->mostly_idle_load = div64_u64((u64)mostly_idle_load *
+				 (u64)sched_ravg_window, (u64)old_window_size);
+		}
 		rq->curr_runnable_sum = rq->prev_runnable_sum = 0;
 		rq->cumulative_runnable_avg = 0;
 		fixup_nr_big_small_task(cpu);
@@ -8286,6 +8292,8 @@ void __init sched_init(void)
 #endif
 #ifdef CONFIG_SCHED_HMP
 		rq->nr_small_tasks = rq->nr_big_tasks = 0;
+		rq->mostly_idle_load = pct_to_real(20);
+		rq->mostly_idle_nr_run = 3;
 		rq->curr_runnable_sum = rq->prev_runnable_sum = 0;
 		rq->hmp_flags = 0;
 #endif
