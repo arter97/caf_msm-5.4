bfd19737af525a31ff693c31a7445b84dcaf4d98
Ramakant Singh <ramaka@codeaurora.org>
2014-11-10 23:11:31 +0530

    mdss: fb: fix ion fbmem release sequence
    In recovery mode and charger only mode, a race
    condition is sometimes seen in the shutdown sequence.
    This happens in the mdss_release where the ion buffers
    used for FB are freed before doing the BLANK which
    results in MDP accessing the buffers even after it
    is freed leading to iommu pagefaults.
    
    To resolve this, free the buffers only after the BLANK
    operation is completed.
    
    CRs-Fixed: 750233
    
    Change-Id: I347e98f933bfd9372ce45b007b8f737b31e8479d
    Signed-off-by: Ramakant Singh <ramaka@codeaurora.org>
    Signed-off-by: Nirmal Abraham <nabrah@codeaurora.org>

--- drivers/video/msm/mdss/mdss_fb.c
+++ drivers/video/msm/mdss/mdss_fb.c
@@ -2260,9 +2260,6 @@ static int mdss_fb_release_all(struct fb_info *info, bool release_all)
 				    mfd->index, task->comm, current->tgid, pid);
 		}
 
-		if (mfd->fb_ion_handle)
-			mdss_fb_free_fb_ion_memory(mfd);
-
 		ret = mdss_fb_blank_sub(FB_BLANK_POWERDOWN, info,
 			mfd->op_enable);
 		if (ret) {
@@ -2270,6 +2267,10 @@ static int mdss_fb_release_all(struct fb_info *info, bool release_all)
 			      mfd->index, ret, task->comm, current->tgid, pid);
 			return ret;
 		}
+
+		if (mfd->fb_ion_handle)
+			mdss_fb_free_fb_ion_memory(mfd);
+
 		atomic_set(&mfd->ioctl_ref_cnt, 0);
 	}
 
