be134ef73389a8e39ad4ede9e6d7b6ad3ab295b3
Ajay Singh Parmar <aparmar@codeaurora.org>
2014-11-14 00:45:14 -0800

    msm: mdss: hdmi: optimize resolution data base
    HDMI has a large number of resolutions supported and currently
    for all supported resolutions, related modules maintain static
    tables which are populated at boot time. This results in huge
    static memory usage. Also, it limits the system to support only
    the define resolutions.
    
    Remove static table which stores all the resolution details.
    Get individual resolution details on need basis. Also, remove
    the dependency to support only the defined resolution. HDMI driver
    can support any non standard resolution within the allowed range.
    
    Change-Id: I0972bc3a0ab96051ea642d685d10c4e5535b7051
    Signed-off-by: Ajay Singh Parmar <aparmar@codeaurora.org>

--- drivers/video/msm/mdss/mdss_hdmi_tx.h
+++ drivers/video/msm/mdss/mdss_hdmi_tx.h
@@ -89,11 +89,13 @@ struct hdmi_tx_ctrl {
 	u32 hpd_feature_on;
 	u32 hpd_initialized;
 	u32 vote_hdmi_core_on;
+	u32 res_info_id;
 	u8  timing_gen_on;
-	u32 mhl_max_pclk;
 	u8  mhl_hpd_on;
 	struct completion hpd_int_done;
 	struct completion hpd_off_done;
+
+	struct hdmi_util_ds_data ds_data;
 	struct work_struct hpd_int_work;
 
 	struct work_struct cable_notify_work;
