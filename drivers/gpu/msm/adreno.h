59269f1257edf13a6922edcd214e92794598feae
Tarun Karra <tkarra@codeaurora.org>
2014-12-02 18:09:59 -0800

    msm: kgsl: Set A530 gpulist patch ID to ANY_ID
    A530 gpulist patch ID currently is 0x1 but it should be ANY_ID
    to match all A530 revisions.
    
    Change-Id: Ia64d005b289bff45dadff231ee52df26435c5157
    Signed-off-by: Tarun Karra <tkarra@codeaurora.org>

--- drivers/gpu/msm/adreno.h
+++ drivers/gpu/msm/adreno.h
@@ -937,7 +937,7 @@ static inline int adreno_is_a530(struct adreno_device *adreno_dev)
 static inline int adreno_is_a530v1(struct adreno_device *adreno_dev)
 {
 	return (ADRENO_GPUREV(adreno_dev) == ADRENO_REV_A530) &&
-			(ADRENO_PATCHID(adreno_dev) == 1);
+		(ADRENO_CHIPID_PATCH(adreno_dev->chipid) == 0);
 }
 
 /**
