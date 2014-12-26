f955223b92030af4fb6cbba9d8ce91160f4ba493
Ajay Singh Parmar <aparmar@codeaurora.org>
2014-12-26 14:48:10 -0800

    msm: mdss: hdmi: give multiple resolution info using sysfs node
    Provide complete resolution details in a sysfs node "res_info"
    limited to PAGE_SIZE. Different modules can query for multiple
    resolution details based on the resolution ids received from
    EDID of the TV.
    In case resolution details exceed PAGE_SIZE, reuse res_info to
    get remaining timing details by provide page details.
    
    Change-Id: I3e8e8d4de29f78d22273b3fb8ff6a059a8cb19e1
    Signed-off-by: Ajay Singh Parmar <aparmar@codeaurora.org>

--- drivers/video/msm/mdss/mdss_hdmi_tx.c
+++ drivers/video/msm/mdss/mdss_hdmi_tx.c
@@ -1002,72 +1002,6 @@ static ssize_t hdmi_tx_sysfs_wta_avi_cn_bits(struct device *dev,
 	return ret;
 } /* hdmi_tx_sysfs_wta_cn_bits */
 
-static ssize_t hdmi_tx_sysfs_wta_res_info(struct device *dev,
-	struct device_attribute *attr, const char *buf, size_t count)
-{
-	int rc, res_info_id;
-	ssize_t ret = strnlen(buf, PAGE_SIZE);
-	struct hdmi_tx_ctrl *hdmi_ctrl = NULL;
-
-	hdmi_ctrl = hdmi_tx_get_drvdata_from_sysfs_dev(dev);
-
-	if (!hdmi_ctrl) {
-		DEV_ERR("%s: invalid input\n", __func__);
-		return -EINVAL;
-	}
-
-	rc = kstrtoint(buf, 10, &res_info_id);
-	if (rc) {
-		DEV_ERR("%s: kstrtoint failed. rc=%d\n", __func__, rc);
-		return rc;
-	}
-
-	if (res_info_id >= 0 && res_info_id < HDMI_VFRMT_MAX)
-		hdmi_ctrl->res_info_id = res_info_id;
-	else
-		hdmi_ctrl->res_info_id = 0;
-
-	DEV_DBG("%s: %d\n", __func__, hdmi_ctrl->res_info_id);
-	return ret;
-}
-
-static ssize_t hdmi_tx_sysfs_rda_res_info(struct device *dev,
-	struct device_attribute *attr, char *buf)
-{
-	ssize_t ret;
-	struct msm_hdmi_mode_timing_info info = {0};
-	struct hdmi_tx_ctrl *hdmi_ctrl =
-		hdmi_tx_get_drvdata_from_sysfs_dev(dev);
-	u32 size = sizeof(info) < PAGE_SIZE ? sizeof(info) : PAGE_SIZE;
-
-	if (!hdmi_ctrl) {
-		DEV_ERR("%s: invalid input\n", __func__);
-		return -EINVAL;
-	}
-
-	if (!hdmi_ctrl->res_info_id)
-		return -EINVAL;
-
-	ret = hdmi_get_supported_mode(&info, &hdmi_ctrl->ds_data,
-		hdmi_ctrl->res_info_id);
-
-	if (ret)
-		return -EINVAL;
-
-	memcpy(buf, &info, size);
-
-	DEV_DBG("%s: %d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d\n",
-		__func__,
-		info.video_format, info.active_h, info.front_porch_h,
-		info.pulse_width_h, info.back_porch_h, info.active_low_h,
-		info.active_v, info.front_porch_v, info.pulse_width_v,
-		info.back_porch_v, info.active_low_v, info.pixel_freq,
-		info.refresh_rate, info.interlaced, info.supported,
-		info.ar);
-
-	return size;
-}
-
 static ssize_t hdmi_tx_sysfs_wta_s3d_mode(struct device *dev,
 	struct device_attribute *attr, const char *buf, size_t count)
 {
@@ -1141,8 +1075,6 @@ static DEVICE_ATTR(avi_itc, S_IWUSR, NULL, hdmi_tx_sysfs_wta_avi_itc);
 static DEVICE_ATTR(avi_cn0_1, S_IWUSR, NULL, hdmi_tx_sysfs_wta_avi_cn_bits);
 static DEVICE_ATTR(s3d_mode, S_IRUGO | S_IWUSR, hdmi_tx_sysfs_rda_s3d_mode,
 	hdmi_tx_sysfs_wta_s3d_mode);
-static DEVICE_ATTR(res_info, S_IRUGO | S_IWUSR, hdmi_tx_sysfs_rda_res_info,
-	hdmi_tx_sysfs_wta_res_info);
 
 static struct attribute *hdmi_tx_fs_attrs[] = {
 	&dev_attr_connected.attr,
@@ -1152,7 +1084,6 @@ static struct attribute *hdmi_tx_fs_attrs[] = {
 	&dev_attr_product_description.attr,
 	&dev_attr_avi_itc.attr,
 	&dev_attr_avi_cn0_1.attr,
-	&dev_attr_res_info.attr,
 	&dev_attr_s3d_mode.attr,
 	NULL,
 };
