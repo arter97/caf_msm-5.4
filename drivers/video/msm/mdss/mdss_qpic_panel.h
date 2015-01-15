268bff730e714694eb4d37ac2488c3d47fbaa6b0
Ken Zhang <kenz@codeaurora.org>
2015-01-15 11:19:54 -0500

    mdm: display: qpic: continue splash screen
    Panel will be blank if qpic is reset and panel is
    turned off. Do not reset qpic or turn off panel if splash
    screen is detected.
    Enable tearing check by default.
    
    Change-Id: Ia2f9aa40b4055e54850b5c86652f7a3d96d465b2
    Signed-off-by: Ken Zhang <kenz@codeaurora.org>

--- drivers/video/msm/mdss/mdss_qpic_panel.h
+++ drivers/video/msm/mdss/mdss_qpic_panel.h
@@ -1,4 +1,4 @@
-/* Copyright (c) 2014, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2014 - 2015, The Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -118,6 +118,7 @@ struct qpic_panel_io_desc {
 	struct regulator *vdd_vreg;
 	struct regulator *avdd_vreg;
 	u32 init;
+	bool splash_screen_transition;
 };
 
 int mdss_qpic_panel_io_init(struct platform_device *pdev,
