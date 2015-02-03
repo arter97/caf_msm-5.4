5578c3f39325ac2b4b3b88b6680f181751cd9a43
Nadine Toledano <nadinet@codeaurora.org>
2015-02-03 12:24:02 +0200

    msm: ipa: add MHI support to IPA
    Add support for MHI (Modem Host Interface) for IPA to
    setup HW accelerated MHI channels using IPA uC.
    This change exposes new APIs for connecting and disconnecting new
    channels of MHI and for power management.
    
    Change-Id: I742c1c6b2f6e528bc47303cd10fc9eab59b7b2b0
    Acked-by: Ady Abraham <adya@qti.qualcomm.com>
    Signed-off-by: Nadine Toledano <nadinet@codeaurora.org>

--- drivers/platform/msm/ipa/ipa_qmi_service.h
+++ drivers/platform/msm/ipa/ipa_qmi_service.h
@@ -95,6 +95,12 @@ int qmi_filter_request_send(struct ipa_install_fltr_rule_req_msg_v01 *req);
 /* sending filter-installed-notify-request to modem*/
 int qmi_filter_notify_send(struct ipa_fltr_installed_notif_req_msg_v01 *req);
 
+int qmi_enable_force_clear_datapath_send(
+	struct ipa_enable_force_clear_datapath_req_msg_v01 *req);
+
+int qmi_disable_force_clear_datapath_send(
+	struct ipa_disable_force_clear_datapath_req_msg_v01 *req);
+
 int copy_ul_filter_rule_to_ipa(struct ipa_install_fltr_rule_req_msg_v01
 	*rule_req, uint32_t *rule_hdl);
 
@@ -130,6 +136,18 @@ static inline int qmi_filter_request_send(
 /* sending filter-installed-notify-request to modem*/
 static inline int qmi_filter_notify_send(
 	struct ipa_fltr_installed_notif_req_msg_v01 *req)
+{
+	return -EPERM;
+}
+
+static inline int qmi_enable_force_clear_datapath_send(
+	struct ipa_enable_force_clear_datapath_req_msg_v01 *req)
+{
+	return -EPERM;
+}
+
+static inline int qmi_disable_force_clear_datapath_send(
+	struct ipa_disable_force_clear_datapath_req_msg_v01 *req)
 {
 	return -EPERM;
 }
