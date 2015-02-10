5d34a8235d1d22739bf027ef8a00576234f70e38
Sivan Reinstein <sivanr@codeaurora.org>
2015-02-10 17:20:25 +0200

    msm: ipa: add a check for header len when adding a header to ipa
    The procedure which adds a header to the IPA was missing a boundary
    check, causing a potential buffer overflow.
    
    CRs-fixed: 791163
    Change-Id: I4c372369feba1df68364a7c7b406e735147932d7
    Acked-by: David Arinzon <darinzon@qti.qualcomm.com>
    Signed-off-by: Sivan Reinstein <sivanr@codeaurora.org>

--- drivers/platform/msm/ipa/ipa_hdr.c
+++ drivers/platform/msm/ipa/ipa_hdr.c
@@ -1,4 +1,4 @@
-/* Copyright (c) 2012-2014, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2012-2015, The Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -222,7 +222,7 @@ static int __ipa_add_hdr(struct ipa_hdr_add *hdr)
 	struct ipa_hdr_tbl *htbl = &ipa_ctx->hdr_tbl;
 	int id;
 
-	if (hdr->hdr_len == 0) {
+	if (hdr->hdr_len == 0 || hdr->hdr_len > IPA_HDR_MAX_SIZE) {
 		IPAERR("bad parm\n");
 		goto error;
 	}
