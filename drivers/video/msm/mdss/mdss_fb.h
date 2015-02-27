26c78a187b7accead26e4f365a9c6143b99a4ca7
Nitesh Gupta <niteshg@codeaurora.org>
2015-02-27 18:18:31 +0530

    msm: mdss: Fix for display Post-Processing threads to halt faster
    Change notify timer from 2 sec to 0.1 sec so that display Post-
    Processing threads halt faster after convergence.
    
    Change-Id: Ibec9d0d0e2d69810c450691f00fa53ad88a59127
    Signed-off-by: Nitesh Gupta <niteshg@codeaurora.org>

--- drivers/video/msm/mdss/mdss_fb.h
+++ drivers/video/msm/mdss/mdss_fb.h
@@ -1,4 +1,4 @@
-/* Copyright (c) 2008-2014, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2008-2015, The Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -270,7 +270,7 @@ static inline void mdss_fb_update_notify_update(struct msm_fb_data_type *mfd)
 		if (mfd->no_update.timer.function)
 			del_timer(&(mfd->no_update.timer));
 
-		mfd->no_update.timer.expires = jiffies + (2 * HZ);
+		mfd->no_update.timer.expires = jiffies + ((1 * HZ) / 10);
 		add_timer(&mfd->no_update.timer);
 		mutex_unlock(&mfd->no_update.lock);
 	}
