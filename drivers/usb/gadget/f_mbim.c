d42a98c6d1e92ea3c4a6834dcaf909a86efcbc49
Hemant Kumar <hemantk@codeaurora.org>
2015-02-26 20:05:46 -0800

    usb: gadget: f_mbim: Update the NTB size for IN and OUT dir
    Change data packet aggregation size for IN and OUT direction
    from 8K to 16K. This improves UDP uplink and downlink
    throughput in High speed mode.
    
    CRs-Fixed: 797021
    Change-Id: Ib864ebcda7452570e8df217e29ec8b75e490e926
    Signed-off-by: Hemant Kumar <hemantk@codeaurora.org>

--- drivers/usb/gadget/f_mbim.c
+++ drivers/usb/gadget/f_mbim.c
@@ -175,8 +175,8 @@ static inline unsigned mbim_bitrate(struct usb_gadget *g)
 #define MBIM_NTB_OUT_SIZE		(0x1000)
 #define MBIM_NDP_IN_DIVISOR		(0x4)
 
-#define NTB_DEFAULT_IN_SIZE_IPA	(0x2000)
-#define MBIM_NTB_OUT_SIZE_IPA		(0x2000)
+#define NTB_DEFAULT_IN_SIZE_IPA	(0x4000)
+#define MBIM_NTB_OUT_SIZE_IPA		(0x4000)
 
 #define MBIM_FORMATS_SUPPORTED	USB_CDC_NCM_NTB16_SUPPORTED
 static int mbim_ntb_out_size_sys2bam;
