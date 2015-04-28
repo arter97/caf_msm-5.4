c1f2554cd9891cbe64300de8e4ef40199cb716e4
Casey Piper <cpiper@codeaurora.org>
2015-04-28 15:23:18 -0700

    msm: mdss: hdmi: determine where HDCP keys are stored
    Determine if the HDCP keys are stored in software
    registers or hardware fuses on supported hardware.
    
    Change-Id: I9492a458c38aaea4303d3387d79c01bb9beb3219
    Signed-off-by: Casey Piper <cpiper@codeaurora.org>
    Signed-off-by: Mitchel Humpherys <mitchelh@codeaurora.org>
    Signed-off-by: Shruthi Krishna <skrish@codeaurora.org>

--- drivers/video/msm/mdss/mdss_hdmi_tx.c
+++ drivers/video/msm/mdss/mdss_hdmi_tx.c
@@ -1429,6 +1429,13 @@ static int hdmi_tx_check_capability(struct hdmi_tx_ctrl *hdmi_ctrl)
 			QFPROM_RAW_FEAT_CONFIG_ROW0_LSB + QFPROM_RAW_VERSION_4);
 		hdcp_disabled = reg_val & BIT(12);
 		hdmi_disabled = reg_val & BIT(13);
+		reg_val = DSS_REG_R_ND(io, SEC_CTRL_HW_VERSION);
+		if (!hdcp_disabled && (reg_val >= HDCP_SEL_MIN_SEC_VERSION)) {
+			reg_val = DSS_REG_R_ND(io,
+				QFPROM_RAW_FEAT_CONFIG_ROW0_MSB +
+				QFPROM_RAW_VERSION_4);
+			hdcp_disabled = !(reg_val & BIT(23));
+		}
 	}
 
 	DEV_DBG("%s: Features <HDMI:%s, HDCP:%s>\n", __func__,
