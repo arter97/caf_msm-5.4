5c337576ab9ae37fd237fca5a5d2eaf86f71dedb
Ravi Aravamudhan <aravamud@codeaurora.org>
2015-06-11 18:34:07 -0700

    diag: Make fixes in memory device mode
    When there are multiple clients voting for memory device mode, diag
    driver is incorrectly picking the client for draining data. Also,
    memory device client that is currently receiving data won't receive
    data when a new memory device client comes up. This patch addresses
    these issues.
    
    Change-Id: I6c2b3f46a1ade60c58db9c35b551f490268cfbee
    Signed-off-by: Ravi Aravamudhan <aravamud@codeaurora.org>

--- drivers/char/diag/diagchar.h
+++ drivers/char/diag/diagchar.h
@@ -373,6 +373,7 @@ struct diag_md_proc_info {
 	int pid;
 	struct task_struct *socket_process;
 	struct task_struct *callback_process;
+	struct task_struct *mdlog_process;
 };
 
 struct diag_feature_t {
@@ -386,13 +387,6 @@ struct diag_feature_t {
 	uint8_t stm_support;
 	uint8_t sockets_enabled;
 	uint8_t sent_feature_mask;
-};
-
-struct diag_mdlog_client_info {
-	struct task_struct *client_process;
-	int client_id;
-	uint16_t notification_list;
-	int signal_type;
 };
 
 struct diagchar_dev {
@@ -506,7 +500,6 @@ struct diagchar_dev {
 	int logging_mode;
 	int mask_check;
 	struct diag_md_proc_info md_proc[DIAG_NUM_PROC];
-	struct diag_mdlog_client_info md_client_info;
 	/* Power related variables */
 	struct diag_ws_ref_t dci_ws;
 	struct diag_ws_ref_t md_ws;
