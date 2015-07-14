20a548ebb5025f637348cc7b68937d87cb447507
Sujeet Kumar <ksujeet@codeaurora.org>
2015-07-14 09:13:33 +0530

    USB: android: Initialize only serial boot ports
    If the usb composition switch is enabling higher
    serial interfaces than earlier then adding the second
    instance would cause target to crash because the
    data structures would not be valid.
    
    Enabling higher serial interface like switching usb
    composition from DUN to DUN,NMEA adds the second
    instance NMEA but it is not initialized earlier
    as the initialization is allowed only once during the
    bind config. Limit the serial ports init only for boot
    ports and allow only those instances to be added.
    
    Change-Id: I892b3d850dc2e70f98444d7245860dac11ed1030
    Signed-off-by: Sujeet Kumar <ksujeet@codeaurora.org>
    Signed-off-by: ChandanaKishori Chiluveru <cchilu@codeaurora.org>

--- drivers/usb/gadget/android.c
+++ drivers/usb/gadget/android.c
@@ -1791,6 +1791,7 @@ static int serial_function_bind_config(struct android_usb_function *f,
 							name);
 					goto out;
 				}
+				config->instances_on++;
 			}
 			ports++;
 			if (ports >= MAX_SERIAL_INSTANCES) {
@@ -1800,7 +1801,9 @@ static int serial_function_bind_config(struct android_usb_function *f,
 		}
 	}
 
-	config->instances_on = ports;
+	/* limit the serial ports init only for boot ports */
+	if (ports > config->instances_on)
+		ports = config->instances_on;
 
 	if (serial_initialized)
 		goto bind_config;
@@ -1811,6 +1814,7 @@ static int serial_function_bind_config(struct android_usb_function *f,
 		goto out;
 	}
 
+	config->instances_on = ports;
 	for (i = 0; i < ports; i++) {
 		config->f_serial_inst[i] = usb_get_function_instance("gser");
 		if (IS_ERR(config->f_serial_inst[i])) {
