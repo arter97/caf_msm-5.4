RonGokhale/codeaurora_kernel

usb: msm_otg: Fix BAM remote-wake up issue during USB low-power mode
When new data is pushed into the USB BAM pipes during USB low-power mode,
the BAM wakeup ISR triggers a USB remote-wakeup operation. This leads to
calling the ci13xxx_wakeup() function. This function issued a low-power
mode exit by a remote task, and then sets the FPR bit for issuing remote
wakeup. The problem is that sometimes the low-power mode exit remote task
does not get a chance to be executed before setting the FPR bit. In result,
the write to FPR bit fails, and no remote-wakeup happens.
The solution implemented for this issue is to wait for the low-power
mode exit to complete before setting the FPR bit.

Change-Id: Icfb2e6ab9dfc2371a4f0d8130e854b0f672f3c7e
CRs-Fixed: 514368
Signed-off-by: Danny Segal <dsegal@codeaurora.org>

drivers/usb/gadget/ci13xxx_msm.c
@@ -197,6 +197,21 @@ static bool ci13xxx_msm_in_lpm(struct ci13xxx *udc)
 	return (atomic_read(&otg->in_lpm) != 0);
 }
 
+static void ci13xxx_msm_set_fpr_flag(struct ci13xxx *udc)
+{
+	struct msm_otg *otg;
+
+	if (udc == NULL)
+		return;
+
+	if (udc->transceiver == NULL)
+		return;
+
+	otg = container_of(udc->transceiver, struct msm_otg, phy);
+
+	atomic_set(&otg->set_fpr_with_lpm_exit, 1);
+}
+
 static irqreturn_t ci13xxx_msm_resume_irq(int irq, void *data)
 {
 	struct ci13xxx *udc = _udc;
@@ -220,6 +235,7 @@ static struct ci13xxx_udc_driver ci13xxx_msm_udc_driver = {
 	.nz_itc			= 0,
 	.notify_event		= ci13xxx_msm_notify_event,
 	.in_lpm                 = ci13xxx_msm_in_lpm,
+	.set_fpr_flag           = ci13xxx_msm_set_fpr_flag,
 };
 
 static int ci13xxx_msm_install_wake_gpio(struct platform_device *pdev,

arter97/caf_msm-5.4

Change-Id: I1b2ad94cc5943dd2365dba7c4567d1930662dca3
Signed-off-by: Tarun Gupta <tarung@codeaurora.org>

drivers/usb/gadget/ci13xxx_msm.c
@@ -105,6 +105,21 @@ static bool ci13xxx_msm_in_lpm(struct ci13xxx *udc)
 	return (atomic_read(&otg->in_lpm) != 0);
 }
 
+static void ci13xxx_msm_set_fpr_flag(struct ci13xxx *udc)
+{
+	struct msm_otg *otg;
+
+	if (udc == NULL)
+		return;
+
+	if (udc->transceiver == NULL)
+		return;
+
+	otg = container_of(udc->transceiver, struct msm_otg, otg);
+
+	atomic_set(&otg->set_fpr_with_lpm_exit, 1);
+}
+
 static irqreturn_t ci13xxx_msm_resume_irq(int irq, void *data)
 {
 	struct ci13xxx *udc = _udc;
@@ -128,6 +143,7 @@ static struct ci13xxx_udc_driver ci13xxx_msm_udc_driver = {
 
 	.notify_event		= ci13xxx_msm_notify_event,
 	.in_lpm                 = ci13xxx_msm_in_lpm,
+	.set_fpr_flag           = ci13xxx_msm_set_fpr_flag,
 };
 
 static int ci13xxx_msm_install_wake_gpio(struct platform_device *pdev,