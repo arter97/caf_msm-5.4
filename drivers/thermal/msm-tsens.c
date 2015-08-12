6db79757b5e00c9c6f02ad02f44721d6e73d49a6
Siddartha Mohanadoss <smohanad@codeaurora.org>
2015-08-12 11:35:40 -0700

    thermal: tsens: Add support for MDM9640v1
    Add compatible property support to read temperature sensors
    on MDM9640v1.
    
    Change-Id: I8d260260884a08aa929c75b84264c47e586ac9ef
    Signed-off-by: Siddartha Mohanadoss <smohanad@codeaurora.org>

--- drivers/thermal/msm-tsens.c
+++ drivers/thermal/msm-tsens.c
@@ -620,6 +620,9 @@ static struct of_device_id tsens_match[] = {
 	{	.compatible = "qcom,mdm9640v2-tsens",
 		.data = (void *)TSENS_CALIB_FUSE_MAP_MDM9640,
 	},
+	{	.compatible = "qcom,mdm9640-tsens",
+		.data = (void *)TSENS_CALIB_FUSE_MAP_MDM9640,
+	},
 	{}
 };
 
