494a58b00368a74c119d19c01026af489f44c9be
Niranjan Reddy Dumbala <ndumba@codeaurora.org>
2015-08-23 11:04:39 -0700

    Revert "diag: Cleanup client information on deinit Ioctl"
    This reverts commit 5fd01508bf510d1fcbfbc3a6c17e620e01c5e37a
    
    Change-Id: If29bbddcd14f432f5ba886c6f48ed52be9052387
    Signed-off-by: Niranjan Reddy <ndumba@codeaurora.org>

--- drivers/char/diag/diagchar_core.c
+++ drivers/char/diag/diagchar_core.c
@@ -443,21 +443,17 @@ static void diag_close_logging_process(int pid)
 
 }
 
-static int diag_remove_client_entry(struct file *file)
+static int diagchar_close(struct inode *inode, struct file *file)
 {
 	int i = -1;
-	struct diagchar_priv *diagpriv_data = NULL;
+	struct diagchar_priv *diagpriv_data = file->private_data;
 	struct diag_dci_client_tbl *dci_entry = NULL;
-	if (!file) {
-		DIAG_LOG(DIAG_DEBUG_USERSPACE, "Invalid file pointer \n");
-		return -ENOENT;
-	}
-	if (!(file->private_data)) {
-		DIAG_LOG(DIAG_DEBUG_USERSPACE, "Invalid private data \n");
-		return -EINVAL;
-	}
 
-	diagpriv_data = file->private_data;
+	pr_debug("diag: process exit %s\n", current->comm);
+	if (!(file->private_data)) {
+		pr_alert("diag: Invalid file pointer");
+		return -ENOMEM;
+	}
 
 	if (!driver)
 		return -ENOMEM;
@@ -491,11 +487,6 @@ static int diag_remove_client_entry(struct file *file)
 	}
 	mutex_unlock(&driver->diagchar_mutex);
 	return 0;
-}
-static int diagchar_close(struct inode *inode, struct file *file)
-{
-	DIAG_LOG(DIAG_DEBUG_USERSPACE, "diag: process exit %s\n", current->comm);
-	return diag_remove_client_entry(file);
 }
 
 void diag_record_stats(int type, int flag)
@@ -2381,9 +2372,7 @@ static ssize_t diagchar_read(struct file *file, char __user *buf, size_t count,
 		data_type = driver->data_ready[index] & DEINIT_TYPE;
 		COPY_USER_SPACE_OR_EXIT(buf, data_type, 4);
 		driver->data_ready[index] ^= DEINIT_TYPE;
-		mutex_unlock(&driver->diagchar_mutex);
-		diag_remove_client_entry(file);
-		return ret;
+		goto exit;
 	}
 
 	if (driver->data_ready[index] & MSG_MASKS_TYPE) {
