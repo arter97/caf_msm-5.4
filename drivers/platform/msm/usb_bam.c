arter97/caf_msm-3.10

USB: Perform block reset during resume for recovery from flush failures
In case EP flush and EP prime failures are seen during disconnect,
recovery flag is set to do block reset to recover on cable re-connect from
EP flush and EP prime failures. This might lead to unclocked access if in
case usb_bam disconnect_work is still in progress. Hence fix this by moving
this block reset to suspend and perform before entering low power mode and
make sure block reset is performed in case of recovery flag is set by not
scheduling statemachine till recovery is done.

Change-Id: Ie87c069a7b1763c01fccfeb1e51d585f3ac4cc08
Signed-off-by: Vijayavardhan Vennapusa <vvreddy@codeaurora.org>

drivers/platform/msm/usb_bam.c
@@ -3513,6 +3513,26 @@ bool msm_bam_usb_lpm_ok(enum usb_ctrl bam)
 }
 EXPORT_SYMBOL(msm_bam_usb_lpm_ok);
 
+bool msm_usb_bam_enable(enum usb_ctrl bam, bool bam_enable)
+{
+	struct msm_usb_bam_platform_data *pdata;
+
+	if (!ctx.usb_bam_pdev)
+		return 0;
+
+	pdata = ctx.usb_bam_pdev->dev.platform_data;
+	if ((bam != CI_CTRL) || !(bam_enable ||
+					pdata->enable_hsusb_bam_on_boot))
+		return 0;
+
+	msm_hw_bam_disable(1);
+	sps_device_reset(ctx.h_bam[bam]);
+	msm_hw_bam_disable(0);
+
+	return 0;
+}
+EXPORT_SYMBOL(msm_usb_bam_enable);
+
 /**
  * msm_bam_hsic_host_pipe_empty - Check all HSIC host BAM pipe state
  *

arter97/caf_msm-5.4

Signed-off-by: Manu Gautam <mgautam@codeaurora.org>

drivers/platform/msm/usb_bam.c
@@ -3136,6 +3136,25 @@ void msm_bam_notify_lpm_resume()
 }
 EXPORT_SYMBOL(msm_bam_notify_lpm_resume);
 
+bool msm_usb_bam_enable(enum usb_bam bam, bool bam_enable)
+{
+	struct msm_usb_bam_platform_data *pdata;
+
+	if (!ctx.usb_bam_pdev)
+		return 0;
+
+	pdata = ctx.usb_bam_pdev->dev.platform_data;
+	if ((bam != HSUSB_BAM) || !(bam_enable ||
+					pdata->enable_hsusb_bam_on_boot))
+		return 0;
+
+	msm_hw_bam_disable(1);
+	sps_device_reset(ctx.h_bam[bam]);
+	msm_hw_bam_disable(0);
+
+	return 0;
+}
+EXPORT_SYMBOL(msm_usb_bam_enable);
 
 void msm_bam_hsic_reset(void)
 {