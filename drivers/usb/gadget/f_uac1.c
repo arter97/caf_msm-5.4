788adb2b5507eb3945fd4ccaeee4c28051063288
Manu Gautam <mgautam@codeaurora.org>
2015-09-22 15:58:59 +0530

    usb: gadget: audio: open/close Audio files on USB connect/disconnect
    Driver currently opens files on bind_config and then doesn't close
    them. It prevents system to enter suspend state as AFE keeps pushing
    data to apps. Change this design to open Audio streams on USB connect
    (after set_alt) and close on disconnect i.e. function_disable. Also,
    free usb_req and req->buff on disconnect after calling ep_disable as
    buffers are currently not freed at all.
    
    Change-Id: Ia6960c399aec2e082d04ad4409fa36880e87a5b7
    Signed-off-by: Manu Gautam <mgautam@codeaurora.org>
    Signed-off-by: Azhar Shaikh <azhars@codeaurora.org>

--- drivers/usb/gadget/f_uac1.c
+++ drivers/usb/gadget/f_uac1.c
@@ -493,6 +493,9 @@ static void f_audio_buffer_free(struct f_audio_buf *audio_buf)
 
 struct f_audio {
 	struct gaudio			card;
+	atomic_t			online;
+	struct mutex                    mutex;
+	struct work_struct		close_work;
 
 	/* endpoints handle full and/or high speeds */
 	struct usb_ep			*out_ep;
@@ -534,6 +537,19 @@ static void f_audio_playback_work(struct work_struct *data)
 	int res = 0;
 
 	pr_debug("%s: started\n", __func__);
+	if (!atomic_read(&audio->online)) {
+		pr_debug("%s offline\n", __func__);
+		return;
+	}
+	/* set up ASLA audio devices if not already done */
+	mutex_lock(&audio->mutex);
+	res = gaudio_setup(&audio->card);
+	if (res < 0) {
+		mutex_unlock(&audio->mutex);
+		return;
+	}
+	mutex_unlock(&audio->mutex);
+
 	spin_lock_irqsave(&audio->playback_lock, flags);
 	if (list_empty(&audio->play_queue)) {
 		pr_err("playback_buf is empty");
@@ -609,6 +625,19 @@ static void f_audio_capture_work(struct work_struct *data)
 	int res = 0;
 
 	pr_debug("%s Started\n", __func__);
+	if (!atomic_read(&audio->online)) {
+		pr_debug("%s offline\n", __func__);
+		return;
+	}
+	/* set up ASLA audio devices if not already done */
+	mutex_lock(&audio->mutex);
+	res = gaudio_setup(&audio->card);
+	if (res < 0) {
+		mutex_unlock(&audio->mutex);
+		return;
+	}
+	mutex_unlock(&audio->mutex);
+
 	spin_lock_irqsave(&audio->capture_lock, flags);
 	if (!list_empty(&audio->capture_queue)) {
 		spin_unlock_irqrestore(&audio->capture_lock, flags);
@@ -701,6 +730,11 @@ static void f_audio_complete(struct usb_ep *ep, struct usb_request *req)
 		break;
 	default:
 		pr_err("Failed completion: status %d", status);
+		/* fall through to to free buffer and req */
+	case -ECONNRESET:
+	case -ESHUTDOWN:
+		kfree(req->buf);
+		usb_ep_free_request(ep, req);
 		break;
 	}
 }
@@ -972,6 +1006,7 @@ static int f_audio_get_alt(struct usb_function *f, unsigned intf)
 
 static int f_audio_set_alt(struct usb_function *f, unsigned intf, unsigned alt)
 {
+	struct usb_composite_dev *cdev = f->config->cdev;
 	struct f_audio		*audio = func_to_audio(f);
 	struct usb_ep		*out_ep = audio->out_ep;
 	struct usb_ep		*in_ep = audio->in_ep;
@@ -981,6 +1016,7 @@ static int f_audio_set_alt(struct usb_function *f, unsigned intf, unsigned alt)
 
 	pr_debug("intf %d, alt %d\n", intf, alt);
 
+	atomic_set(&audio->online, 1);
 	if (intf == ac_header_desc.baInterfaceNr[0]) {
 		if (audio->alt_intf[0] == alt) {
 			pr_debug("Alt interface is already set to %d. Do nothing.\n",
@@ -990,6 +1026,10 @@ static int f_audio_set_alt(struct usb_function *f, unsigned intf, unsigned alt)
 		}
 
 		if (alt == 1) {
+			err = config_ep_by_speed(cdev->gadget, f, in_ep);
+			if (err)
+				return err;
+
 			err = usb_ep_enable(in_ep);
 			if (err) {
 				pr_err("Failed to enable capture ep");
@@ -1022,6 +1062,7 @@ static int f_audio_set_alt(struct usb_function *f, unsigned intf, unsigned alt)
 			schedule_work(&audio->capture_work);
 		} else {
 			struct f_audio_buf *capture_buf;
+			usb_ep_disable(in_ep);
 			spin_lock_irqsave(&audio->capture_lock, flags);
 			while (!list_empty(&audio->capture_queue)) {
 				capture_buf =
@@ -1044,6 +1085,10 @@ static int f_audio_set_alt(struct usb_function *f, unsigned intf, unsigned alt)
 		}
 
 		if (alt == 1) {
+			err = config_ep_by_speed(cdev->gadget, f, out_ep);
+			if (err)
+				return err;
+
 			err = usb_ep_enable(out_ep);
 			if (err) {
 				pr_err("Failed to enable playback ep");
@@ -1085,6 +1130,7 @@ static int f_audio_set_alt(struct usb_function *f, unsigned intf, unsigned alt)
 		} else {
 			struct f_audio_buf *playback_copy_buf =
 				audio->playback_copy_buf;
+			usb_ep_disable(out_ep);
 			if (playback_copy_buf) {
 				pr_err("Schedule playback_work");
 				list_add_tail(&playback_copy_buf->list,
@@ -1103,9 +1149,32 @@ static int f_audio_set_alt(struct usb_function *f, unsigned intf, unsigned alt)
 	return err;
 }
 
+static void f_audio_close_work(struct work_struct *data)
+{
+	struct f_audio *audio =
+			container_of(data, struct f_audio, close_work);
+
+	pr_debug("close audio files\n");
+	mutex_lock(&audio->mutex);
+	gaudio_cleanup();
+	mutex_unlock(&audio->mutex);
+}
+
 static void f_audio_disable(struct usb_function *f)
 {
+	struct f_audio	*audio = func_to_audio(f);
+	struct usb_ep	*out_ep = audio->out_ep;
+	struct usb_ep	*in_ep = audio->in_ep;
+
+	pr_debug("Disable audio");
+	atomic_set(&audio->online, 0);
+	usb_ep_disable(in_ep);
+	usb_ep_disable(out_ep);
+
 	u_audio_clear();
+	schedule_work(&audio->close_work);
+
+	return;
 }
 
 /*-------------------------------------------------------------------------*/
@@ -1238,6 +1307,9 @@ f_audio_unbind(struct usb_configuration *c, struct usb_function *f)
 {
 	struct f_audio *audio = func_to_audio(f);
 
+	flush_work(&audio->playback_work);
+	flush_work(&audio->capture_work);
+	flush_work(&audio->close_work);
 	gaudio_cleanup();
 	usb_free_all_descriptors(f);
 	kfree(audio);
@@ -1339,6 +1411,8 @@ int audio_bind_config(struct usb_configuration *c)
 	control_selector_init(audio);
 	INIT_WORK(&audio->playback_work, f_audio_playback_work);
 	INIT_WORK(&audio->capture_work, f_audio_capture_work);
+	INIT_WORK(&audio->close_work, f_audio_close_work);
+	mutex_init(&audio->mutex);
 
 	/* set up ASLA audio devices */
 	status = gaudio_setup(&audio->card);
