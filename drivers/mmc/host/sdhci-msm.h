110967b4fa2e09b1231a1686201631669dc8876f
Pavan Anamula <pavana@codeaurora.org>
2016-01-29 21:05:09 +0530

    Revert "mmc: sdhci-msm: Fix recursive tuning issue"
    This reverts commit dd9188fc72d2913ba2450b586c57a1160483b014.
    
    Change-Id: I07561eafac1eeae99afb678063e304a0565d42d9
    Signed-off-by: Pavan Anamula <pavana@codeaurora.org>

--- drivers/mmc/host/sdhci-msm.h
+++ drivers/mmc/host/sdhci-msm.h
@@ -164,7 +164,6 @@ struct sdhci_msm_host {
 	enum dev_state mmc_dev_state;
 	struct sdhci_msm_ice_data ice;
 	u32 ice_clk_rate;
-	bool tuning_in_progress;
 	bool enhanced_strobe;
 };
 #endif /* __SDHCI_MSM_H__ */
