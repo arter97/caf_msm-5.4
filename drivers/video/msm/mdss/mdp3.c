6d233ebc17daa2d79e46ad460a03060ca272435b
Mohammed Thasleem <mthasl@codeaurora.org>
2016-01-30 01:50:35 +0530

    8909w : Change the copyright year.
    Change-Id: I0aaf4d4de98e1e7cc62d64ed8d5786bf6ab5ee0b
    Signed-off-by: Mohammed Thasleem <mthasl@codeaurora.org>

--- drivers/video/msm/mdss/mdp3.c
+++ drivers/video/msm/mdss/mdp3.c
@@ -1,4 +1,4 @@
-/* Copyright (c) 2013-2015, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2013-2016, The Linux Foundation. All rights reserved.
  * Copyright (C) 2007 Google Incorporated
  *
  * This software is licensed under the terms of the GNU General Public
