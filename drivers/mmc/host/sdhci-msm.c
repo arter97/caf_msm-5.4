f5ce5b3f500f6e0ff8a02af77226ac172edf77f4
Pavan Anamula <pavana@codeaurora.org>
2016-01-29 21:05:09 +0530

    Revert "mmc: sdhci-msm: Fix recursive tuning issue"
    This reverts commit dd9188fc72d2913ba2450b586c57a1160483b014.
    
    Change-Id: I07561eafac1eeae99afb678063e304a0565d42d9
    Signed-off-by: Pavan Anamula <pavana@codeaurora.org>

--- drivers/mmc/host/sdhci-msm.c
+++ drivers/mmc/host/sdhci-msm.c
@@ -578,13 +578,6 @@ static int msm_init_cm_dll(struct sdhci_host *host)
 	u32 wait_cnt;
 	bool prev_pwrsave, curr_pwrsave;
 
-	/*
-	 * Don't allow re-tuning for CRC errors observed for any commands
-	 * that are sent during tuning sequence itself.
-	 */
-	if (msm_host->tuning_in_progress)
-		return 0;
-	msm_host->tuning_in_progress = true;
 	pr_debug("%s: Enter %s\n", mmc_hostname(mmc), __func__);
 	spin_lock_irqsave(&host->lock, flags);
 	prev_pwrsave = !!(readl_relaxed(host->ioaddr + CORE_VENDOR_SPEC) &
@@ -1155,7 +1148,6 @@ out:
 	if (!rc)
 		msm_host->tuning_done = true;
 	spin_unlock_irqrestore(&host->lock, flags);
-	msm_host->tuning_in_progress = false;
 	pr_debug("%s: Exit %s, err(%d)\n", mmc_hostname(mmc), __func__, rc);
 	return rc;
 }
