67f5e5b76ae5ff59075a7512e524f95fa10a24eb
Suman Mukherjee <sumam@codeaurora.org>
2016-05-02 14:01:07 +0530

    msm: camera: sensor: Validate step_boundary
    step_boundary can take values upto the total_steps
    Validate the step_boundary before consuming it.
    Convert the type of step_index and region_index to uint16_t
    step_index and region_index cannot be negative.
    
    CRs-Fixed: 1001092
    Change-Id: I1f23fd6f28bb897824a1ef99a8873b9f986eee70
    Signed-off-by: Rajesh Bondugula <rajeshb@codeaurora.org>
    Signed-off-by: Suman Mukherjee <sumam@codeaurora.org>
    Signed-off-by: mraja <mraja@codeaurora.org>

--- drivers/media/platform/msm/camera_v2/sensor/actuator/msm_actuator.c
+++ drivers/media/platform/msm/camera_v2/sensor/actuator/msm_actuator.c
@@ -381,11 +381,18 @@ static int32_t msm_actuator_init_step_table(struct msm_actuator_ctrl_t *a_ctrl,
 {
 	int16_t code_per_step = 0;
 	int16_t cur_code = 0;
-	int16_t step_index = 0, region_index = 0;
+	uint16_t step_index = 0, region_index = 0;
 	uint16_t step_boundary = 0;
 	uint32_t max_code_size = 1;
 	uint16_t data_size = set_info->actuator_params.data_size;
 	CDBG("Enter\n");
+
+	/* validate the actuator state */
+	if (a_ctrl->actuator_state != ACTUATOR_POWER_UP) {
+		pr_err("%s:%d invalid actuator_state %d\n"
+			, __func__, __LINE__, a_ctrl->actuator_state);
+		return -EINVAL;
+	}
 
 	for (; data_size > 0; data_size--)
 		max_code_size *= 2;
