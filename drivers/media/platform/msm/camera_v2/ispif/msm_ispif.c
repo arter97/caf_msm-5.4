320f8f1a23bdaffb78a065556e1d9a5a3cd667c8
VijayaKumar T M <vtmuni@codeaurora.org>
2016-06-10 11:04:37 +0530

    msm: camera: ispif: Validate VFE num input during reset
    Userspace supplies the actual number of used VFEs in session to ISPIF.
    Validate the userspace input value and if found to be invalid, return
    error.
    -Fix checkpatch.pl warnings.
    
    CRs-Fixed: 898074
    Signed-off-by: Venu Yeshala <vyeshala@codeaurora.org>
    Signed-off-by: VijayaKumar T M <vtmuni@codeaurora.org>
    Change-Id: I3288ddb6404e817a705a92281b4c54666f372c56

--- drivers/media/platform/msm/camera_v2/ispif/msm_ispif.c
+++ drivers/media/platform/msm/camera_v2/ispif/msm_ispif.c
@@ -1,4 +1,4 @@
-/* Copyright (c) 2013-2014, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2013-2014,2016 The Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -61,8 +61,8 @@ static void msm_ispif_io_dump_reg(struct ispif_device *ispif)
 static inline int msm_ispif_is_intf_valid(uint32_t csid_version,
 	uint8_t intf_type)
 {
-        return ((csid_version <= CSID_VERSION_V22 && intf_type != VFE0) ||
-                (intf_type >= VFE_MAX)) ? false : true;
+	return ((csid_version <= CSID_VERSION_V22 && intf_type != VFE0) ||
+		(intf_type >= VFE_MAX)) ? false : true;
 }
 
 static struct msm_cam_clk_info ispif_8626_reset_clk_info[] = {
@@ -1158,6 +1158,13 @@ static irqreturn_t msm_io_ispif_irq(int irq_num, void *data)
 static int msm_ispif_set_vfe_info(struct ispif_device *ispif,
 	struct msm_ispif_vfe_info *vfe_info)
 {
+	if (!vfe_info || (vfe_info->num_vfe <= 0) ||
+		((uint32_t)(vfe_info->num_vfe) > ispif->hw_num_isps)) {
+		pr_err("Invalid VFE info: %p %d\n", vfe_info,
+			(vfe_info ? vfe_info->num_vfe:0));
+			return -EINVAL;
+	}
+
 	memcpy(&ispif->vfe_info, vfe_info, sizeof(struct msm_ispif_vfe_info));
 
 	return 0;
