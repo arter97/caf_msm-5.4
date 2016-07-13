d81c160fd4b3dab7aef903b88693547103edb785
Chandana Kishori Chiluveru <cchiluve@codeaurora.org>
2016-07-13 10:54:55 +0530

    USB: core: Change USB_DEVICE_ERROR uevent device path
    Change USB_DEVICE_ERROR uevent device path to its parent path.
    With this USB driver sends error event with full device path
    to userspace. Customer can use this path to unbind the driver
    which will trigger disconnect for unsupported device.
    
    Change-Id: I4c546428c996a46ced74f179e9b7bd36e0b654a7
    Signed-off-by: Chandana Kishori Chiluveru <cchiluve@codeaurora.org>

--- drivers/usb/core/hub.c
+++ drivers/usb/core/hub.c
@@ -3351,7 +3351,7 @@ hub_port_init (struct usb_hub *hub, struct usb_device *udev, int port1,
 				dev_err(&udev->dev,
 					"device descriptor read/64, error %d\n",
 					r);
-				kobject_uevent_env(&udev->dev.kobj,
+				kobject_uevent_env(&udev->parent->dev.kobj,
 						KOBJ_CHANGE, error_event);
 				retval = -EMSGSIZE;
 				continue;
@@ -3401,7 +3401,7 @@ hub_port_init (struct usb_hub *hub, struct usb_device *udev, int port1,
 			dev_err(&udev->dev,
 					"device descriptor read/8, error %d\n",
 					retval);
-			kobject_uevent_env(&udev->dev.kobj,
+			kobject_uevent_env(&udev->parent->dev.kobj,
 						KOBJ_CHANGE, error_event);
 			if (retval >= 0)
 				retval = -EMSGSIZE;
