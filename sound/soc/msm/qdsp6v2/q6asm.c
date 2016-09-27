eb3cf7f4186cafc0aefa2fb8fb05607cee300cb7
Yogendra Charya Tangalapally <yogendra@codeaurora.org>
2016-09-27 22:58:31 +0530

    Revert "ASoC: msm: Add Buffer overflow check"
    This reverts commit dbf38aed42749e07d6856a186397daddb3ba8fa9.
    
    Change-Id: I847390aa2cafac89ca8c7975fe490d9c999a7605
    Signed-off-by: Yogendra Charya Tangalapally <yogendra@codeaurora.org>

--- sound/soc/msm/qdsp6v2/q6asm.c
+++ sound/soc/msm/qdsp6v2/q6asm.c
@@ -1074,7 +1074,7 @@ err:
 int q6asm_audio_client_buf_alloc(unsigned int dir,
 			struct audio_client *ac,
 			unsigned int bufsz,
-			uint32_t bufcnt)
+			unsigned int bufcnt)
 {
 	int cnt = 0;
 	int rc = 0;
@@ -1101,7 +1101,7 @@ int q6asm_audio_client_buf_alloc(unsigned int dir,
 			return 0;
 		}
 		mutex_lock(&ac->cmd_lock);
-		if (bufcnt > (U32_MAX/sizeof(struct audio_buffer))) {
+		if (bufcnt > (LONG_MAX/sizeof(struct audio_buffer))) {
 			pr_err("%s: Buffer size overflows", __func__);
 			mutex_unlock(&ac->cmd_lock);
 			goto fail;
