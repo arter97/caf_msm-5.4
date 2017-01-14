e8c4c5c888fa0ecd0f909f73f76fb92ba7bfa266
Iliya Varadzhakov <ivarad@codeaurora.org>
2017-01-14 23:48:08 +0200

    msm: isp: update 4 camera concurrency mode
    Change-Id: I9327cf99861940469c016c944ddf85bfeac90303
    Signed-off-by: Iliya Varadzhakov <ivarad@codeaurora.org>

--- include/uapi/media/msm_camsensor_sdk.h
+++ include/uapi/media/msm_camsensor_sdk.h
@@ -48,6 +48,8 @@
 #define MSM_EEPROM_MEMORY_MAP_MAX_SIZE  80
 #define MSM_EEPROM_MAX_MEM_MAP_CNT      8
 
+#define MSM_SENSOR_BYPASS_VIDEO_NODE    1
+
 enum msm_sensor_camera_id_t {
 	CAMERA_0,
 	CAMERA_1,
