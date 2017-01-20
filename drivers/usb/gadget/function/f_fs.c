e74fa074161c29d8111aa6989f07b358f28c25ff
Arumuga Durai A <cadurai@codeaurora.org>
2017-01-20 21:24:24 +0530

    USB: gadget: f_fs: Fix adb issues due to retrying adb_read
    Commit b0d8bfa81a755 ("USB: gadget: ffs: Fix USBCV compliance
    failure due to adbd") retries read if disconnect/reset happened
    without any data transfers between set_alt and disable. This
    allows userspace to not unnecessarily perform unbind-bind. As
    part of that driver leaves data as not NULL (from previous read)
    and clears only read->error flag. This leads to two issues.
    
    If disconnect-connect happened with adb server not running on host
    then write->error flag is not cleared, hence subsequent adb_write
    fails. This results in adb going offline. Memory corruption due to
    double free of memory as data ptr is not NULL (post retry) when
    adbd receives signal.
    Fix these by clearing all epfile->error flags and set data pointer
    to NULL on adb_read retry
    
    CRs-Fixed: 1114810
    Change-Id: Ic3a5336b8d974d24355539c247434d98ab79b03f
    Signed-off-by: Arumuga Durai A <cadurai@codeaurora.org>

--- drivers/usb/gadget/function/f_fs.c
+++ drivers/usb/gadget/function/f_fs.c
@@ -723,7 +723,6 @@ static ssize_t ffs_epfile_io(struct file *file, struct ffs_io_data *io_data)
 			io_data->read);
 
 retry:
-	first_read = false;
 	if (atomic_read(&epfile->error))
 		return -ENODEV;
 
@@ -950,15 +949,25 @@ retry:
 					ret = ep->status;
 				else
 					ret = -ENODEV;
+
 				/* do wait again if func eps are not enabled */
 				if (io_data->read && first_read && (ret < 0)) {
+					unsigned short count = ffs->eps_count;
 					pr_debug("%s: waiting for the online state\n",
 						 __func__);
 					ret = 0;
 					kfree(data);
+					data = NULL;
+					data_len = -EINVAL;
 					spin_unlock_irq(&epfile->ffs->eps_lock);
 					mutex_unlock(&epfile->mutex);
-					atomic_set(&epfile->error, 0);
+					epfile = ffs->epfiles;
+					do {
+						atomic_set(&epfile->error, 0);
+						++epfile;
+					} while (--count);
+					epfile = file->private_data;
+					first_read = false;
 					goto retry;
 				}
 
