c6d959d5c794da99c37172c2a825363a8151f380
Kumar, Anand <anandkumar@codeaurora.org>
2017-02-16 17:26:31 +0530

    wcnss: Add skb prealloc support
    Add wcnss prealloc support and skb prealloc support
    for driver.
    
    CRs-Fixed: 2012188
    Change-Id: I516ff935396376008baf940d12c7172e96e0cef4
    Signed-off-by: Anand Kumar <anandkumar@codeaurora.org>

--- include/net/wcnss_prealloc.h
+++ include/net/wcnss_prealloc.h
@@ -13,7 +13,19 @@
 #ifndef _WCNSS_PRE_ALLOC_H_
 #define _WCNSS_PRE_ALLOC_H_
 
+#define WCNSS_PRE_ALLOC_GET_THRESHOLD (4*1024)
+#ifdef CONFIG_WCNSS_SKB_PRE_ALLOC
+#define WCNSS_PRE_SKB_ALLOC_GET_THRESHOLD (50*1024)
+#endif
 int wcnss_prealloc_init(void);
 void wcnss_prealloc_deinit(void);
+extern void *wcnss_prealloc_get(unsigned int size);
+extern int wcnss_prealloc_put(void *ptr);
+
+#ifdef CONFIG_WCNSS_SKB_PRE_ALLOC
+extern struct sk_buff *wcnss_skb_prealloc_get(unsigned int size);
+extern int wcnss_skb_prealloc_put(struct sk_buff *skb);
+extern int wcnss_skb_pre_alloc_reset(void);
+#endif
 
 #endif/* _WCNSS_PRE_ALLOC_H_ */
