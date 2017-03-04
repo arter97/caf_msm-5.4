e20b621c6284c73cb3adcaf4b4805b4815c2c3a6
Alexy Joseph <alexyj@codeaurora.org>
2017-03-03 18:50:21 -0800

    ASoC: qdsp6v2: Remove Eagle code
    Eagle driver is not in use any more.
    Remove the code and associated calls
    to it.
    
    CRs-Fixed: 1103106
    Change-Id: Ice5333861beda9538f0783b70b3267523d16fd2b
    Signed-off-by: Alexy Joseph <alexyj@codeaurora.org>

--- sound/soc/msm/qdsp6v2/audio_cal_utils.c
+++ sound/soc/msm/qdsp6v2/audio_cal_utils.c
@@ -1,4 +1,4 @@
-/* Copyright (c) 2014, 2016, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2014, 2016-2017, The Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -127,8 +127,6 @@ size_t get_cal_info_size(int32_t cal_type)
 	case ULP_LSM_CAL_TYPE:
 		size = sizeof(struct audio_cal_info_lsm);
 		break;
-	case DTS_EAGLE_CAL_TYPE:
-		size = 0;
 	case AUDIO_CORE_METAINFO_CAL_TYPE:
 		size = sizeof(struct audio_cal_info_metainfo);
 		break;
@@ -245,8 +243,6 @@ size_t get_user_cal_type_size(int32_t cal_type)
 	case ULP_LSM_CAL_TYPE:
 		size = sizeof(struct audio_cal_type_lsm);
 		break;
-	case DTS_EAGLE_CAL_TYPE:
-		size = 0;
 	case AUDIO_CORE_METAINFO_CAL_TYPE:
 		size = sizeof(struct audio_cal_type_metainfo);
 		break;
