d88765f696df870eb3cee8d3468838c06f8c5132
Greg Hackmann <ghackmann@google.com>
2017-09-19 10:55:17 -0700

    clocksource: arch_timer: make virtual counter access configurable
    Change-Id: Ibdb1fd768b748002b90bfc165612c12c8311f8a2
    Signed-off-by: Neeraj Upadhyay <neeraju@codeaurora.org>
    Signed-off-by: Girish Hulamani <ghulam@codeaurora.org>

--- drivers/clocksource/arm_arch_timer.c
+++ drivers/clocksource/arm_arch_timer.c
@@ -343,6 +343,12 @@ static void arch_counter_set_user_access(void)
 	/* Enable user access to the virtual and physical counters */
 	cntkctl |= ARCH_TIMER_USR_VCT_ACCESS_EN | ARCH_TIMER_USR_PCT_ACCESS_EN;
 
+	/* Enable user access to the virtual counter */
+	if (IS_ENABLED(CONFIG_ARM_ARCH_TIMER_VCT_ACCESS))
+		cntkctl |= ARCH_TIMER_USR_VCT_ACCESS_EN;
+	else
+		cntkctl &= ~ARCH_TIMER_USR_VCT_ACCESS_EN;
+
 	arch_timer_set_cntkctl(cntkctl);
 }
 
