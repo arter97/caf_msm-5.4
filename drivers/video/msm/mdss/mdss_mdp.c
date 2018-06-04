d02c4cd74eacda8f16d1a41a5041e9b0230ad55c
raghavendra ambadas <rambad@codeaurora.org>
2018-06-04 10:56:52 +0530

    msm: mdss: Fix double free during probe fail
    Memory allocated with "devm_kzalloc" is automatically released
    by the kernel if the "probe" function fails with an error code.
    Therefore, using "kfree" is unsafe because it can lead to the Double-Free.
    
    Change-Id: Ic9285ebbd7d246e275a93cde4d03656d99d5ea3d
    Signed-off-by: Raghavendra Ambadas <rambad@codeaurora.org>

--- drivers/video/msm/mdss/mdss_mdp.c
+++ drivers/video/msm/mdss/mdss_mdp.c
@@ -1,7 +1,7 @@
 /*
  * MDSS MDP Interface (used by framebuffer core)
  *
- * Copyright (c) 2007-2014, 2017, The Linux Foundation. All rights reserved.
+ * Copyright (c) 2007-2014, 2017-2018, The Linux Foundation. All rights reserved.
  * Copyright (C) 2007 Google Incorporated
  *
  * This software is licensed under the terms of the GNU General Public
@@ -1765,7 +1765,7 @@ static int mdss_mdp_parse_dt_pipe(struct platform_device *pdev)
 	if (!mdata->vig_pipes) {
 		pr_err("no mem for vig_pipes: kzalloc fail\n");
 		rc = -ENOMEM;
-		goto vig_alloc_fail;
+		goto parse_fail;
 	}
 
 	mdata->rgb_pipes = devm_kzalloc(&mdata->pdev->dev,
@@ -1773,7 +1773,7 @@ static int mdss_mdp_parse_dt_pipe(struct platform_device *pdev)
 	if (!mdata->rgb_pipes) {
 		pr_err("no mem for rgb_pipes: kzalloc fail\n");
 		rc = -ENOMEM;
-		goto rgb_alloc_fail;
+		goto parse_fail;
 	}
 
 	if (mdata->ndma_pipes) {
@@ -1783,7 +1783,7 @@ static int mdss_mdp_parse_dt_pipe(struct platform_device *pdev)
 		if (!mdata->dma_pipes) {
 			pr_err("no mem for dma_pipes: kzalloc fail\n");
 			rc = -ENOMEM;
-			goto dma_alloc_fail;
+			goto parse_fail;
 		}
 	}
 
@@ -1930,16 +1930,8 @@ static int mdss_mdp_parse_dt_pipe(struct platform_device *pdev)
 			mdata->ndma_pipes);
 	}
 
-	goto parse_done;
 
 parse_fail:
-	kfree(mdata->dma_pipes);
-dma_alloc_fail:
-	kfree(mdata->rgb_pipes);
-rgb_alloc_fail:
-	kfree(mdata->vig_pipes);
-parse_done:
-vig_alloc_fail:
 	kfree(xin_id);
 xin_alloc_fail:
 	kfree(ftch_id);
