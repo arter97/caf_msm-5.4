34e38354af9bb8c2158af88d39f0fcc5cd093edf
Tanya Dixit <tdixit@codeaurora.org>
2018-09-11 17:59:29 +0530

    rtac: Add mutex lock to ensure proper fops access
    Add mutex lock in rtac_open and rtac_release
    to avoid usage count discrepancies leading
    to multiple calls to unmap memory resulting in
    null pointer dereference.
    
    CRs-Fixed: 2271712
    Change-Id: Ie6da28837c352030b8d7e377d68a70cf04e7072a
    Signed-off-by: Tanya Dixit <tdixit@codeaurora.org>

--- sound/soc/msm/qdsp6v2/rtac.c
+++ sound/soc/msm/qdsp6v2/rtac.c
@@ -54,6 +54,7 @@ struct rtac_cal_block_data	rtac_cal[MAX_RTAC_BLOCKS] = {
 struct rtac_common_data {
 	atomic_t			usage_count;
 	atomic_t			apr_err_code;
+	struct mutex			rtac_fops_mutex;
 };
 
 static struct rtac_common_data		rtac_common;
@@ -145,11 +146,6 @@ struct mutex			rtac_asm_apr_mutex;
 struct mutex			rtac_voice_mutex;
 struct mutex			rtac_voice_apr_mutex;
 struct mutex			rtac_afe_apr_mutex;
-
-static struct mutex			rtac_asm_cal_mutex;
-static struct mutex			rtac_adm_cal_mutex;
-static struct mutex			rtac_afe_cal_mutex;
-static struct mutex			rtac_voice_cal_mutex;
 
 int rtac_clear_mapping(uint32_t cal_type)
 {
@@ -344,7 +340,9 @@ static int rtac_open(struct inode *inode, struct file *f)
 	int	result = 0;
 	pr_debug("%s\n", __func__);
 
+	mutex_lock(&rtac_common.rtac_fops_mutex);
 	atomic_inc(&rtac_common.usage_count);
+	mutex_unlock(&rtac_common.rtac_fops_mutex);
 	return result;
 }
 
@@ -355,12 +353,15 @@ static int rtac_release(struct inode *inode, struct file *f)
 	int	i;
 	pr_debug("%s\n", __func__);
 
+	mutex_lock(&rtac_common.rtac_fops_mutex);
 	atomic_dec(&rtac_common.usage_count);
 	pr_debug("%s: ref count %d!\n", __func__,
 		atomic_read(&rtac_common.usage_count));
 
-	if (atomic_read(&rtac_common.usage_count) > 0)
+	if (atomic_read(&rtac_common.usage_count) > 0) {
+		mutex_unlock(&rtac_common.rtac_fops_mutex);
 		goto done;
+	}
 
 	for (i = 0; i < MAX_RTAC_BLOCKS; i++) {
 		result2 = rtac_unmap_cal_buffer(i);
@@ -377,6 +378,7 @@ static int rtac_release(struct inode *inode, struct file *f)
 			result = result2;
 		}
 	}
+	mutex_unlock(&rtac_common.rtac_fops_mutex);
 done:
 	return result;
 }
@@ -1647,62 +1649,42 @@ static long rtac_ioctl_shared(struct file *f,
 	}
 
 	case AUDIO_GET_RTAC_ADM_CAL:
-		mutex_lock(&rtac_adm_cal_mutex);
 		result = send_adm_apr((void *)arg, ADM_CMD_GET_PP_PARAMS_V5);
-		mutex_unlock(&rtac_adm_cal_mutex);
 		break;
 	case AUDIO_SET_RTAC_ADM_CAL:
-		mutex_lock(&rtac_adm_cal_mutex);
 		result = send_adm_apr((void *)arg, ADM_CMD_SET_PP_PARAMS_V5);
-		mutex_unlock(&rtac_adm_cal_mutex);
 		break;
 	case AUDIO_GET_RTAC_ASM_CAL:
-		mutex_lock(&rtac_asm_cal_mutex);
 		result = send_rtac_asm_apr((void *)arg,
 			ASM_STREAM_CMD_GET_PP_PARAMS_V2);
-		mutex_unlock(&rtac_asm_cal_mutex);
 		break;
 	case AUDIO_SET_RTAC_ASM_CAL:
-		mutex_lock(&rtac_asm_cal_mutex);
 		result = send_rtac_asm_apr((void *)arg,
 			ASM_STREAM_CMD_SET_PP_PARAMS_V2);
-		mutex_unlock(&rtac_asm_cal_mutex);
 		break;
 	case AUDIO_GET_RTAC_CVS_CAL:
-		mutex_lock(&rtac_voice_cal_mutex);
 		result = send_voice_apr(RTAC_CVS, (void *)arg,
 			VOICE_CMD_GET_PARAM);
-		mutex_unlock(&rtac_voice_cal_mutex);
 		break;
 	case AUDIO_SET_RTAC_CVS_CAL:
-		mutex_lock(&rtac_voice_cal_mutex);
 		result = send_voice_apr(RTAC_CVS, (void *)arg,
 			VOICE_CMD_SET_PARAM);
-		mutex_unlock(&rtac_voice_cal_mutex);
 		break;
 	case AUDIO_GET_RTAC_CVP_CAL:
-		mutex_lock(&rtac_voice_cal_mutex);
 		result = send_voice_apr(RTAC_CVP, (void *)arg,
 			VOICE_CMD_GET_PARAM);
-		mutex_unlock(&rtac_voice_cal_mutex);
 		break;
 	case AUDIO_SET_RTAC_CVP_CAL:
-		mutex_lock(&rtac_voice_cal_mutex);
 		result = send_voice_apr(RTAC_CVP, (void *)arg,
 			VOICE_CMD_SET_PARAM);
-		mutex_unlock(&rtac_voice_cal_mutex);
 		break;
 	case AUDIO_GET_RTAC_AFE_CAL:
-		mutex_lock(&rtac_afe_cal_mutex);
 		result = send_rtac_afe_apr((void *)arg,
 			AFE_PORT_CMD_GET_PARAM_V2);
-		mutex_unlock(&rtac_afe_cal_mutex);
 		break;
 	case AUDIO_SET_RTAC_AFE_CAL:
-		mutex_lock(&rtac_afe_cal_mutex);
 		result = send_rtac_afe_apr((void *)arg,
 			AFE_PORT_CMD_SET_PARAM_V2);
-		mutex_unlock(&rtac_afe_cal_mutex);
 		break;
 	default:
 		pr_err("%s: Invalid IOCTL, command = %d!\n",
@@ -1718,6 +1700,7 @@ static long rtac_ioctl(struct file *f,
 {
 	int result = 0;
 
+	mutex_lock(&rtac_common.rtac_fops_mutex);
 	if (!arg) {
 		pr_err("%s: No data sent to driver!\n", __func__);
 		result = -EFAULT;
@@ -1725,6 +1708,7 @@ static long rtac_ioctl(struct file *f,
 		result = rtac_ioctl_shared(f, cmd, (void __user *)arg);
 	}
 
+	mutex_unlock(&rtac_common.rtac_fops_mutex);
 	return result;
 }
 
@@ -1747,6 +1731,7 @@ static long rtac_compat_ioctl(struct file *f,
 {
 	int result = 0;
 
+	mutex_lock(&rtac_common.rtac_fops_mutex);
 	if (!arg) {
 		pr_err("%s: No data sent to driver!\n", __func__);
 		result = -EINVAL;
@@ -1799,6 +1784,7 @@ process:
 		break;
 	}
 done:
+	mutex_unlock(&rtac_common.rtac_fops_mutex);
 	return result;
 }
 #else
@@ -1826,6 +1812,7 @@ static int __init rtac_init(void)
 	/* Driver */
 	atomic_set(&rtac_common.usage_count, 0);
 	atomic_set(&rtac_common.apr_err_code, 0);
+	mutex_init(&rtac_common.rtac_fops_mutex);
 
 	/* ADM */
 	memset(&rtac_adm_data, 0, sizeof(rtac_adm_data));
@@ -1834,7 +1821,6 @@ static int __init rtac_init(void)
 	init_waitqueue_head(&rtac_adm_apr_data.cmd_wait);
 	mutex_init(&rtac_adm_mutex);
 	mutex_init(&rtac_adm_apr_mutex);
-	mutex_init(&rtac_adm_cal_mutex);
 
 	/* ASM */
 	for (i = 0; i < SESSION_MAX+1; i++) {
@@ -1843,14 +1829,12 @@ static int __init rtac_init(void)
 		init_waitqueue_head(&rtac_asm_apr_data[i].cmd_wait);
 	}
 	mutex_init(&rtac_asm_apr_mutex);
-	mutex_init(&rtac_asm_cal_mutex);
 
 	/* AFE */
 	rtac_afe_apr_data.apr_handle = NULL;
 	atomic_set(&rtac_afe_apr_data.cmd_state, 0);
 	init_waitqueue_head(&rtac_afe_apr_data.cmd_wait);
 	mutex_init(&rtac_afe_apr_mutex);
-	mutex_init(&rtac_afe_cal_mutex);
 
 	/* Voice */
 	memset(&rtac_voice_data, 0, sizeof(rtac_voice_data));
@@ -1861,7 +1845,6 @@ static int __init rtac_init(void)
 	}
 	mutex_init(&rtac_voice_mutex);
 	mutex_init(&rtac_voice_apr_mutex);
-	mutex_init(&rtac_voice_cal_mutex);
 
 	return misc_register(&rtac_misc);
 }
