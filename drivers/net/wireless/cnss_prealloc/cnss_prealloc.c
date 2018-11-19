d77ca928845346a85ac4467ea39a2bfe74782266
Sandeep Singh <sandsing@codeaurora.org>
2018-11-19 13:46:25 +0530

    wcnss: Fix buffer overflow in wcnss_prealloc_get
    There is potential integer truncation in the wcnss_prealloc_get api.
    size_t is 8 byte on x64 platform and "unsigned int" is 4 byte.
    To avoid this integer truncation, pass size as size_t instead
    of unsigned int.
    
    CRs-Fixed: 2269610
    Change-Id: I14b274dd7cad98b55fdce1aaa27783272231afde
    Signed-off-by: Sandeep Singh <sandsing@codeaurora.org>

--- drivers/net/wireless/cnss_prealloc/cnss_prealloc.c
+++ drivers/net/wireless/cnss_prealloc/cnss_prealloc.c
@@ -1,4 +1,4 @@
-/* Copyright (c) 2012,2014-2015 The Linux Foundation. All rights reserved.
+/* Copyright (c) 2012,2014-2015, 2019 The Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -24,7 +24,7 @@ static DEFINE_SPINLOCK(alloc_lock);
 
 struct wcnss_prealloc {
 	int occupied;
-	unsigned int size;
+	size_t size;
 	void *ptr;
 #ifdef CONFIG_SLUB_DEBUG
 	unsigned long stack_trace[WCNSS_MAX_STACK_TRACE];
@@ -148,7 +148,7 @@ static inline void wcnss_prealloc_save_stack_trace(struct wcnss_prealloc *entry)
 }
 #endif
 
-void *wcnss_prealloc_get(unsigned int size)
+void *wcnss_prealloc_get(size_t size)
 {
 	int i = 0;
 	unsigned long flags;
@@ -168,7 +168,7 @@ void *wcnss_prealloc_get(unsigned int size)
 	}
 	spin_unlock_irqrestore(&alloc_lock, flags);
 
-	pr_err("wcnss: %s: prealloc not available for size: %d\n",
+	pr_err("wcnss: %s: prealloc not available for size: %zu\n",
 			__func__, size);
 	WARN_ON(1);
 
@@ -209,8 +209,8 @@ void wcnss_prealloc_check_memory_leak(void)
 			j++;
 		}
 
-		pr_err("Size: %u, addr: %pK, backtrace:\n",
-				wcnss_allocs[i].size, wcnss_allocs[i].ptr);
+		pr_err("Size: %zu, addr: %pK, backtrace:\n",
+		       wcnss_allocs[i].size, wcnss_allocs[i].ptr);
 		print_stack_trace(&wcnss_allocs[i].trace, 1);
 	}
 
