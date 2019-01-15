ccc99631a9790940888dceb73f1380f589fa2446
Suresh H A <c_sha@qti.qualcomm.com>
2019-01-15 15:49:59 -0800

    ASoC: msm: qdsp6v2: add range check for audio port index
    Add range check to make sure the received audio port index
    from ADSP is within the valid range.
    
    Change-Id: Ief647df1659f7f349a843f666d8f92f34a9a43be
    Signed-off-by: Xiaoyu Ye <benyxy@codeaurora.org>
    Signed-off-by: Girish Hulamani <ghulam@codeaurora.org>

--- sound/soc/msm/qdsp6v2/q6asm.c
+++ sound/soc/msm/qdsp6v2/q6asm.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2012-2016, 2019, The Linux Foundation. All rights reserved.
+ * Copyright (c) 2012-2019, The Linux Foundation. All rights reserved.
  * Author: Brian Swetland <swetland@google.com>
  *
  * This software is licensed under the terms of the GNU General Public
@@ -1471,6 +1471,10 @@ static int32_t q6asm_srvc_callback(struct apr_client_data *data, void *priv)
 	}
 
 	dir = (data->token & 0x0F);
+	if (dir != IN && dir != OUT) {
+		pr_err("%s: Invalid audio port index: %d\n", __func__, dir);
+		return 0;
+	}
 	port = &ac->port[dir];
 
 	switch (data->opcode) {
