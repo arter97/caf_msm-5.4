52b8fe6eb4c9a4c2384b9dac1b65796ae837cb00
Liangwei Dong <liangwei@codeaurora.org>
2019-01-18 16:54:38 +0530

    nl80211: Allow set/del pmksa operations for AP
    Host drivers may offload authentication to the user space
    through the commit ("cfg80211: Authentication offload to
    user space in AP mode").
    
    This interface can be used to implement SAE by having the
    userspace do authentication/PMKID key derivation and driver
    handle the association.
    
    A step ahead, this interface can get further optimized if the
    PMKID is passed to the host driver and also have it respond to
    the association request by the STA on a valid PMKID.
    
    This commit enables the userspace to pass the PMKID to the host
    drivers through the set/del pmksa operations in AP mode.
    
    Set/Del pmksa is now restricted to STA/P2P client mode only and
    thus the drivers might not expect them in any other(AP) mode.
    
    This commit also introduces a feature flag
    NL80211_EXT_FEATURE_AP_PMKSA_CACHING (johannes: renamed) to
    maintain the backward compatibility of such an expectation by
    the host drivers. These operations are allowed in AP mode only
    when the drivers advertize the capability through this flag.
    
    Signed-off-by: Srinivas Dasari <dasaris@codeaurora.org>
    [rename flag to NL80211_EXT_FEATURE_AP_PMKSA_CACHING]
    Signed-off-by: Johannes Berg <johannes.berg@intel.com>
    
    Git-commit: 6c900360e7c0df6a4846ac97d7b548d72cd801b0
    Git-repo: git://git.kernel.org/pub/scm/linux/kernel/git/jberg/mac80211-next.git
    CRs-Fixed: 2396953
    Change-Id: I967ec287edb1c685614e965fe67a32e35035ff11
    Signed-off-by: Liangwei Dong <liangwei@codeaurora.org>

--- net/wireless/nl80211.c
+++ net/wireless/nl80211.c
@@ -7968,7 +7968,10 @@ static int nl80211_setdel_pmksa(struct sk_buff *skb, struct genl_info *info)
 	pmksa.bssid = nla_data(info->attrs[NL80211_ATTR_MAC]);
 
 	if (dev->ieee80211_ptr->iftype != NL80211_IFTYPE_STATION &&
-	    dev->ieee80211_ptr->iftype != NL80211_IFTYPE_P2P_CLIENT)
+	    dev->ieee80211_ptr->iftype != NL80211_IFTYPE_P2P_CLIENT &&
+	    !(dev->ieee80211_ptr->iftype == NL80211_IFTYPE_AP &&
+	      wiphy_ext_feature_isset(&rdev->wiphy,
+				      NL80211_EXT_FEATURE_AP_PMKSA_CACHING)))
 		return -EOPNOTSUPP;
 
 	switch (info->genlhdr->cmd) {
