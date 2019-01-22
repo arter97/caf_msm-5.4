6ad8bfed12e87346a43a593665bf598fcd1012cc
Manoj Prabhu B <bmanoj@codeaurora.org>
2019-01-22 20:06:32 +0530

    diag: dci: Validate dci client entries prior to access
    The patch validates the dci entries and its task
    structure before accessing structure members avoiding possible
    kernel bug.
    
    CRs-Fixed: 2035140
    Change-Id: I7b0813defef1cb60400184acc631047cf72af94e
    Signed-off-by: Manoj Prabhu B <bmanoj@codeaurora.org>

--- drivers/char/diag/diagchar_core.c
+++ drivers/char/diag/diagchar_core.c
@@ -1,4 +1,4 @@
-/* Copyright (c) 2008-2018, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2008-2015, 2019 The Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -285,7 +285,7 @@ static int diagchar_close(struct inode *inode, struct file *file)
 	* This will specially help in case of ungraceful exit of any DCI client
 	* This call will remove any pending registrations of such client
 	*/
-	dci_entry = dci_lookup_client_entry_pid(current->pid);
+	dci_entry = dci_lookup_client_entry_pid(current->tgid);
 	if (dci_entry)
 		diag_dci_deinit_client(dci_entry);
 	/* If the exiting process is the socket process */
