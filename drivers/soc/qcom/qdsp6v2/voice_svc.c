0ffe52d04a3c92911c7dcc8e0b318c9ba5d7d874
Ajit Pandey <ajitp@codeaurora.org>
2019-02-28 18:03:17 +0530

    drivers: soc: qcom: Added check to avoid opening multiple instance
    Opening of multiple instance of voice_svc user space from app will
    lead to pointer deference of private data within apr callback. As
    multi-instance not supported added check to deny open() from user
    space if previous instance hasn't been closed.
    
    Change-Id: Ia5ef16c69a517760fc9d45530a8a41a333fa2a21
    Signed-off-by: Ajit Pandey <ajitp@codeaurora.org>

--- drivers/soc/qcom/qdsp6v2/voice_svc.c
+++ drivers/soc/qcom/qdsp6v2/voice_svc.c
@@ -1,4 +1,4 @@
-/* Copyright (c) 2014-2018, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2014-2019, The Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -67,6 +67,8 @@ static void *dummy_q6_mvm;
 static void *dummy_q6_cvs;
 dev_t device_num;
 
+static struct mutex session_lock;
+static bool is_released = 1;
 static int voice_svc_dummy_reg(void);
 static int32_t qdsp_dummy_apr_callback(struct apr_client_data *data,
 					void *priv);
@@ -610,14 +612,23 @@ err:
 static int voice_svc_open(struct inode *inode, struct file *file)
 {
 	struct voice_svc_prvt *prtd = NULL;
+	int ret = 0;
 
 	pr_debug("%s\n", __func__);
+
+	mutex_lock(&session_lock);
+	if (is_released == 0) {
+		pr_err("%s: Access denied to device\n", __func__);
+		ret = -EBUSY;
+		goto done;
+	}
 
 	prtd = kmalloc(sizeof(struct voice_svc_prvt), GFP_KERNEL);
 
 	if (prtd == NULL) {
 		pr_err("%s: kmalloc failed\n", __func__);
-		return -ENOMEM;
+		ret = -ENOMEM;
+		goto done;
 	}
 
 	memset(prtd, 0, sizeof(struct voice_svc_prvt));
@@ -630,6 +641,7 @@ static int voice_svc_open(struct inode *inode, struct file *file)
 	mutex_init(&prtd->response_mutex_lock);
 	file->private_data = (void *)prtd;
 
+	is_released = 0;
 	/* Current APR implementation doesn't support session based
 	 * multiple service registrations. The apr_deregister()
 	 * function sets the destination and client IDs to zero, if
@@ -640,7 +652,9 @@ static int voice_svc_open(struct inode *inode, struct file *file)
 		voice_svc_dummy_reg();
 		reg_dummy_sess = 1;
 	}
-	return 0;
+done:
+	mutex_unlock(&session_lock);
+	return ret;
 }
 
 static int voice_svc_release(struct inode *inode, struct file *file)
@@ -698,6 +712,7 @@ static int voice_svc_release(struct inode *inode, struct file *file)
 
 	kfree(file->private_data);
 	file->private_data = NULL;
+	is_released = 1;
 
 done:
 	return ret;
@@ -767,6 +782,8 @@ static int voice_svc_probe(struct platform_device *pdev)
 		goto add_err;
 	}
 	pr_debug("%s: Device created\n", __func__);
+
+	mutex_init(&session_lock);
 	goto done;
 
 add_err:
@@ -789,6 +806,7 @@ static int voice_svc_remove(struct platform_device *pdev)
 	kfree(voice_svc_dev->cdev);
 	device_destroy(voice_svc_class, device_num);
 	class_destroy(voice_svc_class);
+	mutex_destroy(&session_lock);
 	unregister_chrdev_region(0, MINOR_NUMBER);
 
 	return 0;
