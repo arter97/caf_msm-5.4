ee1ea7c906a95fbe5401869369806dc983aa629c
Arun Prakash <quic_app@quicinc.com>
2019-04-24 21:11:06 +0530

    diag: Check command size against the minimum before parsing
    Add check for minimum length before typecasting to build mask
    structure to prevent out of bound access while processing
    get msg mask command.
    
    CRs-Fixed: 2431047
    Change-Id: I5b8341f278b0b46359800e43c604c5671261c728
    Signed-off-by: Manoj Prabhu B <bmanoj@codeaurora.org>
    Signed-off-by: Arun Prakash <quic_app@quicinc.com>

--- drivers/char/diag/diag_masks.c
+++ drivers/char/diag/diag_masks.c
@@ -1,4 +1,5 @@
 /* Copyright (c) 2008-2016, The Linux Foundation. All rights reserved.
+ * Copyright (c) 2022, Qualcomm Innovation Center, Inc. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -586,7 +587,7 @@ static int diag_cmd_get_msg_mask(unsigned char *src_buf, int src_len,
 
 	mask_info = (!info) ? &msg_mask : info->msg_mask;
 	if (!src_buf || !dest_buf || dest_len <= 0 ||
-	    !mask_info) {
+	    !mask_info || (src_len < sizeof(struct diag_build_mask_req_t))) {
 		pr_err("diag: Invalid input in %s, src_buf: %pK, src_len: %d, dest_buf: %pK, dest_len: %d, mask_info: %pK\n",
 		       __func__, src_buf, src_len, dest_buf, dest_len,
 		       mask_info);
