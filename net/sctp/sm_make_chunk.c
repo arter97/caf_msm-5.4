e564220b1f94bd26797f8d8a8d5b51fb2f16cff8
Joe Perches <joe@perches.com>
2019-10-05 09:46:41 -0700

    net: sctp: Rename fallthrough label to unhandled
    fallthrough will become a pseudo reserved keyword so this only use of
    fallthrough is better renamed to allow it.
    
    Signed-off-by: Joe Perches <joe@perches.com>
    Reviewed-by: Nick Desaulniers <ndesaulniers@google.com>
    Reviewed-by: Kees Cook <keescook@chromium.org>
    Acked-by: Neil Horman <nhorman@tuxdriver.com>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    (cherry picked from commit 48f9bcf91461b43249949f4e172b5c0245dde751)
    
    Change-Id: Ibf41b1586eec5f749314a318d1ea0c6db1bb929f
    Signed-off-by: vbejawad <quic_vbejawad@quicinc.com>

--- net/sctp/sm_make_chunk.c
+++ net/sctp/sm_make_chunk.c
@@ -2158,7 +2158,7 @@ static sctp_ierror_t sctp_verify_param(struct net *net,
 	case SCTP_PARAM_SET_PRIMARY:
 		if (ep->asconf_enable)
 			break;
-		goto fallthrough;
+		goto unhandled;
 
 	case SCTP_PARAM_HOST_NAME_ADDRESS:
 		/* Tell the peer, we won't support this param.  */
@@ -2169,11 +2169,11 @@ static sctp_ierror_t sctp_verify_param(struct net *net,
 	case SCTP_PARAM_FWD_TSN_SUPPORT:
 		if (ep->prsctp_enable)
 			break;
-		goto fallthrough;
+		goto unhandled;
 
 	case SCTP_PARAM_RANDOM:
 		if (!ep->auth_enable)
-			goto fallthrough;
+			goto unhandled;
 
 		/* SCTP-AUTH: Secion 6.1
 		 * If the random number is not 32 byte long the association
@@ -2190,7 +2190,7 @@ static sctp_ierror_t sctp_verify_param(struct net *net,
 
 	case SCTP_PARAM_CHUNKS:
 		if (!ep->auth_enable)
-			goto fallthrough;
+			goto unhandled;
 
 		/* SCTP-AUTH: Section 3.2
 		 * The CHUNKS parameter MUST be included once in the INIT or
@@ -2206,7 +2206,7 @@ static sctp_ierror_t sctp_verify_param(struct net *net,
 
 	case SCTP_PARAM_HMAC_ALGO:
 		if (!ep->auth_enable)
-			goto fallthrough;
+			goto unhandled;
 
 		hmacs = (struct sctp_hmac_algo_param *)param.p;
 		n_elt = (ntohs(param.p->length) - sizeof(sctp_paramhdr_t)) >> 1;
@@ -2228,7 +2228,7 @@ static sctp_ierror_t sctp_verify_param(struct net *net,
 			retval = SCTP_IERROR_ABORT;
 		}
 		break;
-fallthrough:
+unhandled:
 	default:
 		pr_debug("%s: unrecognized param:%d for chunk:%d\n",
 			 __func__, ntohs(param.p->type), cid);
