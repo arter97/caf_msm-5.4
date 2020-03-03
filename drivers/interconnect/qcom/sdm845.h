6f085d0dc8d597752552939cb7323ae25bb53b38
Sibi Sankar <sibis@codeaurora.org>
2020-03-03 19:02:52 +0200

    interconnect: qcom: Add OSM L3 interconnect provider support
    On some Qualcomm SoCs, Operating State Manager (OSM) controls the
    resources of scaling L3 caches. Add a driver to handle bandwidth
    requests to OSM L3 from CPU on SDM845 SoCs.
    
    Change-Id: If210b7d996428b66996092851b5987f870a650db
    Signed-off-by: Sibi Sankar <sibis@codeaurora.org>
    Reviewed-by: Evan Green <evgreen@chromium.org>
    Link: https://lore.kernel.org/r/20200227105632.15041-4-sibis@codeaurora.org
    Signed-off-by: Georgi Djakov <georgi.djakov@linaro.org>
    Git-commit: 5bc9900addafb99163232dbb970eb07661ae4178
    Git-repo: https://kernel.googlesource.com/pub/scm/linux/kernel/git/next/linux-next
    [vvegivad@codeaurora.org: Added aggreate function and minor modifcations
    as per the framework]
    Signed-off-by: Veera Vegivada <vvegivad@codeaurora.org>

---
+++ drivers/interconnect/qcom/sdm845.h
@@ -0,0 +1,12 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+/*
+ * Copyright (c) 2020, The Linux Foundation. All rights reserved.
+ */
+
+#ifndef __DRIVERS_INTERCONNECT_QCOM_SDM845_H
+#define __DRIVERS_INTERCONNECT_QCOM_SDM845_H
+
+#define SDM845_MASTER_OSM_L3_APPS		130
+#define SDM845_SLAVE_OSM_L3			131
+
+#endif
