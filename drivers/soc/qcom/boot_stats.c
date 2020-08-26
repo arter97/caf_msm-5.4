4114a111444f832246c41fefc233073b8f192c89
Saurabh Ambulkar <sambulka@codeaurora.org>
2020-08-26 10:02:29 +0530

    msm: boot markers: Add boot markers driver
    Add boot time marker driver and made support for boot stats, It is
    a snapshot from 4.14 with the commit id '4a32bc94f44048'.
    
    Change-Id: Ib1aece617a5863c85fdd3f70daa3fc5da0d627a5
    Signed-off-by: Saurabh Ambulkar <sambulka@codeaurora.org>

--- drivers/soc/qcom/boot_stats.c
+++ drivers/soc/qcom/boot_stats.c
@@ -15,17 +15,12 @@
 #include <linux/sched.h>
 #include <linux/of.h>
 #include <linux/of_address.h>
-
-struct boot_stats {
-	uint32_t bootloader_start;
-	uint32_t bootloader_end;
-	uint32_t bootloader_display;
-	uint32_t bootloader_load_kernel;
-};
+#include <soc/qcom/boot_stats.h>
 
 static void __iomem *mpm_counter_base;
+static phys_addr_t mpm_counter_pa;
 static uint32_t mpm_counter_freq;
-static struct boot_stats __iomem *boot_stats;
+struct boot_stats __iomem *boot_stats;
 
 static int mpm_parse_dt(void)
 {
@@ -74,20 +69,61 @@ err1:
 static void print_boot_stats(void)
 {
 	pr_info("KPI: Bootloader start count = %u\n",
-		readl_relaxed(&boot_stats->bootloader_start));
+			readl_relaxed(&boot_stats->bootloader_start));
 	pr_info("KPI: Bootloader end count = %u\n",
-		readl_relaxed(&boot_stats->bootloader_end));
-	pr_info("KPI: Bootloader display count = %u\n",
-		readl_relaxed(&boot_stats->bootloader_display));
+			readl_relaxed(&boot_stats->bootloader_end));
 	pr_info("KPI: Bootloader load kernel count = %u\n",
-		readl_relaxed(&boot_stats->bootloader_load_kernel));
+			readl_relaxed(&boot_stats->load_kernel_done) -
+			readl_relaxed(&boot_stats->load_kernel_start));
 	pr_info("KPI: Kernel MPM timestamp = %u\n",
-		readl_relaxed(mpm_counter_base));
+			readl_relaxed(mpm_counter_base));
 	pr_info("KPI: Kernel MPM Clock frequency = %u\n",
-		mpm_counter_freq);
+			mpm_counter_freq);
 }
 
-static int __init boot_stats_init(void)
+unsigned long long msm_timer_get_sclk_ticks(void)
+{
+	unsigned long long t1, t2;
+	int loop_count = 10;
+	int loop_zero_count = 3;
+	u64 tmp = USEC_PER_SEC;
+	void __iomem *sclk_tick;
+
+	do_div(tmp, TIMER_KHZ);
+	tmp /= (loop_zero_count-1);
+	sclk_tick = mpm_counter_base;
+	if (!sclk_tick)
+		return -EINVAL;
+
+	while (loop_zero_count--) {
+		t1 = readl_no_log(sclk_tick);
+		do {
+			udelay(1);
+			t2 = t1;
+			t1 = readl_no_log(sclk_tick);
+		} while ((t2 != t1) && --loop_count);
+		if (!loop_count) {
+			pr_err("boot_stats: SCLK  did not stabilize\n");
+			return 0;
+		}
+		if (t1)
+			break;
+
+		udelay(tmp);
+	}
+	if (!loop_zero_count) {
+		pr_err("boot_stats: SCLK reads zero\n");
+		return 0;
+	}
+	return t1;
+}
+
+phys_addr_t msm_timer_get_pa(void)
+{
+	return mpm_counter_pa;
+}
+
+int boot_stats_init(void)
 {
 	int ret;
 
@@ -97,17 +133,16 @@ static int __init boot_stats_init(void)
 
 	print_boot_stats();
 
-	iounmap(boot_stats);
-	iounmap(mpm_counter_base);
+	if (!(boot_marker_enabled()))
+		boot_stats_exit();
 
 	return 0;
 }
-module_init(boot_stats_init);
 
-static void __exit boot_stats_exit(void)
+int boot_stats_exit(void)
 {
+	iounmap(boot_stats);
+	iounmap(mpm_counter_base);
+	return 0;
 }
-module_exit(boot_stats_exit)
 
-MODULE_DESCRIPTION("MSM boot stats info driver");
-MODULE_LICENSE("GPL v2");
