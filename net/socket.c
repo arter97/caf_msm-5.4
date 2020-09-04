64fae59bc5d4e725b72ac3dbfcbeee507812bc43
Sharath Chandra Vurukala <sharathv@codeaurora.org>
2020-09-04 20:20:25 +0530

    net:sockev: hold file reference till the sock event is sent
    Hold file reference till the sock event is sent.
    
    Change-Id: I7c9bbea9da476560be255038a5b377a7482a0bdd
    Signed-off-by: Sharath Chandra Vurukala <sharathv@codeaurora.org>
    Signed-off-by: rkamble <quic_rkamble@quicinc.com>

--- net/socket.c
+++ net/socket.c
@@ -1568,13 +1568,9 @@ SYSCALL_DEFINE3(bind, int, fd, struct sockaddr __user *, umyaddr, int, addrlen)
 						      (struct sockaddr *)
 						      &address, addrlen);
 		}
-		if (!err) {
-			if (sock->sk)
-				sock_hold(sock->sk);
+		if (!err)
 			sockev_notify(SOCKEV_BIND, sock);
-			if (sock->sk)
-				sock_put(sock->sk);
-		}
+
 		fput_light(sock->file, fput_needed);
 	}
 	return err;
@@ -1602,13 +1598,9 @@ SYSCALL_DEFINE2(listen, int, fd, int, backlog)
 		if (!err)
 			err = sock->ops->listen(sock, backlog);
 
-		if (!err) {
-			if (sock->sk)
-				sock_hold(sock->sk);
+		if (!err)
 			sockev_notify(SOCKEV_LISTEN, sock);
-			if (sock->sk)
-				sock_put(sock->sk);
-		}
+
 		fput_light(sock->file, fput_needed);
 	}
 	return err;
