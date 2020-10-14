3d994da58d2ef31be1612c2d8709d16a73f44c9a
Upendra Chhipa <uchhipa@codeaurora.org>
2020-10-13 21:48:34 -0700

    Revert "cfg80211/nl80211: Kernel support to enable SAE and OWE"
    This reverts commit 23e9a9b7ab09c69b3ba2650566846449d7f69eb7.
    
    Change-Id: I019d920da9fa2f3df66d1687eb9ba440ad3bd7bb
    Signed-off-by: Upendra Chhipa <uchhipa@codeaurora.org>

--- net/wireless/rdev-ops.h
+++ net/wireless/rdev-ops.h
@@ -966,31 +966,4 @@ rdev_del_tx_ts(struct cfg80211_registered_device *rdev,
 	return ret;
 }
 
-static inline int
-rdev_external_auth(struct cfg80211_registered_device *rdev,
-		   struct net_device *dev,
-		   struct cfg80211_external_auth_params *params)
-{
-	int ret = -EOPNOTSUPP;
-
-	trace_rdev_external_auth(&rdev->wiphy, dev, params);
-	if (rdev->ops->external_auth)
-		ret = rdev->ops->external_auth(&rdev->wiphy, dev, params);
-	trace_rdev_return_int(&rdev->wiphy, ret);
-	return ret;
-}
-
-static inline int rdev_update_owe_info(struct cfg80211_registered_device *rdev,
-				       struct net_device *dev,
-				       struct cfg80211_update_owe_info *oweinfo)
-{
-	int ret = -EOPNOTSUPP;
-
-	trace_rdev_update_owe_info(&rdev->wiphy, dev, oweinfo);
-	if (rdev->ops->update_owe_info)
-		ret = rdev->ops->update_owe_info(&rdev->wiphy, dev, oweinfo);
-	trace_rdev_return_int(&rdev->wiphy, ret);
-	return ret;
-}
-
 #endif /* __CFG80211_RDEV_OPS */
