964f14643b591e84c398c8a032e09768bb9a9811
Yingzhong.Zha <yzha@codeaurora.org>
2021-01-14 14:25:49 +0800

    Revert "drivers: soc: secure Revert tracking of hyp-assing pages"
    This reverts commit 85e4663ae1dc31fbea4be4566939a6d330e12a80.
    
    Change-Id: I37338fc1e81b259a43bf8dde34b1a3d693e5f597
    Signed-off-by: Yingzhong.Zha <yzha@codeaurora.org>

--- drivers/soc/qcom/secure_buffer.c
+++ drivers/soc/qcom/secure_buffer.c
@@ -239,6 +239,10 @@ int hyp_assign_table(struct sg_table *table,
 				break;
 		}
 
+		if (dest_nelems == -1)
+			set_each_page_of_sg(table, SECURE_PAGE_MAGIC);
+		else
+			set_each_page_of_sg(table, 0);
 	}
 
 
