UtsavBalar1231/kernel_xiaomi_raphael

init: support early userspace
We define a new platform driver whose purpose is to start
our early userspace solution naturally during the original
linux booting sequence. By defining our early-devices node
as the first node under /soc in device tree, our driver
will start in level 3 sync stage around the very beginning
of of_platform_default_populate_init. First we populate
early devices that are commonly needed by all subsystems
and install their drivers so these devices will be probed.
We wait and sync after this until all platform devices
have been populated before we start all subsystems. Each
subsystem runs in a separate thread so runs in parallel.
We guarantee devices for each subsystem are populated
before we install their drivers in each thread so these
devices will be probed in each separate thread for their
subsystem. This way, our multiple cpu cores will be fully
utilized and linux booting progress will be very fast. And
userspace applications will start much earlier which means
certain user experience will be improved.

Change-Id: Ic99441706b81ac3c48dc0a1e999bcdd0cfc5c1d2
Signed-off-by: Jiacheng Zheng <jiaczhen@codeaurora.org>

init/early_userspace.c
@@ -12,11 +12,23 @@
 
 
 #include <linux/init.h>
+#include <linux/completion.h>
+#include <linux/async.h>
 #include <linux/platform_device.h>
 #include <linux/module.h>
 #include <linux/of.h>
 #include <linux/of_device.h>
+#include <linux/kallsyms.h>
+#include <linux/slab.h>
 
+struct subsystem_work {
+	struct work_struct work;
+	int id;
+};
+
+static DECLARE_COMPLETION(populate_done);
+static DECLARE_COMPLETION(subsys_done);
+static DECLARE_COMPLETION(plat_subsys_done);
 bool is_early_userspace;
 EXPORT_SYMBOL(is_early_userspace);
 
@@ -26,3 +38,147 @@ static int __init early_userspace(char *p)
 	return 0;
 }
 early_param("early_userspace", early_userspace);
+
+static initcall_t *early_initcall_levels[] = {
+	__early0_initcall_start,
+	__early1_initcall_start,
+	__early2_initcall_start,
+	__early3_initcall_start,
+	__early4_initcall_start,
+	__early5_initcall_start,
+	__early6_initcall_start,
+	__early7_initcall_start,
+	__early_initcall_end,
+};
+
+static void do_early_subsys_init(int id)
+{
+	int ret;
+	initcall_t *fn;
+
+	for (fn = early_initcall_levels[id]; fn < early_initcall_levels[id+1];
+		fn++) {
+		ret = (*fn)();
+		if (ret) {
+			print_ip_sym(*((unsigned long *)fn));
+			pr_err("fails with %d\n", ret);
+		}
+	}
+}
+
+static void early_subsys_init(struct work_struct *w)
+{
+	struct subsystem_work *subsys_work =
+		container_of(w, struct subsystem_work, work);
+
+	do_early_subsys_init(subsys_work->id);
+	kfree(subsys_work);
+}
+
+static void early_system_init(struct work_struct *w)
+{
+	struct subsystem_work *subsys_work;
+	int id;
+
+	do_early_subsys_init(EARLY_SUBSYS_PLATFORM);
+	complete(&plat_subsys_done);
+	pr_info("early_common_platform initialized\n");
+	wait_for_completion(&populate_done);
+	pr_info("early_subsystems starting\n");
+
+	for (id = EARLY_SUBSYS_1; id < EARLY_SUBSYS_NUM; id++) {
+		subsys_work = kzalloc(sizeof(struct subsystem_work),
+			GFP_KERNEL);
+		if (subsys_work) {
+			subsys_work->id = id;
+			INIT_WORK(&subsys_work->work, early_subsys_init);
+			queue_work_on(WORK_CPU_UNBOUND, system_highpri_wq,
+				&subsys_work->work);
+		} else {
+			pr_err("no mem to start early_subsys_init\n");
+		}
+	}
+	kfree(w);
+}
+
+static const struct of_device_id early_devices_match_table[] = {
+	{ .compatible = "qcom,early-devices" },
+	{ }
+};
+MODULE_DEVICE_TABLE(of, early_devices_match_table);
+
+static int early_devices_probe(struct platform_device *pdev)
+{
+	struct device_node *early_node;
+	int i, len = 0;
+	struct work_struct *work;
+	struct device *dev = &pdev->dev;
+	struct device_node *np = dev->of_node;
+
+	of_find_property(np, "devices", &len);
+	for (i = 0; i < len / sizeof(u32); i++) {
+		early_node = of_parse_phandle(np, "devices", i);
+		of_platform_node_probe(early_node, pdev->dev.parent);
+		of_node_put(early_node);
+	}
+	work = kzalloc(sizeof(struct work_struct), GFP_KERNEL);
+	if (work) {
+		INIT_WORK(work, early_system_init);
+		queue_work_on(WORK_CPU_UNBOUND, system_highpri_wq, work);
+	} else {
+		pr_err("no mem to start early_system_init\n");
+	}
+
+	return 0;
+}
+
+static struct platform_driver early_devices_driver = {
+	.probe		= early_devices_probe,
+	.driver		= {
+		.name	= "early-devices",
+		.of_match_table = early_devices_match_table,
+	},
+};
+
+static int __init early_devices_init(void)
+{
+	if (is_early_userspace)
+		return platform_driver_register(&early_devices_driver);
+	else
+		return 0;
+}
+arch_initcall(early_devices_init);
+
+static void __exit early_devices_exit(void)
+{
+	if (is_early_userspace)
+		platform_driver_unregister(&early_devices_driver);
+}
+module_exit(early_devices_exit);
+
+static int __init early_populate_sync(void)
+{
+	complete(&populate_done);
+	pr_info("early populate_sync start\n");
+	if (is_early_userspace)
+		wait_for_completion(&plat_subsys_done);
+	pr_info("early populate_sync end\n");
+	return 0;
+}
+subsys_initcall(early_populate_sync);
+
+static int __init early_subsys_sync(void)
+{
+	complete(&subsys_done);
+	return 0;
+}
+subsys_initcall_sync(early_subsys_sync);
+
+static int __init early_subsys_wait(void)
+{
+	pr_info("early subsys wait start\n");
+	wait_for_completion(&subsys_done);
+	pr_info("early subsys wait end\n");
+	return 0;
+}
+early_init(early_subsys_wait, EARLY_SUBSYS_1, EARLY_INIT_LEVEL7);

arter97/caf_msm-5.4

init/early_userspace.c
@@ -12,11 +12,23 @@
 
 
 #include <linux/init.h>
+#include <linux/completion.h>
+#include <linux/async.h>
 #include <linux/platform_device.h>
 #include <linux/module.h>
 #include <linux/of.h>
 #include <linux/of_device.h>
+#include <linux/kallsyms.h>
+#include <linux/slab.h>
 
+struct subsystem_work {
+	struct work_struct work;
+	int id;
+};
+
+static DECLARE_COMPLETION(populate_done);
+static DECLARE_COMPLETION(subsys_done);
+static DECLARE_COMPLETION(plat_subsys_done);
 bool is_early_userspace;
 EXPORT_SYMBOL(is_early_userspace);
 
@@ -26,3 +38,147 @@ static int __init early_userspace(char *p)
 	return 0;
 }
 early_param("early_userspace", early_userspace);
+
+static initcall_entry_t *early_initcall_levels[] = {
+	__early0_initcall_start,
+	__early1_initcall_start,
+	__early2_initcall_start,
+	__early3_initcall_start,
+	__early4_initcall_start,
+	__early5_initcall_start,
+	__early6_initcall_start,
+	__early7_initcall_start,
+	__early_initcall_end,
+};
+
+static void do_early_subsys_init(int id)
+{
+	int ret;
+	initcall_entry_t *fn;
+
+	for (fn = early_initcall_levels[id]; fn < early_initcall_levels[id+1];
+		fn++) {
+		ret = (*initcall_from_entry(fn))();
+		if (ret) {
+			print_ip_sym(*((unsigned long *)fn));
+			pr_err("fails with %d\n", ret);
+		}
+	}
+}
+
+static void early_subsys_init(struct work_struct *w)
+{
+	struct subsystem_work *subsys_work =
+		container_of(w, struct subsystem_work, work);
+
+	do_early_subsys_init(subsys_work->id);
+	kfree(subsys_work);
+}
+
+static void early_system_init(struct work_struct *w)
+{
+	struct subsystem_work *subsys_work;
+	int id;
+
+	do_early_subsys_init(EARLY_SUBSYS_PLATFORM);
+	complete(&plat_subsys_done);
+	pr_info("early_common_platform initialized\n");
+	wait_for_completion(&populate_done);
+	pr_info("early_subsystems starting\n");
+
+	for (id = EARLY_SUBSYS_1; id < EARLY_SUBSYS_NUM; id++) {
+		subsys_work = kzalloc(sizeof(struct subsystem_work),
+			GFP_KERNEL);
+		if (subsys_work) {
+			subsys_work->id = id;
+			INIT_WORK(&subsys_work->work, early_subsys_init);
+			queue_work_on(WORK_CPU_UNBOUND, system_unbound_wq,
+				&subsys_work->work);
+		} else {
+			pr_err("no mem to start early_subsys_init\n");
+		}
+	}
+	kfree(w);
+}
+
+static const struct of_device_id early_devices_match_table[] = {
+	{ .compatible = "early-devices" },
+	{ }
+};
+MODULE_DEVICE_TABLE(of, early_devices_match_table);
+
+static int early_devices_probe(struct platform_device *pdev)
+{
+	struct device_node *early_node;
+	int i, len = 0;
+	struct work_struct *work;
+	struct device *dev = &pdev->dev;
+	struct device_node *np = dev->of_node;
+
+	of_find_property(np, "devices", &len);
+	for (i = 0; i < len / sizeof(u32); i++) {
+		early_node = of_parse_phandle(np, "devices", i);
+		of_platform_node_probe(early_node, pdev->dev.parent);
+		of_node_put(early_node);
+	}
+	work = kzalloc(sizeof(struct work_struct), GFP_KERNEL);
+	if (work) {
+		INIT_WORK(work, early_system_init);
+		queue_work_on(WORK_CPU_UNBOUND, system_unbound_wq, work);
+	} else {
+		pr_err("no mem to start early_system_init\n");
+	}
+
+	return 0;
+}
+
+static struct platform_driver early_devices_driver = {
+	.probe		= early_devices_probe,
+	.driver		= {
+		.name	= "early-devices",
+		.of_match_table = early_devices_match_table,
+	},
+};
+
+static int __init early_devices_init(void)
+{
+	if (is_early_userspace)
+		return platform_driver_register(&early_devices_driver);
+	else
+		return 0;
+}
+arch_initcall(early_devices_init);
+
+static void __exit early_devices_exit(void)
+{
+	if (is_early_userspace)
+		platform_driver_unregister(&early_devices_driver);
+}
+module_exit(early_devices_exit);
+
+static int __init early_populate_sync(void)
+{
+	complete(&populate_done);
+	pr_info("early populate_sync start\n");
+	if (is_early_userspace)
+		wait_for_completion(&plat_subsys_done);
+	pr_info("early populate_sync end\n");
+	return 0;
+}
+subsys_initcall(early_populate_sync);
+
+static int __init early_subsys_sync(void)
+{
+	complete(&subsys_done);
+	return 0;
+}
+subsys_initcall_sync(early_subsys_sync);
+
+static int __init early_subsys_wait(void)
+{
+	pr_info("early subsys wait start\n");
+	wait_for_completion(&subsys_done);
+	pr_info("early subsys wait end\n");
+	return 0;
+}
+early_init(early_subsys_wait, EARLY_SUBSYS_1, EARLY_INIT_LEVEL7);