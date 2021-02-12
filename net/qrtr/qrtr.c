d0ecac2f426854d1f41745868906af107c51cf17
Arun Prakash <app@codeaurora.org>
2021-02-12 15:43:01 +0530

    qrtr: Enhance qrtr to carry filters for transport filtering
    Enhance qrtr to carry eBPF filter program and execute the filter
    program on different qrtr events. Packet forwarding decision will
    be taken based on the status returned by the eBPF filter program.
    
    QMI data packets from clients will be dropped if the client doesn't
    have enough permission to communicate with qmi service. Similarly
    NEW SERVER control message will not forward/receive to/from connected
    soc if that service broadcast is restricted to local soc hop.
    
    Change-Id: I3f5dbe01fb1926e688598250551d085de5697ebf
    Signed-off-by: Arun Prakash <app@codeaurora.org>

--- net/qrtr/qrtr.c
+++ net/qrtr/qrtr.c
@@ -635,6 +635,9 @@ static int qrtr_node_enqueue(struct qrtr_node *node, struct sk_buff *skb,
 			     int type, struct sockaddr_qrtr *from,
 			     struct sockaddr_qrtr *to, unsigned int flags)
 {
+#if IS_ENABLED(CONFIG_QRTR_BPF_FILTER)
+	struct qrtr_ctrl_pkt pkt = {0,};
+#endif
 	struct qrtr_hdr_v1 *hdr;
 	size_t len = skb->len;
 	int rc = -ENODEV;
@@ -676,6 +679,22 @@ static int qrtr_node_enqueue(struct qrtr_node *node, struct sk_buff *skb,
 	hdr->size = cpu_to_le32(len);
 	hdr->confirm_rx = !!confirm_rx;
 
+#if IS_ENABLED(CONFIG_QRTR_BPF_FILTER)
+	if (type == QRTR_TYPE_NEW_SERVER) {
+		skb_copy_bits(skb, QRTR_HDR_MAX_SIZE, &pkt, sizeof(pkt));
+		/**
+		 * Run qrtr filter to drop the restricted new server message
+		 * that is being transmitted to connected soc
+		 */
+		rc = qrtr_run_bpf_filter(skb, le32_to_cpu(pkt.server.service),
+					 le32_to_cpu(pkt.server.instance),
+					 type, le32_to_cpu(hdr->dst_node_id));
+		if (rc) {
+			kfree_skb(skb);
+			return rc;
+		}
+	}
+#endif
 	qrtr_log_tx_msg(node, hdr, skb);
 	rc = skb_put_padto(skb, ALIGN(len, 4) + sizeof(*hdr));
 	if (rc) {
@@ -1155,6 +1174,10 @@ static void qrtr_node_rx_work(struct kthread_work *work)
 	struct sk_buff *skb;
 	char name[32] = {0,};
 
+#if IS_ENABLED(CONFIG_QRTR_BPF_FILTER)
+	struct qrtr_ctrl_pkt pkt = {0,};
+	int rc;
+#endif
 	if (unlikely(!node->ilc)) {
 		snprintf(name, sizeof(name), "qrtr_%d", node->nid);
 		node->ilc = ipc_log_context_create(QRTR_LOG_PAGE_CNT, name, 0);
@@ -1167,6 +1190,26 @@ static void qrtr_node_rx_work(struct kthread_work *work)
 		if (cb->type != QRTR_TYPE_DATA)
 			qrtr_fwd_ctrl_pkt(node, skb);
 
+#if IS_ENABLED(CONFIG_QRTR_BPF_FILTER)
+		if (cb->type == QRTR_TYPE_NEW_SERVER &&
+		    skb->len == sizeof(pkt)) {
+			skb_copy_bits(skb, 0, &pkt, sizeof(pkt));
+
+			/**
+			 * Run qrtr filter to drop the restricted new server
+			 * message that is being transmitted from connected soc
+			 */
+			rc = qrtr_run_bpf_filter
+				(skb,
+				 le32_to_cpu(pkt.server.service),
+				 le32_to_cpu(pkt.server.instance),
+				 cb->type, cb->src_node);
+			if (rc) {
+				kfree_skb(skb);
+				continue;
+			}
+		}
+#endif
 		if (cb->type == QRTR_TYPE_RESUME_TX) {
 			if (cb->dst_node != qrtr_local_nid) {
 				qrtr_fwd_pkt(skb, cb);
@@ -1798,6 +1841,10 @@ static int qrtr_sendmsg(struct socket *sock, struct msghdr *msg, size_t len)
 	u32 type;
 	int rc;
 
+#if IS_ENABLED(CONFIG_QRTR_BPF_FILTER)
+	struct service_info *info;
+#endif
+
 	if (msg->msg_flags & ~(MSG_DONTWAIT))
 		return -EINVAL;
 
@@ -1869,6 +1916,20 @@ static int qrtr_sendmsg(struct socket *sock, struct msghdr *msg, size_t len)
 		goto out_node;
 	}
 
+#if IS_ENABLED(CONFIG_QRTR_BPF_FILTER)
+	/* look up service information from service radix tree */
+	rc = qrtr_service_lookup(addr->sq_node, addr->sq_port, &info);
+	/* run bpf filter only if the sender is a valid qmi client */
+	if (!rc && ipc->us.sq_port != QRTR_PORT_CTRL) {
+		rc = qrtr_run_bpf_filter(skb, info->service_id,
+					 info->instance_id,
+					 QRTR_TYPE_DATA, 0);
+		if (rc) {
+			kfree_skb(skb);
+			goto out_node;
+		}
+	}
+#endif
 	if (ipc->us.sq_port == QRTR_PORT_CTRL ||
 	    addr->sq_port == QRTR_PORT_CTRL) {
 		if (len < 4) {
