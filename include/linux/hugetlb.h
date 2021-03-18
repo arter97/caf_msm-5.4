ee5de79c773b06460ab8399183dbb1f104ffe575
Axel Rasmussen <axelrasmussen@google.com>
2021-03-18 17:01:50 +1100

    BACKPORT: FROMGIT: userfaultfd: add UFFDIO_CONTINUE ioctl
    This ioctl is how userspace ought to resolve "minor" userfaults. The
    idea is, userspace is notified that a minor fault has occurred. It might
    change the contents of the page using its second non-UFFD mapping, or
    not. Then, it calls UFFDIO_CONTINUE to tell the kernel "I have ensured
    the page contents are correct, carry on setting up the mapping".
    
    Note that it doesn't make much sense to use UFFDIO_{COPY,ZEROPAGE} for
    MINOR registered VMAs. ZEROPAGE maps the VMA to the zero page; but in
    the minor fault case, we already have some pre-existing underlying page.
    Likewise, UFFDIO_COPY isn't useful if we have a second non-UFFD mapping.
    We'd just use memcpy() or similar instead.
    
    It turns out hugetlb_mcopy_atomic_pte() already does very close to what
    we want, if an existing page is provided via `struct page **pagep`. We
    already special-case the behavior a bit for the UFFDIO_ZEROPAGE case, so
    just extend that design: add an enum for the three modes of operation,
    and make the small adjustments needed for the MCOPY_ATOMIC_CONTINUE
    case. (Basically, look up the existing page, and avoid adding the
    existing page to the page cache or calling set_page_huge_active() on
    it.)
    
    Link: https://lkml.kernel.org/r/20210301222728.176417-5-axelrasmussen@google.com
    Signed-off-by: Axel Rasmussen <axelrasmussen@google.com>
    Reviewed-by: Peter Xu <peterx@redhat.com>
    Cc: Adam Ruprecht <ruprecht@google.com>
    Cc: Alexander Viro <viro@zeniv.linux.org.uk>
    Cc: Alexey Dobriyan <adobriyan@gmail.com>
    Cc: Andrea Arcangeli <aarcange@redhat.com>
    Cc: Anshuman Khandual <anshuman.khandual@arm.com>
    Cc: Cannon Matthews <cannonmatthews@google.com>
    Cc: Catalin Marinas <catalin.marinas@arm.com>
    Cc: Chinwen Chang <chinwen.chang@mediatek.com>
    Cc: David Rientjes <rientjes@google.com>
    Cc: "Dr . David Alan Gilbert" <dgilbert@redhat.com>
    Cc: Huang Ying <ying.huang@intel.com>
    Cc: Ingo Molnar <mingo@redhat.com>
    Cc: Jann Horn <jannh@google.com>
    Cc: Jerome Glisse <jglisse@redhat.com>
    Cc: Kirill A. Shutemov <kirill@shutemov.name>
    Cc: Lokesh Gidra <lokeshgidra@google.com>
    Cc: "Matthew Wilcox (Oracle)" <willy@infradead.org>
    Cc: Michael Ellerman <mpe@ellerman.id.au>
    Cc: "Michal Koutn" <mkoutny@suse.com>
    Cc: Michel Lespinasse <walken@google.com>
    Cc: Mike Kravetz <mike.kravetz@oracle.com>
    Cc: Mike Rapoport <rppt@linux.vnet.ibm.com>
    Cc: Mina Almasry <almasrymina@google.com>
    Cc: Nicholas Piggin <npiggin@gmail.com>
    Cc: Oliver Upton <oupton@google.com>
    Cc: Shaohua Li <shli@fb.com>
    Cc: Shawn Anastasio <shawn@anastas.io>
    Cc: Steven Price <steven.price@arm.com>
    Cc: Steven Rostedt <rostedt@goodmis.org>
    Cc: Vlastimil Babka <vbabka@suse.cz>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Stephen Rothwell <sfr@canb.auug.org.au>
    
    (cherry picked from commit 14ea86439abaf3423cd9b6712ed5ce8451d2d181
    https://git.kernel.org/pub/scm/linux/kernel/git/next/linux-next.git akpm)
    Link: https://lore.kernel.org/patchwork/patch/1388136/
    
    Conflicts: fs/userfaultfd.c
    	include/linux/hugetlb.h
    	include/linux/userfaultfd_k.h
    	include/uapi/linux/userfaultfd.h
    	mm/hugetlb.c
    	mm/userfaultfd.c
    (1. 8f251a3d5ce3bdea73bd045ed35db64f32e0d0d9 is not cherry-picked yet so
        switched SetHPageMigratable() to set_active_huge_page() in
        mm/hugetlb.c,
    2. Other files conflicts due to lack of write-protect userfaultfd
    support. Manually rebased accordingly
    3. Included linux/mm.h in linux/userfaultfd_k.h for definitions of
    VM_UFFD_*)
    
    Signed-off-by: Axel Rasmussen<axelrasmussen@google.com>
    Signed-off-by: Lokesh Gidra <lokeshgidra@google.com>
    Bug: 160737021
    Bug: 169683130
    
    Git-commit:  b69f713e60d03ae448e5c9fd92e5b0b193dea7be
    Git-repo: https://android.googlesource.com/kernel/common
    
    Change-Id: I45b62959dcb1d343154cb831113a26e47e77c8af
    Signed-off-by: Ashok Gummadidala <quic_c_agumma@quicinc.com>

--- include/linux/hugetlb.h
+++ include/linux/hugetlb.h
@@ -10,6 +10,7 @@
 #include <linux/list.h>
 #include <linux/kref.h>
 #include <asm/pgtable.h>
+#include <linux/userfaultfd_k.h>
 
 struct ctl_table;
 struct user_struct;
@@ -93,6 +94,7 @@ int hugetlb_mcopy_atomic_pte(struct mm_struct *dst_mm, pte_t *dst_pte,
 				struct vm_area_struct *dst_vma,
 				unsigned long dst_addr,
 				unsigned long src_addr,
+				enum mcopy_atomic_mode mode,
 				struct page **pagep);
 int hugetlb_reserve_pages(struct inode *inode, long from, long to,
 						struct vm_area_struct *vma,
@@ -186,7 +188,7 @@ static inline void hugetlb_show_meminfo(void)
 #define is_hugepage_only_range(mm, addr, len)	0
 #define hugetlb_free_pgd_range(tlb, addr, end, floor, ceiling) ({BUG(); 0; })
 #define hugetlb_mcopy_atomic_pte(dst_mm, dst_pte, dst_vma, dst_addr, \
-				src_addr, pagep)	({ BUG(); 0; })
+				src_addr, mode, pagep)	({ BUG(); 0; })
 #define huge_pte_offset(mm, address, sz)	0
 
 static inline bool isolate_huge_page(struct page *page, struct list_head *list)
