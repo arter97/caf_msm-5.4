3a14643fe6c5debbbba6859d6ce87bf7856f2686
Yadu MG <ymg@codeaurora.org>
2021-04-01 21:53:02 +0530

    [arm64] Refactor gold core boot changes on 5.4
    Refactor and fix the logical boot cpu for msm-5.4.
    
    Change-Id: I4a8a2711c8ef3ac2e8a37a5d1d2d52613cfbd4af
    Signed-off-by: Yadu MG <ymg@codeaurora.org>

--- arch/arm64/kernel/setup.c
+++ arch/arm64/kernel/setup.c
@@ -86,8 +86,6 @@ u64 __cacheline_aligned boot_args[4];
 unsigned int logical_bootcpu_id __read_mostly;
 EXPORT_SYMBOL(logical_bootcpu_id);
 
-extern void *__init fixmap_remap_fdt(phys_addr_t dt_phys, int *size,
-				       pgprot_t prot);
 /*
  * Parse the device tree cpu nodes and enumerate logical cpu number for
  * the boot cpu based on the mpidr value and reg value from the cpu node.
