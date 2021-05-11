3a8b345c9cfc4753c901b82b2ed5a069c130877b
Hongwei Ren <hongren@codeaurora.org>
2021-05-11 09:15:42 +0530

    include: uapi: add scsi uapi header files
    add scsi uapi header files.
    
    Change-Id: I283c402d84e6c4955089d700d05feafbdced4ef7
    Signed-off-by: Hongwei Ren <hongren@codeaurora.org>

--- scripts/headers_install.sh
+++ scripts/headers_install.sh
@@ -97,6 +97,7 @@ include/uapi/linux/eventpoll.h:CONFIG_PM_SLEEP
 include/uapi/linux/hw_breakpoint.h:CONFIG_HAVE_MIXED_BREAKPOINTS_REGS
 include/uapi/linux/pktcdvd.h:CONFIG_CDROM_PKTCDVD_WCACHE
 include/uapi/linux/raw.h:CONFIG_MAX_RAW_DEVS
+include/uapi/scsi/ufs/ufs.h:CONFIG_SCSI_UFSHCD_QTI
 "
 
 for c in $configs
