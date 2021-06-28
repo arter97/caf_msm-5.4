f2d61e7761f7605464853f8f275db1f71a00d019
Marcelo Ricardo Leitner <marcelo.leitner@gmail.com>
2021-06-28 16:13:44 -0300

    sctp: add param size validation for SCTP_PARAM_SET_PRIMARY
    commit ef6c8d6ccf0c1dccdda092ebe8782777cd7803c9 upstream.
    
    When SCTP handles an INIT chunk, it calls for example:
    sctp_sf_do_5_1B_init
      sctp_verify_init
        sctp_verify_param
      sctp_process_init
        sctp_process_param
          handling of SCTP_PARAM_SET_PRIMARY
    
    sctp_verify_init() wasn't doing proper size validation and neither the
    later handling, allowing it to work over the chunk itself, possibly being
    uninitialized memory.
    
    Signed-off-by: Marcelo Ricardo Leitner <marcelo.leitner@gmail.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    (cherry picked from commit ccb79116c37242c07c34c991868acded87509e4c)
    
    Change-Id: I9bd04958d1614025f1169d64a3c7003c181b91d7
    Signed-off-by: dinesubr <quic_dinesubr@quicinc.com>

--- net/sctp/sm_make_chunk.c
+++ net/sctp/sm_make_chunk.c
@@ -2141,6 +2141,16 @@ static sctp_ierror_t sctp_verify_param(struct net *net,
 		if (net->sctp.addip_enable)
 			break;
 		goto fallthrough;
+		if (!ep->asconf_enable)
+			goto unhandled;
+
+		if (ntohs(param.p->length) < sizeof(struct sctp_addip_param) +
+					     sizeof(struct sctp_paramhdr)) {
+			sctp_process_inv_paramlength(asoc, param.p,
+						     chunk, err_chunk);
+			retval = SCTP_IERROR_ABORT;
+		}
+		break;
 
 	case SCTP_PARAM_HOST_NAME_ADDRESS:
 		/* Tell the peer, we won't support this param.  */
