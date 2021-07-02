f67d8f9edb6b9b75ce4bb466b5274dac989f0960
Hui Li <hul@codeaurora.org>
2021-07-02 11:07:56 +0800

    msm: kgsl: increasing wait-time for active count
    Sometimes kgsl failed to suspend when system try to go
    into LPM, there is timeout to wait for active count to
    become 0, add a retry to wait more time for active count.
    
    Change-Id: I8d9d746448f101d3c6a8e0c648f160f2062b3c86
    Signed-off-by: Hui Li <hul@codeaurora.org>

--- drivers/gpu/msm/adreno_a6xx.h
+++ drivers/gpu/msm/adreno_a6xx.h
@@ -215,6 +215,13 @@ struct cpu_gpu_lock {
 	 (1 << A6XX_INT_TSB_WRITE_ERROR))
 
 /**
+ * retry times of wait active count become 0, currently
+ * we wait 10 seconds here, *gmu* should already has
+ * troubles after 10 seconds waiting
+ */
+#define NUM_TIMES_WAIT_ACTIVE_COUNT_RETRY 10
+
+/**
  * to_a6xx_core - return the a6xx specific GPU core struct
  * @adreno_dev: An Adreno GPU device handle
  *
