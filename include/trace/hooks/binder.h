32a526f01d0e855ac18e1e6df3c30542eda7efe7
xieliujie <xieliujie@oppo.com>
2021-07-15 17:28:28 +0800

    ANDROID: vendor_hooks: Modify the function name
    Vendor hook is add in the commit:
    https://android-review.googlesource.com/c/kernel/common/+/1687592
    When we register the vendor hook and build, there is build error:
    too long symbol "__tracepoint_android_vh_binder_transaction_priority_skip"
    So, I shorten the function name
    android_vh_binder_transaction_priority_skip -->
    android_vh_binder_priority_skip
    
    Bug: 186482511
    
    Signed-off-by: xieliujie <xieliujie@oppo.com>
    Change-Id: I27f39dd32436b09257bcf6e746f4ccdfbd6d6cfe
    (cherry picked from commit 4322bb07e85d6dbaede321f236b43650769d993e)

--- include/trace/hooks/binder.h
+++ include/trace/hooks/binder.h
@@ -17,7 +17,7 @@ struct task_struct;
 DECLARE_HOOK(android_vh_binder_transaction_init,
 	TP_PROTO(struct binder_transaction *t),
 	TP_ARGS(t));
-DECLARE_HOOK(android_vh_binder_transaction_priority_skip,
+DECLARE_HOOK(android_vh_binder_priority_skip,
 	TP_PROTO(struct task_struct *task, bool *skip),
 	TP_ARGS(task, skip));
 DECLARE_HOOK(android_vh_binder_set_priority,
