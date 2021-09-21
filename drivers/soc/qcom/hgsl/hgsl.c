2b08b785fb86d53e8ad5c51f9f1d639c0ff2b635
Jie Feng <jiefeng@codeaurora.org>
2021-09-21 12:49:01 -0400

    soc: qcom: hgsl: fix KW warnings
    Handle allocation failures.
    
    Change-Id: I403aaa7a6701722a84920a31e21d7b4848a07508
    Signed-off-by: Jie Feng <jiefeng@codeaurora.org>

--- drivers/soc/qcom/hgsl/hgsl.c
+++ drivers/soc/qcom/hgsl/hgsl.c
@@ -2226,6 +2226,10 @@ static int hgsl_ioctl_issueib_with_alloc_list(struct file *filep,
 		be_data_size = (params.num_ibs + params.num_allocations) *
 			(sizeof(struct gsl_memdesc_t) + sizeof(uint64_t));
 		be_descs = (struct gsl_memdesc_t *)hgsl_malloc(be_data_size);
+		if (be_descs == NULL) {
+			ret = -ENOMEM;
+			goto out;
+		}
 		be_offsets = (uint64_t *)&be_descs[params.num_ibs +
 							params.num_allocations];
 		if (copy_from_user(be_descs, USRPTR(params.be_data), be_data_size)) {
@@ -2466,7 +2470,7 @@ static int hgsl_ioctl_syncobj_wait_multiple(struct file *filep,
 			ret = -EFAULT;
 			goto out;
 		}
-		if (copy_to_user(USRPTR(param.status), &status, status_size)) {
+		if (copy_to_user(USRPTR(param.status), status, status_size)) {
 			ret = -EFAULT;
 			goto out;
 		}
@@ -2696,6 +2700,11 @@ static int hgsl_open(struct inode *inodep, struct file *filep)
 
 	if (!priv)
 		return -ENOMEM;
+
+	if (!task) {
+		ret = -EINVAL;
+		goto out;
+	}
 
 	idr_init(&priv->isync_timeline_idr);
 	spin_lock_init(&priv->isync_timeline_lock);
