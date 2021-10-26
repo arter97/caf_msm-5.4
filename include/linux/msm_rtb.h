53516ad4400f8eea8136710bf15e0199a8a5c22f
Shreyas K K <shrekk@codeaurora.org>
2021-10-26 13:11:01 +0530

    msm_rtb: Add __force attribute for type conversions
    Silence the sparse warnings by adding __force attribute
    for type conversions.
    
    Change-Id: I64e8197d2f789579caf8a702f396768815c91e75
    Signed-off-by: Shreyas K K <shrekk@codeaurora.org>

--- include/linux/msm_rtb.h
+++ include/linux/msm_rtb.h
@@ -69,11 +69,11 @@ int uncached_logk(enum logk_event_type log_type, void *data);
 /* Override the #defines in asm/io.h with the logged ones */
 #define __raw_read_logged(a, _l, _t)    ({ \
 	_t __a; \
-	void *_addr = (void *)(a); \
+	void *_addr = (__force void *)(a); \
 	int _ret; \
 	_ret = uncached_logk(LOGK_READL, _addr); \
 	ETB_WAYPOINT; \
-	__a = __raw_read##_l(_addr); \
+	__a = __raw_read##_l((__force void __iomem *)_addr); \
 	if (_ret) \
 		LOG_BARRIER; \
 	__a; \
@@ -147,10 +147,10 @@ int uncached_logk(enum logk_event_type log_type, void *data);
 
 #define __raw_write_logged(v, a, _t) ({ \
 	int _ret; \
-	void *_addr = (void *)(a); \
+	void *_addr = (__force void *)(a); \
 	_ret = uncached_logk(LOGK_WRITEL, _addr); \
 	ETB_WAYPOINT; \
-	__raw_write##_t((v), _addr); \
+	__raw_write##_t((v), (__force void __iomem *)_addr); \
 	if (_ret) \
 		LOG_BARRIER; \
 	})
