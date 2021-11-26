8a7a5b244da5fa72c6897e22438462bce0dddee0
Swathi K <kataka@codeaurora.org>
2021-11-26 15:58:57 +0530

    msm: adsprpc: Handle UAF in process shell memory
    Added flag to indicate memory used
    in process initialization. And, this memory
    would not removed in internal unmap to avoid
    UAF or double free.
    
    Change-Id: Ifa621dee171b3d1f98b82302c847f4d767f3e736
    Signed-off-by: Swathi K <kataka@codeaurora.org>

--- drivers/char/adsprpc.c
+++ drivers/char/adsprpc.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2012-2018, The Linux Foundation. All rights reserved.
+ * Copyright (c) 2012-2021, The Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -251,6 +251,7 @@ struct fastrpc_mmap {
 	int refs;
 	uintptr_t raddr;
 	int uncached;
+	bool is_filemap; /* flag to indicate map used in process init*/
 };
 
 struct fastrpc_file {
@@ -457,9 +458,10 @@ static int fastrpc_mmap_remove(struct fastrpc_file *fl, uintptr_t va,
 
 	spin_lock(&me->hlock);
 	hlist_for_each_entry_safe(map, n, &me->maps, hn) {
-		if (map->raddr == va &&
+		if (map->refs == 1 && map->raddr == va &&
 			map->raddr + map->len == va + len &&
-			map->refs == 1) {
+			/* Remove map if not used in process initialization */
+			!map->refs == 1) {
 			match = map;
 			hlist_del_init(&map->hn);
 			break;
@@ -472,9 +474,10 @@ static int fastrpc_mmap_remove(struct fastrpc_file *fl, uintptr_t va,
 	}
 	spin_lock(&fl->hlock);
 	hlist_for_each_entry_safe(map, n, &fl->maps, hn) {
-		if (map->raddr == va &&
+		if (map->refs == 1 && map->raddr == va &&
 			map->raddr + map->len == va + len &&
-			map->refs == 1) {
+			/* Remove map if not used in process initialization */
+			!map->refs == 1) {
 			match = map;
 			hlist_del_init(&map->hn);
 			break;
@@ -580,6 +583,7 @@ static int fastrpc_mmap_create(struct fastrpc_file *fl, int fd, uintptr_t va,
 	INIT_HLIST_NODE(&map->hn);
 	map->fl = fl;
 	map->fd = fd;
+	map->is_filemap = false;
 	if (mflags == ADSP_MMAP_HEAP_ADDR) {
 		DEFINE_DMA_ATTRS(rh_attrs);
 
@@ -1583,6 +1587,8 @@ static int fastrpc_init_process(struct fastrpc_file *fl,
 			goto bail;
 		VERIFY(err, !fastrpc_mmap_create(fl, init->filefd, init->file,
 					init->filelen, mflags, &file));
+		if (file)
+			file->is_filemap = true;
 		if (err)
 			goto bail;
 		inbuf.pageslen = 1;
