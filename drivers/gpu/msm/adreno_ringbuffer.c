0eadbcee3ff824d2b30d8f9564094380eddb4626
Abhishek Barman <quic_abarman@quicinc.com>
2021-12-07 12:50:34 +0530

    msm: kgsl: Fix gpuaddr_in_range() to check upper bound.
    Currently gpuaddr_in_range() accepts only the gpuaddr & returns
    true if it lies in valid range. But this does not mean that the
    entire buffer is within range.
    Modify the function to accept size as a parameter and check that
    both starting & ending points of buffer lie within mmu range.
    
    Change-Id: I1d722295b9a27e746bfdb6d3bf409ffe722193cb
    Signed-off-by: Rohan Sethi <quic_rohsethi@quicinc.com>
    Signed-off-by: Abhishek Barman <quic_abarman@quicinc.com>
    Signed-off-by: ravnar <quic_ravnar@quicinc.com>

--- drivers/gpu/msm/adreno_ringbuffer.c
+++ drivers/gpu/msm/adreno_ringbuffer.c
@@ -1,4 +1,5 @@
-/* Copyright (c) 2002,2007-2017,2019-2020 The Linux Foundation. All rights reserved.
+/* Copyright (c) 2002,2007-2016,2019-2020, The Linux Foundation. All rights reserved.
+ * Copyright (c) 2022 Qualcomm Innovation Center, Inc. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -767,7 +768,7 @@ static inline bool _ringbuffer_verify_ib(struct kgsl_device_private *dev_priv,
 	}
 
 	/* Make sure that the address is mapped */
-	if (!kgsl_mmu_gpuaddr_in_range(private->pagetable, ib->gpuaddr)) {
+	if (!kgsl_mmu_gpuaddr_in_range(private->pagetable, ib->gpuaddr, ib->size)) {
 		pr_context(device, context, "ctxt %d invalid ib gpuaddr %llX\n",
 			context->id, ib->gpuaddr);
 		return false;
