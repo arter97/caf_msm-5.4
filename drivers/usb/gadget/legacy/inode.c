e7df842e6e93df37240cacdd41648574b0b9506d
Greg Kroah-Hartman <gregkh@linuxfoundation.org>
2021-12-09 18:59:27 +0100

    USB: gadget: detect too-big endpoint 0 requests.
    commit 153a2d7e3350cc89d406ba2d35be8793a64c2038 upstream.
    
    Sometimes USB hosts can ask for buffers that are too large from endpoint
    0, which should not be allowed.  If this happens for OUT requests, stall
    the endpoint, but for IN requests, trim the request size to the endpoint
    buffer size.
    
    Co-developed-by: Szymon Heidrich <szymon.heidrich@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    (cherry picked from commit 7193ad3e50e596ac2192531c58ba83b9e6d2444b)
    
    Change-Id: I4f4c2eedd2acbe96f31520a824512a6bdcf859eb
    Signed-off-by: Li Long <quic_lonli@quicinc.com>

--- drivers/usb/gadget/legacy/inode.c
+++ drivers/usb/gadget/legacy/inode.c
@@ -1333,6 +1333,18 @@ gadgetfs_setup (struct usb_gadget *gadget, const struct usb_ctrlrequest *ctrl)
 	u16				w_value = le16_to_cpu(ctrl->wValue);
 	u16				w_length = le16_to_cpu(ctrl->wLength);
 
+	if (w_length > RBUF_SIZE) {
+		if (ctrl->bRequestType == USB_DIR_OUT) {
+			return value;
+		} else {
+			/* Cast away the const, we are going to overwrite on purpose. */
+			__le16 *temp = (__le16 *)&ctrl->wLength;
+
+			*temp = cpu_to_le16(RBUF_SIZE);
+			w_length = RBUF_SIZE;
+		}
+	}
+
 	spin_lock (&dev->lock);
 	dev->setup_abort = 0;
 	if (dev->state == STATE_DEV_UNCONNECTED) {
