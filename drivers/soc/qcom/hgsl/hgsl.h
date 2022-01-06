341d8b33084dfc207770cad329b789c3def7df85
Hai Li <quic_hali@quicinc.com>
2022-01-05 23:56:39 -0500

    soc: qcom: hgsl: add gpu_mem trace support
    Support gpu_mem/gpu_mem_total trace
    event
    
    Change-Id: Ie935f4232f2d00348a8d8d0719c5fd0d3eadb3f7
    Signed-off-by: Hai Li <quic_hali@quicinc.com>
    Signed-off-by: Kasin Li <quic_donglil@quicinc.com>

--- drivers/soc/qcom/hgsl/hgsl.h
+++ drivers/soc/qcom/hgsl/hgsl.h
@@ -1,6 +1,7 @@
 /* SPDX-License-Identifier: GPL-2.0-only */
 /*
  * Copyright (c) 2020-2021, The Linux Foundation. All rights reserved.
+ * Copyright (c) 2022 Qualcomm Innovation Center, Inc. All rights reserved.
  */
 
 #ifndef __HGSL_H_
@@ -65,6 +66,8 @@ struct hgsl_priv {
 	struct mutex lock;
 	struct list_head mem_mapped;
 	struct list_head mem_allocated;
+
+	atomic64_t total_mem_size;
 };
 
 
