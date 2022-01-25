6fced5b21ffe78781672ff5eac15702d1fa00822
Zelong Ren <quic_zeloren@quicinc.com>
2022-01-25 14:33:20 +0800

    linux-msm: Add i2c support for 800KHZ
    Add i2c support for 800KHZ for both 8195 and 8295 board.
    
    Change-Id: I225c9cfcbfb123e9997386edd93528f7aedbf98b
    Signed-off-by: Zelong Ren <quic_zeloren@quicinc.com>

--- drivers/i2c/busses/i2c-msm-geni.c
+++ drivers/i2c/busses/i2c-msm-geni.c
@@ -184,6 +184,7 @@ static struct geni_i2c_clk_fld geni_i2c_clk_map[] = {
 	{KHz(100), 7, 10, 11, 26},
 	{KHz(400), 2,  7, 10, 24},
 	{KHz(1000), 1, 3,  9, 18},
+	{KHz(800), 1, 3,  7, 16},
 };
 
 static int geni_i2c_clk_map_idx(struct geni_i2c_dev *gi2c)
