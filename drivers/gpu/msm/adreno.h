47ed88bd9c80cebb86b672a51ae2e4d95f47c349
Pankaj Gupta <quic_gpankaj@quicinc.com>
2022-03-02 19:49:58 +0530

    msm: kgsl: Add a sysfs node to control performance counter reads
    Currently performance counters are global and can be read by anyone. Change
    the behaviour to disable reading global counters as default and add a sysfs
    node to enable/disable reads.
    
    Change-Id: Ic3785acd9bd7425c2a844ed103d7b870d9f80adf
    Signed-off-by: Mohammed Mirza Mandayappurath Manzoor <quic_mmandaya@quicinc.com>
    Signed-off-by: Harshitha Sai Neelati <quic_hsaineel@quicinc.com>
    Signed-off-by: Pankaj Gupta <quic_gpankaj@quicinc.com>

--- drivers/gpu/msm/adreno.h
+++ drivers/gpu/msm/adreno.h
@@ -434,6 +434,12 @@ struct adreno_device {
 
 	struct list_head active_list;
 	spinlock_t active_list_lock;
+
+	/*
+	 * @perfcounter: Flag to clear perfcounters across contexts and
+	 * controls perfcounter ioctl read
+	 */
+	bool perfcounter;
 };
 
 /**
