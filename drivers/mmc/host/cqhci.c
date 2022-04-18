8e597a2034f976c583d5edf59547ca05b10118ab
Maramaina Naresh <quic_mnaresh@quicinc.com>
2022-04-18 09:58:50 +0530

    Revert "mmc: core: Quickboot/Deepsleep support for monaco"
    This reverts commit d7935c6ab19fc0ae21dc55a37dcf569b33a714ee.
    
    Reverting temporary hot fix for MMC deepsleep / quickboot.
    
    Change-Id: I04059fd79b8e8f460328dea4ef4f89874663e703
    Signed-off-by: Maramaina Naresh <quic_mnaresh@quicinc.com>

--- drivers/mmc/host/cqhci.c
+++ drivers/mmc/host/cqhci.c
@@ -307,11 +307,10 @@ static void __cqhci_enable(struct cqhci_host *cq_host)
 	cq_host->activated = true;
 
 #if defined(CONFIG_SDC_QTI)
-	if (mmc->hiber_notifier || mmc->deepsleep) {
+	if (mmc->hiber_notifier) {
 		if (cqhci_host_is_crypto_supported(cq_host))
 			cq_host->crypto_vops->restore_from_hibernation(cq_host);
 		mmc->hiber_notifier = false;
-		mmc->deepsleep = false;
 	}
 #endif
 	mmc_log_string(mmc, "CQ enabled\n");
