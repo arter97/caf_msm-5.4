47b225fecda5433f8b5011cb57b8d542a53e9ee2
Khushal Bawankar <quic_kbawanka@quicinc.com>
2022-06-21 10:30:05 +0530

    clk: qcom: Add support for SDXPOORWILLS RPMh clocks
    Add support for following clocks maintained by RPMh of SDXPOORWILLS.
    * BI TCXO
    * BI_TCXO_AO
    * RF_CLK1
    * RF_CLK1_AO.
    
    Change-Id: I0cca981290c9524e6ca7a64d07e973ab431e6775
    Signed-off-by: Khushal Bawankar <quic_kbawanka@quicinc.com>

--- drivers/clk/qcom/clk-rpmh.c
+++ drivers/clk/qcom/clk-rpmh.c
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: GPL-2.0-only
 /*
  * Copyright (c) 2018-2021, The Linux Foundation. All rights reserved.
+ * Copyright (c) 2022, Qualcomm Innovation Center, Inc. All rights reserved.
  */
 
 #include <linux/clk-provider.h>
@@ -630,6 +631,18 @@ static const struct clk_rpmh_desc clk_rpmh_lemans = {
 	.num_clks = ARRAY_SIZE(lemans_rpmh_clocks),
 };
 
+static struct clk_hw *sdxpoorwills_rpmh_clocks[] = {
+	[RPMH_CXO_CLK]		= &sdm845_bi_tcxo.hw,
+	[RPMH_CXO_CLK_A]	= &sdm845_bi_tcxo_ao.hw,
+	[RPMH_RF_CLK1]		= &sdm845_rf_clk1.hw,
+	[RPMH_RF_CLK1_A]	= &sdm845_rf_clk1_ao.hw,
+};
+
+static const struct clk_rpmh_desc clk_rpmh_sdxpoorwills = {
+	.clks = sdxpoorwills_rpmh_clocks,
+	.num_clks = ARRAY_SIZE(sdxpoorwills_rpmh_clocks),
+};
+
 static struct clk_hw *of_clk_rpmh_hw_get(struct of_phandle_args *clkspec,
 					 void *data)
 {
@@ -732,6 +745,7 @@ static const struct of_device_id clk_rpmh_match_table[] = {
 	{ .compatible = "qcom,direwolf-rpmh-clk", .data = &clk_rpmh_direwolf},
 	{ .compatible = "qcom,sdx55-rpmh-clk",  .data = &clk_rpmh_sdx55},
 	{ .compatible = "qcom,lemans-rpmh-clk", .data = &clk_rpmh_lemans},
+	{ .compatible = "qcom,sdxpoorwills-rpmh-clk", .data = &clk_rpmh_sdxpoorwills},
 	{ }
 };
 MODULE_DEVICE_TABLE(of, clk_rpmh_match_table);
