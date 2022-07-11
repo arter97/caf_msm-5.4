e04d35ab5d3bc094d0ec324267e3652bac626b83
Zelong Ren <quic_zeloren@quicinc.com>
2022-07-11 15:45:52 +0800

    soc: qcom: hab: add a new physical channel for VPP
    This new HAB physical channel will be used by
    VPP (Video Post Processing) virtualization.
    
    Change-Id: Ia5dc70accb4be073d07842d929fc65cd5f2895a1
    Signed-off-by: Zelong Ren <quic_zeloren@quicinc.com>

--- drivers/soc/qcom/hab/hab.h
+++ drivers/soc/qcom/hab/hab.h
@@ -47,6 +47,7 @@ enum hab_payload_type {
 #define DEVICE_GFX_NAME "hab_ogles"
 #define DEVICE_VID_NAME "hab_vid"
 #define DEVICE_VID2_NAME "hab_vid2"
+#define DEVICE_VID3_NAME "hab_vid3"
 #define DEVICE_MISC_NAME "hab_misc"
 #define DEVICE_QCPE1_NAME "hab_qcpe_vm1"
 #define DEVICE_CLK1_NAME "hab_clock_vm1"
