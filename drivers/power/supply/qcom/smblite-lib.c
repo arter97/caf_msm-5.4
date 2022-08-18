f007062c6b07d9ada7278c3608bc825fdf624056
Rakesh Kota <quic_kotarake@quicinc.com>
2022-08-18 15:50:05 +0530

    power: smblite: Destroy all votables in cleanup path
    There is a possibility of memory leak if any of the votable
    is not cleaned-up in error path. Fix this by destroying all
    the votables in batt_deinit which are created by batt_init.
    
    Change-Id: Ic49c8463ab5bad8067fe0d6c59dd7abaa753e467
    Signed-off-by: Rakesh Kota <quic_kotarake@quicinc.com>

--- drivers/power/supply/qcom/smblite-lib.c
+++ drivers/power/supply/qcom/smblite-lib.c
@@ -4449,8 +4449,10 @@ static int smblite_lib_create_votables(struct smb_charger *chg)
 
 static void smblite_lib_destroy_votables(struct smb_charger *chg)
 {
-	if (chg->usb_icl_votable)
-		destroy_votable(chg->usb_icl_votable);
+	if (chg->temp_change_irq_disable_votable)
+		destroy_votable(chg->temp_change_irq_disable_votable);
+	if (chg->icl_irq_disable_votable)
+		destroy_votable(chg->icl_irq_disable_votable);
 	if (chg->awake_votable)
 		destroy_votable(chg->awake_votable);
 	if (chg->chg_disable_votable)
