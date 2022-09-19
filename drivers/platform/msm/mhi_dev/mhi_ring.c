42e1f43f4e1de19a61cb644f175224ecd98af233
Nitesh Gupta <quic_nitegupt@quicinc.com>
2022-09-19 13:41:50 +0530

    msm: mhi_dev: Fast-Forward MHI EP driver to 5.4 Mainline
    This change syncs the MHI EP driver with 5.4 Mainline,
    commit 03fd2dac40:Merge "arm: defconfig: Enable GCC,
    RPMHCC and DEBUGCC for SDXPOORWILLS".
    
    Change-Id: Ia48dc2b8c867cb0502d86cd62089a61277069b90
    Signed-off-by: Mrinmay Sarkar <quic_msarkar@quicinc.com>
    Signed-off-by: Nitesh Gupta <quic_nitegupt@quicinc.com>

--- drivers/platform/msm/mhi_dev/mhi_ring.c
+++ drivers/platform/msm/mhi_dev/mhi_ring.c
@@ -1,5 +1,6 @@
 // SPDX-License-Identifier: GPL-2.0-only
 /* Copyright (c) 2015-2021, The Linux Foundation. All rights reserved.*/
+/* Copyright (c) 2022 Qualcomm Innovation Center, Inc. All rights reserved.*/
 
 #include <linux/kernel.h>
 #include <linux/of.h>
@@ -197,7 +198,7 @@ int mhi_dev_process_ring_element(struct mhi_dev_ring *ring, size_t offset)
 						el->tre.len, offset);
 
 	if (ring->ring_cb)
-		ring->ring_cb(ring->mhi_dev, el, (void *)ring);
+		return ring->ring_cb(ring->mhi_dev, el, (void *)ring);
 	else
 		mhi_log(MHI_MSG_ERROR, "No callback registered for ring %d\n",
 				ring->id);
@@ -557,7 +558,7 @@ void mhi_ring_init(struct mhi_dev_ring *ring, enum mhi_dev_ring_type type,
 EXPORT_SYMBOL(mhi_ring_init);
 
 void mhi_ring_set_cb(struct mhi_dev_ring *ring,
-			void (*ring_cb)(struct mhi_dev *dev,
+			int (*ring_cb)(struct mhi_dev *dev,
 			union mhi_dev_ring_element_type *el, void *ctx))
 {
 	if (WARN_ON(!ring || !ring_cb))
