48e7c37a798bc7c056661599266b0204af849c8a
lixiang <quic_lixian@quicinc.com>
2023-01-06 18:00:27 +0800

    soc: qcom: hab: add sanity check when hab probe
    Currently, we have
    * 29 HAB devices defined in the array hab_devices
    * 28 pchan_factory_settings entries
    * 26 vdev-shmem devices defined in most of the platforms for HAB.
    
    A crash issue would happen if we add enough vdev-shmem devices in
    linux-lv/la.config, makes the total number of shmem >= 29, even
    they are not for HAB.
    
    hab:get_guest_ctrl_paddr:61 name = vm3-hab_aud1,
    factory paddr = 0x4d534732474d5651, irq 0, pages 257
    Unable to handle kernel paging request at virtual address 00534732474d5651
    Internal error: Oops: 96000004 [#1] PREEMPT SMP
    pc : get_guest_ctrl_paddr+0x70/0x1b8
    lr : get_guest_ctrl_paddr+0x6c/0x1b8
    Call trace:
    get_guest_ctrl_paddr+0x70/0x1b8
    hab_shmem_attach+0x58
    
    Root cause is OOB write happens when we probe and update the 29th shmem
    factory and irq info in the array pchan_factory_settings while the size
    of it is only 28. HAB driver is probing shmem devices more than expected.
    
    Since the compatible name of vdev-shmem cannot tell apart from different
    shmem users due to the limitation of vdev-shmem library, we decide to
    decrease the number of maximum successful probe attempts to 26 by adding
    the number of pchan_factory_setting entries into the sanity check during
    probe and meanwhile decreasing the number to 26 from 28. Excessive probe
    attempts would return -ENODEV if the number of successfully probed
    devices reaches to 26. The actual number of created physical channels is
    based on device tree settings.
    
    Considering no need for CLK1/2 and XVM1/2/3 physical channels, 26 is
    enough for all hab use cases. Meanwhile, an assumption is required if
    there is another vdev-shmem user other than HAB:
    there must be 26 vdev-shmem devices for hab in the linux-lv/la.config if
    we hope to successfully probe and only probe the exact number of
    vdev-shmem devices defined for HAB.
    
    Change-Id: I6fd7001faa52c7924cf5721c565f934282d39470
    Signed-off-by: lixiang <quic_lixian@quicinc.com>

--- drivers/soc/qcom/hab/hab_qvm.c
+++ drivers/soc/qcom/hab/hab_qvm.c
@@ -1,14 +1,19 @@
 // SPDX-License-Identifier: GPL-2.0-only
 /*
  * Copyright (c) 2016-2021, The Linux Foundation. All rights reserved.
- * Copyright (c) 2022 Qualcomm Innovation Center, Inc. All rights reserved.
+ * Copyright (c) 2022-2023, Qualcomm Innovation Center, Inc. All rights reserved.
  */
 #include "hab.h"
 #include "hab_qvm.h"
 
 /*
- * this is for platform does not provide probe features. the size should match
- * hab device side (all mmids)
+ * 1. The entry values are only for platform without probe features. For those
+ * platforms with probe feature, correct address and irq will be updated in it.
+ * 2. Length of the array decides the maximum number of successful probe attempts.
+ * Excessive probe attempts will fail and return -ENODEV.
+ * 3. The number of vdev-shmem devices for hab in linux-lv/la.config should be
+ * the same as the entry number of the below array to successfully probe and only
+ * probe those vdev-shmem devices for HAB if other vdev-shmem users exist.
  */
 static struct shmem_irq_config pchan_factory_settings[] = {
 	{0x1b000000, 7},
@@ -37,8 +42,6 @@ static struct shmem_irq_config pchan_factory_settings[] = {
 	{0x1b017000, 30},
 	{0x1b018000, 31},
 	{0x1b019000, 32},
-	{0x1b01a000, 33},
-	{0x1b01b000, 34},
 };
 
 struct qvm_plugin_info qvm_priv_info = {
