arter97/caf_msm-5.4

commit 87af59be02920eab92d77a9a883619cde39d0754

usb: f_gsi: Allocating GSI EPs dynamically
This change allocates EPs & interrupters dynamically
for SA515 & SA415 data use cases, especially in case
when the machine supports more than 3 interrupters.

SA415 will be using 2 S/w EPs, if CV2X is present
in the composition make sure 2 H/w EPs should be
assigned and S/w EPs should be assigned to other
functions.

Change-Id: I6324507bb391cfb919975a070bddd18fe0d2dc5e
Signed-off-by: Hiren Gohel <quic_vhiren@quicinc.com>

drivers/usb/gadget/function/f_gsi.c
@@ -32,6 +32,7 @@ static void gsi_rndis_ipa_reset_trigger(struct gsi_data_port *d_port);
 static int gsi_ctrl_send_notification(struct f_gsi *gsi);
 static struct gsi_ctrl_pkt *gsi_ctrl_pkt_alloc(unsigned int len, gfp_t flags);
 static void gsi_ctrl_pkt_free(struct gsi_ctrl_pkt *pkt);
+static int gsi_dynamic_ep_allocation(struct usb_function *f);
 
 static inline bool usb_gsi_remote_wakeup_allowed(struct usb_function *f)
 {
@@ -2534,6 +2535,76 @@ static int gsi_get_alt(struct usb_function *f, unsigned int intf)
 	return -EINVAL;
 }
 
+/*
+ * Wrapper to allocate GSI EP interrupters dynamically
+ * when more than 3 GSI EPs are used.
+ */
+static int gsi_dynamic_ep_allocation(struct usb_function *f)
+{
+	struct f_gsi	 *gsi = func_to_gsi(f);
+	struct f_gsi	 *gsi_rmnet_v2x = inst_status[IPA_USB_RMNET_CV2X].opts->gsi;
+	struct usb_composite_dev *cdev = f->config->cdev;
+
+	if (gsi_normal_ep_support(cdev->gadget)) {
+		switch (gsi->prot_id) {
+		case IPA_USB_RMNET:
+		case IPA_USB_ECM:
+			if (gsi_rmnet_v2x->function.fs_descriptors) {
+				if (gsi->d_port.in_ep)
+					usb_gsi_ep_op(gsi->d_port.in_ep,
+						&gsi->d_port.in_request,
+						GSI_DYNAMIC_EP_INTR_CALC);
+				if (gsi->d_port.out_ep)
+					usb_gsi_ep_op(gsi->d_port.out_ep,
+						&gsi->d_port.out_request,
+						GSI_SW_EP_PATH);
+			} else {
+				if (gsi->d_port.in_ep)
+					usb_gsi_ep_op(gsi->d_port.in_ep,
+						&gsi->d_port.in_request,
+						GSI_DYNAMIC_EP_INTR_CALC);
+				if (gsi->d_port.out_ep)
+					usb_gsi_ep_op(gsi->d_port.out_ep,
+						&gsi->d_port.out_request,
+						GSI_DYNAMIC_EP_INTR_CALC);
+			}
+		break;
+		case IPA_USB_DIAG:
+			if (!gsi_rmnet_v2x->function.fs_descriptors)
+				usb_gsi_ep_op(gsi->d_port.in_ep,
+					&gsi->d_port.in_request,
+					GSI_DYNAMIC_EP_INTR_CALC);
+			else
+				usb_gsi_ep_op(gsi->d_port.in_ep,
+					&gsi->d_port.in_request,
+					GSI_SW_EP_PATH);
+		break;
+		case IPA_USB_RMNET_CV2X:
+			if (gsi->d_port.in_ep)
+				usb_gsi_ep_op(gsi->d_port.in_ep,
+					&gsi->d_port.in_request,
+					GSI_DYNAMIC_EP_INTR_CALC);
+			if (gsi->d_port.out_ep)
+				usb_gsi_ep_op(gsi->d_port.out_ep,
+					&gsi->d_port.out_request,
+					GSI_DYNAMIC_EP_INTR_CALC);
+		break;
+		default:
+			log_event_dbg("%s: Data interface not supported\n", __func__);
+			return -EINVAL;
+		}
+	} else {
+		if (gsi->d_port.in_ep)
+			usb_gsi_ep_op(gsi->d_port.in_ep, &gsi->d_port.in_request,
+					GSI_DYNAMIC_EP_INTR_CALC);
+		if (gsi->d_port.out_ep)
+			usb_gsi_ep_op(gsi->d_port.out_ep, &gsi->d_port.out_request,
+					GSI_DYNAMIC_EP_INTR_CALC);
+	}
+
+	return 0;
+}
+
 static int gsi_set_alt(struct usb_function *f, unsigned int intf,
 						unsigned int alt)
 {
@@ -2596,13 +2667,12 @@ static int gsi_set_alt(struct usb_function *f, unsigned int intf,
 			gsi->d_port.ntb_info.ntb_input_size =
 				MBIM_NTB_DEFAULT_IN_SIZE;
 		if (alt == 1) {
-			if (gsi->d_port.in_ep)
-				usb_gsi_ep_op(gsi->d_port.in_ep, &gsi->d_port.in_request,
-						GSI_DYNAMIC_EP_INTR_CALC);
 
-			if (gsi->d_port.out_ep)
-				usb_gsi_ep_op(gsi->d_port.out_ep, &gsi->d_port.out_request,
-						GSI_DYNAMIC_EP_INTR_CALC);
+			/*
+			 * Check if target supports SW EPs
+			 */
+			if (gsi_dynamic_ep_allocation(f))
+				pr_err("%s: Failed allocating GSI interrupters\n", __func__);
 
 			gsi->d_port.gadget = cdev->gadget;
 			gsi->d_port.cdev = cdev;

drivers/usb/dwc3/dwc3-msm.c
@@ -2074,10 +2074,35 @@ int usb_gsi_ep_op(struct usb_ep *ep, void *op_data, enum gsi_ep_op op)
 		 * Some targets have more number of H/w accelerated EP's
 		 * in order to support them, need to increase GSI
 		 * interrupts, this case will do the same.
+		 *
+		 * e.g If target supports 5 HW EPs
+		 *
+		 * ((dwc->num_gsi_eps - 1) - ((dwc->num_eps - 1) - dep->number))
+		 * 5 - (31 - 27) = 1
+		 *
 		 */
 		request = (struct usb_gsi_request *)op_data;
-		request->ep_intr_num =
-				(dwc->num_gsi_eps - ((dwc->num_eps - 1) - dep->number));
+
+		/*
+		 * Check if normal_eps_in_gsi_mode property is present
+		 * which indicates 1IN + 1OUT SW EPs will be used in GSI mode.
+		 * Though SW EPs will be used but the calculation
+		 * part will be same as used for HW EP interrupt no.
+		 * assignment.
+		 */
+		if (dwc->normal_eps_in_gsi_mode) {
+			request->ep_intr_num =
+				((dwc->num_gsi_eps + 2) -
+					((dwc->num_eps - 1) - dep->number));
+
+		} else {
+			request->ep_intr_num =
+				((dwc->num_gsi_eps) - ((dwc->num_eps - 1) - dep->number));
+		}
+		break;
+	case GSI_SW_EP_PATH:
+		request = (struct usb_gsi_request *)op_data;
+		request->ep_intr_num = 0;
 		break;
 	default:
 		dev_err(mdwc->dev, "%s: Invalid opcode GSI EP\n", __func__);
@@ -2086,6 +2111,23 @@ int usb_gsi_ep_op(struct usb_ep *ep, void *op_data, enum gsi_ep_op op)
 	return ret;
 }
 EXPORT_SYMBOL(usb_gsi_ep_op);
+
+/*
+ * Wrapper to check if device supports normal eps in GSI mode.
+ *
+ * @return 1 if device supports the fucntionality.
+ */
+
+bool gsi_normal_ep_support(struct usb_gadget *gadget)
+{
+	struct dwc3 *dwc = gadget_to_dwc(gadget);
+	bool ret = false;
+
+	if (dwc->normal_eps_in_gsi_mode)
+		ret = true;
+	return ret;
+}
+EXPORT_SYMBOL(gsi_normal_ep_support);
 
 /* Return true if host is supporting remote wakeup functionality. */
 bool usb_get_remote_wakeup_status(struct usb_gadget *gadget)

include/linux/usb/dwc3-msm.h
@@ -53,6 +53,7 @@ enum gsi_ep_op {
 	GSI_EP_OP_CHECK_FOR_SUSPEND,
 	GSI_EP_OP_DISABLE,
 	GSI_DYNAMIC_EP_INTR_CALC,
+	GSI_SW_EP_PATH,
 };
 
 /*
@@ -134,6 +135,7 @@ bool msm_dwc3_reset_ep_after_lpm(struct usb_gadget *gadget);
 int msm_dwc3_reset_dbm_ep(struct usb_ep *ep);
 int dwc3_msm_release_ss_lane(struct device *dev, bool usb_dp_concurrent_mode);
 bool usb_get_remote_wakeup_status(struct usb_gadget *gadget);
+bool gsi_normal_ep_support(struct usb_gadget *gadget);
 #else
 static inline struct usb_ep *usb_ep_autoconfig_by_name(
 		struct usb_gadget *gadget, struct usb_endpoint_descriptor *desc,

commit 97a21a4f6833902265d4684b6f6822d814f47747

usb: f_gsi: Check CV2X presence if normal ep used
'commit 87af59be0292 ("usb: f_gsi: Allocating GSI EPs
dynamically") introduced regression while allocating
EPs dynamically. It has a logic to check if CV2X
is part of the composition as part of check it
dereferenced a NULL pointer "opts" when CV2X is
not present.
This changes checks if "opts" is valid based on
it decides CV2X present in the composition.

Change-Id: Idac414e30d287cdb35f0d19155347856f061e349
Signed-off-by: Hiren Gohel <quic_vhiren@quicinc.com>

drivers/usb/gadget/function/f_gsi.c
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: GPL-2.0-only
 /*
  * Copyright (c) 2015-2021, The Linux Foundation. All rights reserved.
- * Copyright (c) 2022, Qualcomm Innovation Center, Inc. All rights reserved.
+ * Copyright (c) 2022-2023, Qualcomm Innovation Center, Inc. All rights reserved.
  */
 
 #include <linux/module.h>
@@ -2542,14 +2542,13 @@ static int gsi_get_alt(struct usb_function *f, unsigned int intf)
 static int gsi_dynamic_ep_allocation(struct usb_function *f)
 {
 	struct f_gsi	 *gsi = func_to_gsi(f);
-	struct f_gsi	 *gsi_rmnet_v2x = inst_status[IPA_USB_RMNET_CV2X].opts->gsi;
 	struct usb_composite_dev *cdev = f->config->cdev;
 
 	if (gsi_normal_ep_support(cdev->gadget)) {
 		switch (gsi->prot_id) {
 		case IPA_USB_RMNET:
 		case IPA_USB_ECM:
-			if (gsi_rmnet_v2x->function.fs_descriptors) {
+			if (inst_status[IPA_USB_RMNET_CV2X].opts) {
 				if (gsi->d_port.in_ep)
 					usb_gsi_ep_op(gsi->d_port.in_ep,
 						&gsi->d_port.in_request,
@@ -2570,7 +2569,7 @@ static int gsi_dynamic_ep_allocation(struct usb_function *f)
 			}
 		break;
 		case IPA_USB_DIAG:
-			if (!gsi_rmnet_v2x->function.fs_descriptors)
+			if (!inst_status[IPA_USB_RMNET_CV2X].opts)
 				usb_gsi_ep_op(gsi->d_port.in_ep,
 					&gsi->d_port.in_request,
 					GSI_DYNAMIC_EP_INTR_CALC);