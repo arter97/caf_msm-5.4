d707f6d1aa31d873b1d9529290eb23e0ecf5d284
Aniket Randive <quic_arandive@quicinc.com>
2023-06-06 13:01:33 +0530

    serial: msm_geni_serial: Pull RFR before stop_rx for uart
    Pull the RFR line before doing stop_rx in uart driver. This is to
    prevent any rx data to come into the uart rx fifo when stop_rx is
    being executed. During baud rate change, stop rx is being called
    to stop the rx engine.
    
    Change-Id: I51d63d2498a10748b6e63e6b4c56b35f2bffd66c
    Signed-off-by: Aniket Randive <quic_arandive@quicinc.com>

--- drivers/tty/serial/msm_geni_serial.c
+++ drivers/tty/serial/msm_geni_serial.c
@@ -2248,6 +2248,13 @@ static int stop_rx_sequencer(struct uart_port *uport)
 
 	if (!uart_console(uport)) {
 		/*
+		 * Enable SW flow control and pull RFR line HIGH before doing stop_rx.
+		 * This is to prevent any rx data to come into the uart rx fifo
+		 * when stop_rx is being done.
+		 */
+		msm_geni_serial_set_manual_flow(false, port);
+
+		/*
 		 * Wait for the stale timeout around 10msec to happen
 		 * if there is any data pending in the rx fifo.
 		 * This will help to handle incoming rx data in stop_rx_sequencer
@@ -2352,7 +2359,13 @@ static int stop_rx_sequencer(struct uart_port *uport)
 			geni_se_dump_dbg_regs(&port->serial_rsc,
 				uport->membase, port->ipc_log_misc);
 		}
-		msm_geni_serial_allow_rx(port);
+		/*
+		 * Do not override client requested manual_flow using set_mctrl
+		 * else driver can override client configured flow.
+		 */
+		if (!uart_console(uport) && !port->manual_flow)
+			msm_geni_serial_allow_rx(port);
+
 		geni_write_reg(FORCE_DEFAULT, uport->membase,
 					GENI_FORCE_DEFAULT_REG);
 
@@ -2376,6 +2389,13 @@ static int stop_rx_sequencer(struct uart_port *uport)
 	port->s_cmd = false;
 
 exit_rx_seq:
+	/*
+	 * Do not override client requested manual_flow using set_mctrl
+	 * else driver can override client configured flow.
+	 */
+	if (!uart_console(uport) && !port->manual_flow)
+		msm_geni_serial_set_manual_flow(true, port);
+
 	geni_status = geni_read_reg_nolog(uport->membase, SE_GENI_STATUS);
 	IPC_LOG_MSG(port->ipc_log_misc, "%s: End 0x%x dma_dbg:0x%x\n",
 		    __func__, geni_status,
