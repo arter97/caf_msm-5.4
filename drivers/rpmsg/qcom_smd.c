arter97/caf_msm-5.4

commit 77d5054e277f20f5a989a89730e15adb2b3c8097

rpmsg: qcom_smd: Add GET/SET signal support
Add support to GET and SET the transport signals by the clients.

Change-Id: Iba3ba8653d777187f11c3cbfe29b587cbc64d213
Signed-off-by: Santhosh Kumar M <santhoshm@codeaurora.org>
Signed-off-by: Sivasri Kumar Vanka <sivasri@codeaurora.org>
Signed-off-by: Arun Prakash <quic_app@quicinc.com>

drivers/rpmsg/qcom_smd.c
@@ -17,6 +17,7 @@
 #include <linux/sizes.h>
 #include <linux/slab.h>
 #include <linux/soc/qcom/smem.h>
+#include <linux/termios.h>
 #include <linux/wait.h>
 #include <linux/rpmsg.h>
 #include <linux/rpmsg/qcom_smd.h>
@@ -242,6 +243,7 @@ struct qcom_smd_channel {
 	void *drvdata;
 
 	struct list_head list;
+	u32 rsigs;
 
 	void *extended_buf;
 	void *ext_buf;
@@ -306,6 +308,16 @@ struct smd_channel_info_word_pair {
 		le32_to_cpu(channel->info_word ?			      \
 			channel->info_word->rx.param :			      \
 			channel->info->rx.param);			      \
+	})
+
+#define GET_RX_CHANNEL_SIGNAL(channel)					      \
+	({								      \
+		(GET_RX_CHANNEL_FLAG(channel, fDSR) ? TIOCM_DSR : 0) |	      \
+		(GET_RX_CHANNEL_FLAG(channel, fCTS) ? TIOCM_CTS : 0) |	      \
+		(GET_RX_CHANNEL_FLAG(channel, fCD) ? TIOCM_CD : 0) |	      \
+		(GET_RX_CHANNEL_FLAG(channel, fRI) ? TIOCM_RI : 0); |	\
+		(GET_TX_CHANNEL_FLAG(channel, fDSR) ? TIOCM_DTR : 0) |	\
+		(GET_TX_CHANNEL_FLAG(channel, fCTS) ? TIOCM_RTS : 0);	\
 	})
 
 #define SET_RX_CHANNEL_FLAG(channel, param, value)			     \
@@ -681,9 +693,11 @@ exit:
  */
 static bool qcom_smd_channel_intr(struct qcom_smd_channel *channel)
 {
+	struct rpmsg_endpoint *ept = &channel->qsept->ept;
 	bool need_state_scan = false;
 	int remote_state;
 	__le32 pktlen;
+	u32 rsig = 0;
 	int avail;
 	int ret;
 
@@ -705,6 +719,14 @@ static bool qcom_smd_channel_intr(struct qcom_smd_channel *channel)
 	/* Don't consume any data until we've opened the channel */
 	if (channel->state != SMD_CHANNEL_OPENED)
 		goto out;
+
+	/* Check if any signal updated */
+	rsig = GET_RX_CHANNEL_SIGNAL(channel);
+
+	if ((ept->sig_cb) && (channel->rsigs != rsig)) {
+		ept->sig_cb(ept->rpdev, ept->priv, channel->rsigs, rsig);
+		channel->rsigs = rsig;
+	}
 
 	/* Indicate that we've seen the new data */
 	SET_RX_CHANNEL_FLAG(channel, fHEAD, 0);
@@ -1116,6 +1138,37 @@ static __poll_t qcom_smd_poll(struct rpmsg_endpoint *ept,
 	return mask;
 }
 
+static int qcom_smd_get_sigs(struct rpmsg_endpoint *ept)
+{
+	struct qcom_smd_endpoint *qsept = to_smd_endpoint(ept);
+	struct qcom_smd_channel *channel = qsept->qsch;
+
+	return GET_RX_CHANNEL_SIGNAL(channel);
+}
+
+static int qcom_smd_set_sigs(struct rpmsg_endpoint *ept, u32 set, u32 clear)
+{
+	struct qcom_smd_endpoint *qsept = to_smd_endpoint(ept);
+	struct qcom_smd_channel *channel = qsept->qsch;
+
+	if (set & TIOCM_DTR)
+		SET_TX_CHANNEL_FLAG(channel, fDSR, 1);
+	else
+		SET_TX_CHANNEL_FLAG(channel, fDSR, 0);
+
+	if (set & TIOCM_RTS)
+		SET_TX_CHANNEL_FLAG(channel, fCTS, 1);
+	else
+		SET_TX_CHANNEL_FLAG(channel, fDSR, 0);
+
+	SET_TX_CHANNEL_FLAG(channel, fSTATE, 1);
+	qcom_smd_signal_channel(channel);
+	smd_ipc(channel->edge->ipc, false, NULL, "%s: ch %s ed %s\n",
+		__func__, channel->name, channel->edge->name);
+
+	return 0;
+}
+
 /*
  * Finds the device_node for the smd child interested in this channel.
  */
@@ -1167,6 +1220,8 @@ static const struct rpmsg_endpoint_ops qcom_smd_endpoint_ops = {
 	.send = qcom_smd_send,
 	.trysend = qcom_smd_trysend,
 	.poll = qcom_smd_poll,
+	.get_signals = qcom_smd_get_sigs,
+	.set_signals = qcom_smd_set_sigs,
 };
 
 static void qcom_smd_release_device(struct device *dev)

commit 618c98871ed871983c9ff071aed7f2463372ecdb

rpmsg: qcom_smd: Fix PF verification failure
Fix PF verification failure introduced by the commit
(77d5054e277f2 'rpmsg: qcom_smd: Add GET/SET signal support').

Change-Id: I649b965fbc7c22376a4cc1c77796a24394d3596f
Signed-off-by: Arun Prakash <quic_app@quicinc.com>

drivers/rpmsg/qcom_smd.c
@@ -315,7 +315,7 @@ struct smd_channel_info_word_pair {
 		(GET_RX_CHANNEL_FLAG(channel, fDSR) ? TIOCM_DSR : 0) |	      \
 		(GET_RX_CHANNEL_FLAG(channel, fCTS) ? TIOCM_CTS : 0) |	      \
 		(GET_RX_CHANNEL_FLAG(channel, fCD) ? TIOCM_CD : 0) |	      \
-		(GET_RX_CHANNEL_FLAG(channel, fRI) ? TIOCM_RI : 0); |	\
+		(GET_RX_CHANNEL_FLAG(channel, fRI) ? TIOCM_RI : 0) |	\
 		(GET_TX_CHANNEL_FLAG(channel, fDSR) ? TIOCM_DTR : 0) |	\
 		(GET_TX_CHANNEL_FLAG(channel, fCTS) ? TIOCM_RTS : 0);	\
 	})