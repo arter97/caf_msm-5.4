fb6cd76512c9d7a9d04776f901df5af386d9ff80
Hui Li <quic_hul@quicinc.com>
2023-09-16 10:38:22 +0800

    soc: qcom: hgsl: fix potential use after free
    A malicous user can close the fd after dma_buf_fd, then there
    will be a use after free issue due to later's get_dma_buf, fix
    it by installing fd after get_dma_buf.
    
    Change-Id: Iffca7c0eb1cc58a6a52a7100e55a639c75ff0a62
    Signed-off-by: Hui Li <quic_hul@quicinc.com>
    (cherry picked from commit 0182f47488e9ef0b175bbf915e23606c80dc422a)

--- drivers/soc/qcom/hgsl/hgsl.c
+++ drivers/soc/qcom/hgsl/hgsl.c
@@ -1282,13 +1282,15 @@ static int hgsl_ioctl_get_shadowts_mem(struct file *filep, unsigned long arg)
 
 	dma_buf = ctxt->shadow_ts_node.dma_buf;
 	if (dma_buf) {
+		/* increase reference count before install fd. */
+		get_dma_buf(dma_buf);
 		params.fd = dma_buf_fd(dma_buf, O_CLOEXEC);
 		if (params.fd < 0) {
 			LOGE("dma buf to fd failed\n");
 			ret = -ENOMEM;
+			dma_buf_put(dma_buf);
 			goto out;
 		}
-		get_dma_buf(dma_buf);
 	}
 
 	if (copy_to_user(USRPTR(arg), &params, sizeof(params))) {
@@ -1862,7 +1864,30 @@ static int hgsl_ioctl_mem_alloc(struct file *filep, unsigned long arg)
 		list_add(&mem_node->node, &priv->mem_allocated);
 		mutex_unlock(&priv->lock);
 		hgsl_trace_gpu_mem_total(priv, mem_node->memdesc.size64);
+	/* increase reference count before install fd. */
+	get_dma_buf(mem_node->dma_buf);
+	params.fd = dma_buf_fd(mem_node->dma_buf, O_CLOEXEC);
+
+	if (params.fd < 0) {
+		LOGE("dma_buf_fd failed, size 0x%x", mem_node->memdesc.size);
+		ret = -EINVAL;
+		dma_buf_put(mem_node->dma_buf);
+		goto out;
 	}
+
+	if (copy_to_user(USRPTR(arg), &params, sizeof(params))) {
+		ret = -EFAULT;
+		goto out;
+	}
+	if (copy_to_user(USRPTR(params.memdesc),
+		&mem_node->memdesc, sizeof(mem_node->memdesc))) {
+		ret = -EFAULT;
+		goto out;
+	}
+	mutex_lock(&priv->lock);
+	list_add(&mem_node->node, &priv->mem_allocated);
+	mutex_unlock(&priv->lock);
+	hgsl_trace_gpu_mem_total(priv, mem_node->memdesc.size64);
 
 out:
 	if (ret && mem_node) {
