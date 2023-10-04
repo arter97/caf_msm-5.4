6d4f02ee217471e90afb9c17e824e5187f2bcf62
DEEPAK SANNAPAREDDY <quic_sdeeredd@quicinc.com>
2023-10-04 14:53:03 +0530

    msm: adsprpc : Fix use after free in fastrpc_internal_mem_unmap
    Thread 1 can make a to call fastrpc_mmap_create under internal mem map
    and release fl->map_mutex. Thread 2 can make call to internal mem unmap,
    acquire fl->map_mutex and get same map though fastrpc_mmap_remove.
    Thread 1 fail in fastrpc_mem_map_to_dsp jumps to bail and do map free.
    Thread 2 still holds same map which can lead use after free. Serialize
    fastrpc internal mem map and unmap.
    
    Change-Id: I54a3602914b43fc67635c0de193bd21aa13daaa3
    Signed-off-by: DEEPAK SANNAPAREDDY <quic_sdeeredd@quicinc.com>
    (cherry picked from commit 65e9480dfe7e41928b14d442da57749ab1ed3bd4)
    (cherry picked from commit 6f740df14dadb26884998b86cfb5b18c6daa55e7)

--- drivers/char/adsprpc.c
+++ drivers/char/adsprpc.c
@@ -4929,6 +4929,7 @@ static int fastrpc_internal_mem_map(struct fastrpc_file *fl,
 	int err = 0;
 	struct fastrpc_mmap *map = NULL;
 
+	mutex_lock(&fl->internal_map_mutex);
 	VERIFY(err, fl->dsp_proc_init == 1);
 	if (err) {
 		pr_err("adsprpc: ERROR: %s: user application %s trying to map without initialization\n",
@@ -4967,6 +4968,7 @@ bail:
 			mutex_unlock(&fl->map_mutex);
 		}
 	}
+	mutex_unlock(&fl->internal_map_mutex);
 	return err;
 }
 
@@ -4976,6 +4978,7 @@ static int fastrpc_internal_mem_unmap(struct fastrpc_file *fl,
 	int err = 0;
 	struct fastrpc_mmap *map = NULL;
 
+	mutex_lock(&fl->internal_map_mutex);
 	VERIFY(err, fl->dsp_proc_init == 1);
 	if (err) {
 		pr_err("adsprpc: ERROR: %s: user application %s trying to map without initialization\n",
@@ -5021,6 +5024,7 @@ bail:
 			mutex_unlock(&fl->map_mutex);
 		}
 	}
+	mutex_unlock(&fl->internal_map_mutex);
 	return err;
 }
 
