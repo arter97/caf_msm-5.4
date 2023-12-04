1e7f4c9d8623514677a84d201089166d2398ce01
Ramireddy KrishnaKanth Reddy <quic_ramikris@quicinc.com>
2023-12-04 17:05:59 +0530

    ASoC: msm-pcm-host-voice: Address buffer overflow in hpcm capture copy
    There is no check for the copy data size of the
    ADSP returned payload. Add check for the max
    hpcm_buf_node size before copy to avoid
    buffer out of bounds issue.
    
    Change-Id: I264fbf3588ab20ac82f81eb103a27f61d5ff2a55
    Signed-off-by: Ramireddy KrishnaKanth Reddy <quic_ramikris@quicinc.com>
    (cherry picked from commit 66cdb889a81c50cb4aefbf1daee4816755be16c9)

--- sound/soc/msm/qdsp6v2/msm-pcm-host-voice-v2.c
+++ sound/soc/msm/qdsp6v2/msm-pcm-host-voice-v2.c
@@ -9,6 +9,8 @@
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
+ *
+ * Copyright (c) 2024 Qualcomm Innovation Center, Inc. All rights reserved.
  */
 
 #include <linux/init.h>
@@ -668,6 +670,12 @@ static void hpcm_copy_playback_data_from_queue(struct dai_data *dai_data,
 	if (dai_data->substream == NULL)
 		return;
 
+	if (len >= HPCM_MAX_VOC_PKT_SIZE) {
+		pr_err("%s: Playbcak data len %d is > HPCM_MAX_VOC_PKT_SIZE\n",
+			__func__, len);
+		return;
+	}
+
 	spin_lock_irqsave(&dai_data->dsp_lock, dsp_flags);
 
 	if (!list_empty(&dai_data->filled_queue)) {
@@ -701,6 +709,12 @@ static void hpcm_copy_capture_data_to_queue(struct dai_data *dai_data,
 
 	if (dai_data->substream == NULL)
 		return;
+
+	if (len > HPCM_MAX_VOC_PKT_SIZE) {
+		pr_err("%s: Capture data len %d overflow\n",
+				__func__, len);
+		return;
+	}
 
 	/* Copy out buffer packet into free_queue */
 	spin_lock_irqsave(&dai_data->dsp_lock, dsp_flags);
