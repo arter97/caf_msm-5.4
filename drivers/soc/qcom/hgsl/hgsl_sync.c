683f1e4f014e8a1e0bd067b34a77ca9629b396e3
Long Li <quic_lonli@quicinc.com>
2024-01-18 19:08:44 +0800

    Revert "soc: hgsl: fix race of isync timeline when creating"
    This reverts commit 97137058e226e6e292fcdbaf0d536b64ae52eda4.
    Signed-off-by: Li Long <quic_lonli@quicinc.com>
    Change-Id: Ie24d67e63f832b31fade7291240f180e4c654665

--- drivers/soc/qcom/hgsl/hgsl_sync.c
+++ drivers/soc/qcom/hgsl/hgsl_sync.c
@@ -306,18 +306,16 @@ int hgsl_isync_timeline_create(struct hgsl_priv *priv,
 	timeline->last_ts = 0;
 
 	idr_preload(GFP_KERNEL);
-	spin_lock(&hgsl->isync_timeline_lock);
-	idr = idr_alloc(&hgsl->isync_timeline_idr, timeline, 1, 0, GFP_NOWAIT);
+	spin_lock(&priv->isync_timeline_lock);
+	idr = idr_alloc(&priv->isync_timeline_idr, timeline, 1, 0, GFP_NOWAIT);
+	spin_unlock(&priv->isync_timeline_lock);
+	idr_preload_end();
+
 	if (idr > 0) {
 		timeline->id = idr;
 		*timeline_id = idr;
 		ret = 0;
-	}
-	spin_unlock(&hgsl->isync_timeline_lock);
-	idr_preload_end();
-
-	/* allocate IDR failed */
-	if (ret != 0)
+	} else
 		kfree(timeline);
 
 	return ret;
