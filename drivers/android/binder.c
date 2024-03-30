f09bd53bb884b2addd9a24cd65483be98ac8355c
Carlos Llamas <cmllamas@google.com>
2024-03-30 19:01:14 +0000

    FROMLIST: binder: check offset alignment in binder_get_object()
    Commit 6d98eb95b450 ("binder: avoid potential data leakage when copying
    txn") introduced changes to how binder objects are copied. In doing so,
    it unintentionally removed an offset alignment check done through calls
    to binder_alloc_copy_from_buffer() -> check_buffer().
    
    These calls were replaced in binder_get_object() with copy_from_user(),
    so now an explicit offset alignment check is needed here. This avoids
    later complications when unwinding the objects gets harder.
    
    It is worth noting this check existed prior to commit 7a67a39320df
    ("binder: add function to copy binder object from buffer"), likely
    removed due to redundancy at the time.
    
    Fixes: 6d98eb95b450 ("binder: avoid potential data leakage when copying txn")
    Cc:  <stable@vger.kernel.org>
    Signed-off-by: Carlos Llamas <cmllamas@google.com>
    
    Bug: 320661088
    Bug: 332642763
    Link: https://lore.kernel.org/all/20240330190115.1877819-1-cmllamas@google.com/
    Change-Id: Iaddabaa28de7ba7b7d35dbb639d38ca79dbc5077
    Signed-off-by: Carlos Llamas <cmllamas@google.com>
    Bug: 332642763
    Signed-off-by: Pindar Yang <pindaryang@google.com>
    (cherry picked from commit 668dfb812db727a038035a2386057811f5f08fb0)
    (cherry picked from commit bf4f9bc41c3b5203e1e7284e1de78e82f0630473)

--- drivers/android/binder.c
+++ drivers/android/binder.c
@@ -1820,6 +1820,7 @@ static size_t binder_get_object(struct binder_proc *proc,
 
 	read_size = min_t(size_t, sizeof(*object), buffer->data_size - offset);
 	if (offset > buffer->data_size || read_size < sizeof(*hdr) ||
+	    !IS_ALIGNED(offset, sizeof(u32)) ||
 	    binder_alloc_copy_from_buffer(&proc->alloc, object, buffer,
 					  offset, read_size))
 		return 0;
