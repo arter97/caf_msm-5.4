f723067cc18c674a7269ebf9123757d6b61912dc
Jun Zhang <quic_juzhan@quicinc.com>
2024-05-22 11:24:33 +0800

    soc: hgsl: fix mem_node UAF issue
    In hgsl_ioctl_mem_map_smmu and hgsl_ioctl_mem_alloc, after
    adding mem_node into realted list and unlocking the
    priv->lock, mem_node may be delete immediately,so
    hgsl_trace_gpu_mem_total using mem_node may cause UAF issue.
    Move the hgsl_trace_gpu_mem_total into lock to avoid.
    
    Change-Id: I1f3b271c05e33e074d888af477d5448137b46c0e
    Signed-off-by: Jun Zhang <quic_juzhan@quicinc.com>
    (cherry picked from commit 736cd13990d97bdcc8213dfbbfa612e4c8b610cd)
    (cherry picked from commit 25aaf54b25b3c449b884cc3eab34b70f339c735c)

--- drivers/soc/qcom/hgsl/hgsl.c
+++ drivers/soc/qcom/hgsl/hgsl.c
@@ -1701,8 +1701,8 @@ static int hgsl_ioctl_mem_alloc(struct file *filep, unsigned long arg)
 		}
 		mutex_lock(&priv->lock);
 		list_add(&mem_node->node, &priv->mem_allocated);
-		mutex_unlock(&priv->lock);
 		hgsl_trace_gpu_mem_total(priv, mem_node->memdesc.size64);
+		mutex_unlock(&priv->lock);
 	}
 
 out:
@@ -1876,9 +1876,8 @@ static int hgsl_ioctl_mem_map_smmu(struct file *filep, unsigned long arg)
 		}
 		mutex_lock(&priv->lock);
 		list_add(&mem_node->node, &priv->mem_mapped);
-		mutex_unlock(&priv->lock);
-
 		hgsl_trace_gpu_mem_total(priv, mem_node->memdesc.size64);
+		mutex_unlock(&priv->lock);
 	}
 
 out:
