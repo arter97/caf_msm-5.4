c3a0a6bd84f0fbc5066b18094e8cfd4cecddf291
Xiongtao Fu <quic_xionfu@quicinc.com>
2024-07-19 15:51:06 +0800

    perf: arm: Modify hotplug state of perf counter to CPUHP_AP_ONLINE_DYN
    CPUHP_AP_ONLINE is a predefined fixed state, repeat registration with
    cpuhp_setup_state_nocalls is an incorrect usage. On the other hand,
    CPUHP_AP_ONLINE_DYN allows dynamic allocation and can be registerd
    repeatedly.
    
    Change-Id: I63109abb4ee7b12d05516e3cdbf3c6e8f96ed71d
    Signed-off-by: Xiongtao Fu <quic_xionfu@quicinc.com>

--- arch/arm/kernel/perf_trace_counters.c
+++ arch/arm/kernel/perf_trace_counters.c
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: GPL-2.0-only
 /*
- * Copyright (c) 2021, Qualcomm Innovation Center, Inc. All rights reserved.
+ * Copyright (c) 2021,2024 Qualcomm Innovation Center, Inc. All rights reserved.
  */
 
 #include <linux/uaccess.h>
@@ -19,7 +19,7 @@ DEFINE_PER_CPU(u32[NUM_L1_CTRS], previous_l1_cnts);
 DEFINE_PER_CPU(u32, old_pid);
 DEFINE_PER_CPU(u32, hotplug_flag);
 
-#define USE_CPUHP_STATE CPUHP_AP_ONLINE
+#define USE_CPUHP_STATE CPUHP_AP_ONLINE_DYN
 
 static int tracectr_cpu_hotplug_coming_up(unsigned int cpu)
 {
