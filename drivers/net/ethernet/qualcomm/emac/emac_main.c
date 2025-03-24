ba9e29f2c7c61dd0c0925e5f6088034787487848
Sneh Shah <quic_snehshah@quicinc.com>
2025-03-24 18:36:57 +0530

    net: emac: unregister from msm bus scaling if driver probe fails
    unregister from msm bus scaling if driver probe fails.
    
    Change-Id: I473263e332e7d77892afdc5ab75ac157fd4b7569
    Signed-off-by: Sneh Shah <quic_snehshah@quicinc.com>

--- drivers/net/ethernet/qualcomm/emac/emac_main.c
+++ drivers/net/ethernet/qualcomm/emac/emac_main.c
@@ -3223,6 +3223,7 @@ err_ldo_init:
 	    (ATH8035_PHY_ID == adpt->phydev->phy_id))
 		emac_disable_regulator(adpt, EMAC_VREG1, EMAC_VREG5);
 err_get_resource:
+	msm_emac_clk_path_teardown(adpt);
 	free_netdev(netdev);
 
 	return ret;
