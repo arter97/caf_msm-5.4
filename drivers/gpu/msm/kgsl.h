995df503b855172c8405308c7bb53a0cae3652b7
Sai Krishna Kota <saikota@qti.qualcomm.com>
2026-01-29 12:56:22 +0530

    kgsl: Add copyright message
    Copyright message was removed in change:I03ad7e260434a5b5a3ea08c006128bc59cbcd617
    Add back the copyright message.
    
    Change-Id: I74c84166d6c89c9efe6eeaff4379cef95b48ee5f
    Signed-off-by: Sai Krishna Kota <saikota@qti.qualcomm.com>

--- drivers/gpu/msm/kgsl.h
+++ drivers/gpu/msm/kgsl.h
@@ -1,5 +1,15 @@
 /* Copyright (c) 2008-2019, The Linux Foundation. All rights reserved.
  * Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 and
+ * only version 2 as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
  */
 #ifndef __KGSL_H
 #define __KGSL_H
