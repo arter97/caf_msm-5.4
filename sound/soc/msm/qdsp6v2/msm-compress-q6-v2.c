aedef10ec5efa19b39d502c9f52c7d0bc29fff23
kasamset <kasamset@qti.qualcomm.com>
2026-08-18 13:11:19 +0530

    asoc: compress: race condition handling in msm_compr_volume_put function
    protect driver data using mutex lock available to protect against
    race condition due to multiple thread access.
    
    Change-Id: I4b4671f5951ca405fc29c90a3f32e02c62f3f6bb
    Signed-off-by: kasamset <kasamset@qti.qualcomm.com>
    (cherry picked from commit 36514283238329bd2b72990c13e70692ed0d0744)

--- sound/soc/msm/qdsp6v2/msm-compress-q6-v2.c
+++ sound/soc/msm/qdsp6v2/msm-compress-q6-v2.c
@@ -10,6 +10,7 @@
  * GNU General Public License for more details.
  *
  * Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
+ * All rights reserved.
  */
 
 
@@ -3274,7 +3275,7 @@ static int msm_compr_volume_put(struct snd_kcontrol *kcontrol,
 			__func__, fe_id);
 		return -EINVAL;
 	}
-
+        mutex_lock(&pdata->lock);
 	cstream = pdata->cstream[fe_id];
 	volume = pdata->volume[fe_id];
 
@@ -3284,6 +3285,7 @@ static int msm_compr_volume_put(struct snd_kcontrol *kcontrol,
 		 __func__, fe_id, volume[0], volume[1]);
 	if (cstream)
 		msm_compr_set_volume(cstream, volume[0], volume[1]);
+        mutex_unlock(&pdata->lock);
 	return 0;
 }
 
