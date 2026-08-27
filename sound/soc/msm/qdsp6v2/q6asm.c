76a2ebac191c40d58050a8e538e699cb3d70ec2b
kasamset <kasamset@qti.qualcomm.com>
2026-08-27 13:09:51 +0530

    ASoC: qdsp6v2: Update license headers
    Change-Id: I12cb9ada626479dc6b4b79efc226c3a033ff8e87
    Signed-off-by: kasamset <kasamset@qti.qualcomm.com>

--- sound/soc/msm/qdsp6v2/q6asm.c
+++ sound/soc/msm/qdsp6v2/q6asm.c
@@ -1,17 +1,8 @@
 /*
  * Copyright (c) 2012-2019, The Linux Foundation. All rights reserved.
  * Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
- * All rights reserved.
+ * SPDX-License-Identifier: GPL-2.0-only
  * Author: Brian Swetland <swetland@google.com>
- *
- * This software is licensed under the terms of the GNU General Public
- * License version 2, as published by the Free Software Foundation, and
- * may be copied, distributed, and modified under those terms.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
  */
 #include <linux/fs.h>
 #include <linux/mutex.h>
